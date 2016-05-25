f628f4d958af37698e95f895b80d9694364c84ee
Mauro Cabrales <maurocabrales@Macs-MacBook-Pro-2.local>
2016-05-25 14:16:28 -0500

    This is the php page for the individual page

---
+++ public/individual_item.php
@@ -0,0 +1,60 @@
+<?php  
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="individual_item.css">
+    <title>Individual Item</title>
+</head>
+<body>
+<header>
+<?php require_once '../views/partials/navbar.php'; ?>
+</header>
+<div class="container">
+<h1>Individual Items Page</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="thumbnail">
+                <img src="css/bears.jpg" class="img-responsive" alt="">
+                <div class="caption">
+                    <h3>Item Name Goes HERE!!</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+                <h1><div>Price: This is where the price goes</div></h1>
+                  <h3><p>Contact Information:</p></h3> 
+                       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      </p>
+                    <div>
+                        <h3><a href="">Posted BY USER</a></h3>
+                    </div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-1">
+                    <a href="#" class="btn btn-primary" role="button">EDIT AD</a>
+                </div>
+                <div class="col-md-1">
+                <a href="#" class="btn btn-primary" role="button">Delete AD</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
