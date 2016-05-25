10aab66cac359c34843e17fe6528c7164f6c473a
BobbieO <bobbie2504@msn.com>
2016-05-25 17:29:28 -0500

    Added basic form: name, price, description with placeholders and submit button.

--- public/templates/ads.create.php
+++ public/templates/ads.create.php
@@ -1,4 +1,5 @@
 <?php
+
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +12,29 @@
 <header>
    <?php require '../../views/partials/navbar.php'; ?>
 </header>
-<div class="container text-center"> 
-
-
-
+<div class="container"> 
+    <h1>Create a New Ad</h1>
+    
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <form class="text-center">
+                <div class="form-group">
+                    <label for="itemname">Name</label>
+                    <input type="text" class="form-control" placeholder="Item Name">
+                </div>
+                <div class="form-group">
+                    <label for="price">Price</label>
+                    <input type="number" class="form-control" placeholder="Price">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea class="form-control" rows="5" placeholder="Describe your item"></textarea>
+                </div>
+              
+              <button type="submit" class="btn btn-default">Make My Ad</button>
+            </form>
+        </div>
+    </div>
 
 </div>
 <?php require '../../views/partials/common_js.php'; ?>
