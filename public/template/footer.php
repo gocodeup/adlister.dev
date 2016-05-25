7e683225e17cf335797855f805a408c7509efc56
lo v <lov@los-MacBook-Pro.local>
2016-05-25 14:00:47 -0500

    added a header and footer php

---
+++ public/template/footer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main template</title>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="/template/css/test.css">
+    <!-- <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'> -->
+</head>
+<body>
+
+
+
+
+
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col 4 s12">
+        <h5 class="white-text">Footer Content</h5>
+        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    </div>
+  </div>
+</footer>
+
+
+
+
+
+
+  
+
+</body>
+</html>
+
+
+
+
