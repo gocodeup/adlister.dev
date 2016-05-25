a4f7cf50b97da479ec910c54fae2b4c6b614a511
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-25 10:27:29 -0500

    Adding bootstrap links and scripts for page.

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,1 +1,5 @@
 <!--Partial for head.  Good place to include things like css files.-->
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
