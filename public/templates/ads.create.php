241b69f235959688ccbf3071cfe9e68af128af17
BobbieO <bobbie2504@msn.com>
2016-05-25 17:08:28 -0500

    Initial commit with requires and div container

---
+++ public/templates/ads.create.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create a New Ad</title>
+    <?php require '../../views/partials/head.php'; ?>
+</head>
+<body>
+<header>
+   <?php require '../../views/partials/navbar.php'; ?>
+</header>
+<div class="container text-center"> 
+
+
+
+
+</div>
+<?php require '../../views/partials/common_js.php'; ?>
+</body>
+</html>
