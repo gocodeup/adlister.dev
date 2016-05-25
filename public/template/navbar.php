d9de2093ba4b753853075e797599b27fd7a702de
lo v <lov@los-MacBook-Pro.local>
2016-05-25 14:44:19 -0500

    navbar to be included on all pages

--- public/template/navbar.php
+++ public/template/navbar.php
@@ -1,77 +1,16 @@
-<?php
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>nav/header</title>
-</head>
-<body>
-
-<header>
-  <div class="navbar">
-    <nav>
-      <div class="nav-wrapper">
-        <!-- <a href="#!" class="brand-logo">Logo</a> -->
-        <ul class="right hide-on-med-and-down">
-          <li class="active"><a href="home.html">Home</a></li>
-          <li><a href="items.php">Items</a></li>
-          <li><a href="login.php">Login</a></li>
-           <li><a href="signup.html">Sign-Up</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</header>
-<div class="valign-wrapper">
-  <h1 class ="center-block">Welcome to Adlister!</h1>
+<div class="navbar">
+  <nav>
+    <div class="nav-wrapper">
+      <!-- <a href="#!" class="brand-logo">Logo</a> -->
+      <ul class="right hide-on-med-and-down">
+        <li class="active"><a href="">Home</a></li>
+        <li><a href="">Items</a></li>
+        <li><a href="">Login</a></li>
+         <li><a href="">Sign-Up</a></li>
+      </ul>
+    </div>
+  </nav>
 </div>
 
 
-<main> 
-<!-- this main section is essential if we are going to implement a footer -->
 
-
-
-
-<!--  <div class="container">
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-  </p>
-  </div> -->
-</main>
-
-</body>   
-</html>
