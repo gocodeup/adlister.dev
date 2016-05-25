ed863db25c65a3da582ed888cc386c250d191fde
Will Campbell <sufuninja@gmail.com>
2016-05-25 17:23:49 -0500

    added form and submit btn

--- public/temp-pages/item-create.php
+++ public/temp-pages/item-create.php
@@ -7,11 +7,46 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 
+
+<style type="text/css">
+    .container {
+        background-color: #eeeeee;
+        margin-top: 40px;
+        padding: 20px;
+    }
+
+</style>
 </head>
 <body>
     <?php require 'temp-navbar.php'; ?>
 
-
-
+    <div class="container">
+        <div class="row title center-align">
+            <h1>Adlist It!</h1>
+        </div>
+        <div class="row">
+            <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s12">
+                          <input placeholder="Awesome" id="name" type="text" class="validate">
+                          <label for="name">Name</label>
+                    </div>
+                    <div class="input-field col s12">
+                          <input id="price" type="text" class="validate">
+                          <label for="price">Price</label>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                              <textarea id="description" class="materialize-textarea"></textarea>
+                              <label for="description">Description</label>
+                        </div>
+                    </div>
+                </div>
+                    <button class="center-btn btn waves-effect waves-light" type="submit" name="action">Submit
+                        <i class="material-icons right"></i>
+                    </button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
