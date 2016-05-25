ee2a3202a2c15796a0c3e943666391c6425dc3ff
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-25 14:53:05 -0500

    Completed front end for signup and login modals.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -15,8 +15,6 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="#">Items <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Login</a></li>
-        <li><a href="#">Signup</a></li>
         <li><a href="#">Account</a></li>
         <li><a href="#">Logout</a></li>
         <li><a href="#">Post Ad</a></li>    
@@ -52,6 +50,60 @@
           </ul>
         </li>
       </ul> -->
+      <!-- Login & Signup Buttons -->
+       <div class="nav-menu btn-group pull-right" role="group">
+        <button type="button" class="btn btn-default" id="login-button" data-toggle="modal" data-target=".login-modal">Login</button>
+        <button type="button" class="btn btn-default" id="signup-button" data-toggle="modal" data-target=".signup-modal">Signup</button>
+      </div>
+
+      <!-- Login modal -->
+      <div class="modal fade login-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+              <h4 class="modal-title" id="mySmallModalLabel">Welcome back!</h4>
+              <div id="loginEmailField" class="form-group col-sm-10 col-sm-offset-1">
+                <label for="loginEmail" class="sr-only">Email</label>
+                <input id="loginEmail" type="email" class="form-control" placeholder="Email Address">
+              </div>
+              <div id="loginPasswordField" class="form-group col-sm-10 col-sm-offset-1">
+                  <label for="loginPass" class="sr-only">Password</label>
+                  <input id="loginPass" type="password" class="form-control" placeholder="Password">
+              </div>
+              <div id="emailLoginBtnDiv" class="form-group col-sm-10 col-sm-offset-1 text-center">
+                  <button id="loginWithEmailBtn" type="submit" class="btn btn-primary btn-block">CLICK TO LOG IN</button>
+              </div>
+              <div class="text-center">
+                  <a href="#" id="forgotPasswordLink">Forgot your password?</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Signup modal -->
+      <div class="modal fade signup-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+              <h4 class="modal-title text-center" id="mySmallModalLabel">Let's Get Started!</h4>
+              <div id="loginEmailField" class="form-group col-sm-10 col-sm-offset-1">
+                <label for="loginEmail" class="sr-only">Email</label>
+                <input id="loginEmail" type="email" class="form-control" placeholder="Email Address">
+              </div>
+              <div id="loginPasswordField" class="form-group col-sm-10 col-sm-offset-1">
+                  <label for="loginPass" class="sr-only">Password</label>
+                  <input id="loginPass" type="password" class="form-control" placeholder="Password">
+              </div>
+              <div id="emailLoginBtnDiv" class="form-group col-sm-10 col-sm-offset-1 text-center">
+                  <button id="loginWithEmailBtn" type="submit" class="btn btn-primary btn-block">CLICK TO LOG IN</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
