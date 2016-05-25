71f273ddef205b8ad5645b2713ff4c1d87e171fb
Niraj <niraj.mulji@gmail.com>
2016-05-25 13:54:22 -0500

    This commit contains the created "Ad" class. Additionally, we decided to use "Category" instead of "Categories", as we will create a table of Categories.

--- database/seeds/ads_seeder.php
+++ database/seeds/ads_seeder.php
@@ -4,12 +4,12 @@ require_once __DIR__ . '/../../models/Ad.php';
 
 $ad = new Ad;
 $ad->user_id = '1';
-$ad->date_listed = '2016-25-05';
+$ad->date_listed = '2016-05-25';
 $ad->title = 'Motorcycle';
 $ad->description = 'Yamaha Verago 250cc';
 $ad->price = '5000';
 $ad->img_ulr = "https://upload.wikimedia.org/wikipedia/commons/f/f1/Xv250.JPG";
-$ad->catagories = "vehicles";
+$ad->category = "vehicles";
 $ad->tags = "things that are red";
 $ad->save();
 
