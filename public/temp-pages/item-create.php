9aa3e9c5107ba012772bf730a2c3057d12962607
Will Campbell <sufuninja@gmail.com>
2016-05-25 16:50:00 -0500

    first commit for items page

---
+++ public/temp-pages/item-create.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Adlister | Item Create</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+
+</head>
+<body>
+    <?php require 'temp-navbar.php'; ?>
+
+
+
+</body>
+</html>
