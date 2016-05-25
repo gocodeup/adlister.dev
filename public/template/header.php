13eb74045b3d0be233aa00bbf68f2457ed89fe34
James <canning.jimmy@gmail.com>
2016-05-25 13:03:39 -0500

    Database table migration and seeders

--- public/template/header.php
+++
@@ -1,9 +0,0 @@
-<div class="header">
-	<div class="jumbotron">
-		<div class="container">
-			<h1>Welcome to RadLister!</h1>
-		  	<p>Here's where we can describe what Rad Lister's </p>
-		  	<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-		  </div>
-	</div>
-</div>
