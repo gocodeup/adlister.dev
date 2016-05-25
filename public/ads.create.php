c596c8aaf14aa2c5f17cd8365b00dbb6e024808b
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-25 14:18:39 -0500

    Updated ad create form

--- public/ads.create.php
+++ public/ads.create.php
@@ -1,19 +1,24 @@
-<form method="POST" action="?????????" enctype="multipart/form-data">
+<form method="POST" action="./items/create.php" enctype="multipart/form-data">
 	<fieldset class="form-group">
         <label>A Title For your Ad</label>
-        <input type="text" class="form-control" name="name" maxlength="50" value="<?php if (Input::has('name')) echo $_POST['name']; ?>" required>
+        <input type="text" class="form-control" name="title" maxlength="50" value="<?php if (Input::has('title')) echo $_POST['title']; ?>" required>
     </fieldset>
     <fieldset class="form-group">
         <label>Write a detailed description of what you're selling</label>
-        <input type="text" class="form-control" name="location" maxlength="50" value="<?php if (Input::has('location')) echo $_POST['location']; ?>" required>
+        <input type="text" class="form-control" name="description" maxlength="50" value="<?php if (Input::has('description')) echo $_POST['description']; ?>" required>
     </fieldset>
     <fieldset class="form-group">
         <label>Price</label>
-        <input type="date" class="form-control" name="date_established" value="<?php if (Input::has('date_established')) echo $_POST['date_established']; ?>" required>
+        <input type="date" class="form-control" name="price" value="<?php if (Input::has('price')) echo $_POST['price']; ?>" required>
     </fieldset>
     <fieldset class="form-group">
-        <label>Area In Acres</label>
-        <input type="number" class="form-control" name="area_in_acres" value="<?php if (Input::has('area_in_acres')) echo $_POST['area_in_acres']; ?>" required>
+        <label>Category</label>
+        <select class="c-select" name="category">
+		  <option selected>Car</option>
+		  <option value="1">Motorcycle</option>
+		  <option value="2">Furniture</option>
+		  <option value="3">Electronics</option>
+		</select>
     </fieldset>
     <fieldset class="form-group">
 	    <label>Select an image to upload</label>
