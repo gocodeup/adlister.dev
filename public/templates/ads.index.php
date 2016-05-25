659bd7755924bc61ead320838fc15671052daeb2
BobbieO <bobbie2504@msn.com>
2016-05-25 13:53:05 -0500

    Added images for thumbnails, changed buttons text, added temp lorem text to thumbnails.

--- public/templates/ads.index.php
+++ public/templates/ads.index.php
@@ -6,56 +6,76 @@
 <html>
 <head>
     <title>Items Listing</title>
-    <?php require '../views/partials/head.php'; ?>
+    <?php require '../../views/partials/head.php'; ?>
 </head>
 <body>
 <header>
-   <!--  <?php require '../views/partials/navbar.php'; ?> -->
+   <?php require '../../views/partials/navbar.php'; ?>
 </header>
 
-<div class="container-fluid text-center">
+<div class="container text-center">
     
     <div class="row">
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/artisanChair.jpg" alt="lawnchair">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
 
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/cakeInAJar.jpg" alt="jarcakes">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
 
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/deerWallSculpture.jpg" alt="deerhead">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
 
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/growSpices.jpg" alt="spicejars">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
@@ -64,44 +84,64 @@
     <div class="row">
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/homemadePolish.jpg" alt="nailpolish">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
 
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/naturalCandles.jpg" alt="candles">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
 
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/organicCarSeat.jpg" alt="carseat">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
 
       <div class="col-sm-3">
         <div class="thumbnail">
-          <img src="..." alt="...">
+          <img src="img/priceyPinecone.jpeg" alt="pinecone">
           <div class="caption">
             <h3>Thumbnail label</h3>
-            <p>...</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+            <p><a href="#" class="btn btn-primary" role="button">View</a> <a href="#" class="btn btn-default" role="button">Add to Cart</a></p>
           </div>
         </div>
       </div>
@@ -110,6 +150,6 @@
 </div>
 
 
-<?php require '../views/partials/common_js.php'; ?>
+<?php require '../../views/partials/common_js.php'; ?>
 </body>
 </html>
