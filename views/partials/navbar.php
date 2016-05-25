faa11f5495dcbe5b86532c3852b50821b9ef829c
lo v <lov@los-MacBook-Pro.local>
2016-05-25 14:56:20 -0500

    created a navbar to be included on every page

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,1 +1,13 @@
-<!--partial view for navbar-->
+<div class="navbar">
+  <nav>
+    <div class="nav-wrapper">
+      <!-- <a href="#!" class="brand-logo">Logo</a> -->
+      <ul class="right hide-on-med-and-down">
+        <li class="active"><a href="">Home</a></li>
+        <li><a href="">Items</a></li>
+        <li><a href="">Login</a></li>
+         <li><a href="">Sign-Up</a></li>
+      </ul>
+    </div>
+  </nav>
+</div>
