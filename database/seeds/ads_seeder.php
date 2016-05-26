2e4298aedb5d3655811ce315edafefa2d6466613
lo v <lov@los-MacBook-Pro.local>
2016-05-26 10:50:52 -0500

    creating base of seeder

---
+++ database/seeds/ads_seeder.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once __DIR__ . '/../../models/User.php';
+
+$ads = new AD;
+$ads->itemName = 'Finn Mertens';
+$ads->description = 'finn@fansofbilly.com';
+$ads->price = "finn_the_human";
+$ads->phone = $_ENV['USER_PASS'];
+$ads->email = "finn_the_human";
+$ads->username = "finn_the_human";
+$ads->save();
+
+$ads = new AD;
+$ads->name = 'Jake';
+$ads->email = 'jake@fansofbilly.com';
+$ads->username = "jake_the_dog";
+$ads->password = $_ENV['USER_PASS'];
+$ads->save();
+
+$ads = new AD;
+$ads->name = 'Simon Petrikov';
+$ads->email = 'iceking@princessaholicanonymous.com';
+$ads->username = "ice_king";
+$ads->password = $_ENV['USER_PASS'];
+$ads->save();
+
+$ads = new AD;
+$ads->name = 'Marceline Abadeer';
+$ads->email = 'marceline@vampirequeen.com';
+$ads->username = "marceline_abadeer";
+$ads->password = $_ENV['USER_PASS'];
+$ads->save();
+
+$ads = new AD;
+$ads->name = 'Marceline Abadeer';
+$ads->email = 'marceline@vampirequeen.com';
+$ads->username = "marceline_abadeer";
+$ads->password = $_ENV['USER_PASS'];
+$ads->save();
+
+$ads = new AD;
+$ads->name = 'Marceline Abadeer';
+$ads->email = 'marceline@vampirequeen.com';
+$ads->username = "marceline_abadeer";
+$ads->password = $_ENV['USER_PASS'];
+$ads->save();
+
+$ads = new AD;
+$ads->name = 'Marceline Abadeer';
+$ads->email = 'marceline@vampirequeen.com';
+$ads->username = "marceline_abadeer";
+$ads->password = $_ENV['USER_PASS'];
+$ads->save();
