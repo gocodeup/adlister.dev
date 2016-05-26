6a27db58a0bf1cdc5f3116894e8c0fdc9e852097
Andrew Powell-Morse <andrewpomo815@gmail.com>
2016-05-26 11:31:45 -0500

    Started on user profile page. Deleted some uneccessary files.

---
+++ public/users.show.php
@@ -0,0 +1,57 @@
+<?php require '../views/partials/header.php'; ?>
+<div class="main-container">
+	<div class="container">
+		<h1>Seller Profile: John Doe</h1>
+		<h3>Contact: johnMFdoe@johnMFdoe.com</h3>
+		<div class="row">
+			<div class="col-xs-10">
+				<div class="inner-box">
+					<div class="row">
+						<div class="col-md-5 col-xs-4 col-xxs-12">
+							
+							</div>
+						<div class="col-md-7 col-xs-8 col-xxs-12">
+							<div class="header-data text-center-xs">
+							 
+								<div class="hdata">
+								<div class="mcol-left">
+								 
+								<i class="fa fa-eye ln-shadow"></i></div>
+								<div class="mcol-right">
+								 
+								<p><a href="#">7000</a> <em>visits</em></p>
+								</div>
+								<div class="clearfix"></div>
+								</div>
+							 
+								<div class="hdata">
+								<div class="mcol-left">
+								 
+								<i class="icon-th-thumb ln-shadow"></i></div>
+								<div class="mcol-right">
+								 
+								<p><a href="#">12</a><em>Ads</em></p>
+								</div>
+								<div class="clearfix"></div>
+								</div>
+							 
+								<div class="hdata">
+									<div class="mcol-left">
+									 
+									<i class="fa fa-user ln-shadow"></i></div>
+									<div class="mcol-right">
+									 
+									<p><a href="#">18</a> <em>Favorites </em></p>
+									</div>
+									<div class="clearfix"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php require '../views/partials/footer.php'; ?>
