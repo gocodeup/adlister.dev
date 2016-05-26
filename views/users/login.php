acdacf0c7209ba376ec26e61c62eb0fea11a27ec
lo v <lov@los-MacBook-Pro.local>
2016-05-26 09:37:32 -0500

    Added the placeholder for the form input

--- views/users/login.php
+++ views/users/login.php
@@ -40,12 +40,12 @@ require_once "navbar.php";
                             <form class="col s12">
                                 <div class="row">
                                     <div class="input-field col s12">
-                                        <input id="email" type="email" class="validate">
+                                        <input placeholder="Email/Username" id="email" type="email" class="validate">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="input-field col s12">
-                                        <input id="password" type="password" class="validate">
+                                        <input placeholder="Password" id="password" type="password" class="validate">
                                     </div>
                                 </div>
                                 <div class="center-align">
