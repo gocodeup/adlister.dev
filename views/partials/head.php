abe1d8ed08cd0ea9e157736411a49f2db32b2f76
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-26 10:53:27 -0500

    head file merged with given file from master/public/views/partials/head.php jquery, and materialize are put in.   space for others but no js, css, fonts or icons

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,1 +1,24 @@
-<!--Partial for head.  Good place to include things like css files.-->
+<html>
+<head>
+    
+<!-- jquery hosted by google-->
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+
+<!-- include css here -->
+
+<!-- any foreign fonts -->
+
+<!-- any foreign icons / logos -->
+
+<!-- any require / include that needs to be on all pages here -->
+
+<!-- materialize link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+ 
+    <title>Head Template</title>
+</head>
+<body>
+
+</body>
+</html>
