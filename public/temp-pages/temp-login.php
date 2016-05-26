0c6a5faf764d708d2cf7a466e0158d358fee12df
micahbohemian <Micahjohnson1983@gmail.com>
2016-05-26 08:43:33 -0500

    initial login page commit.  password not yet protected.

--- public/temp-pages/temp-login.php
+++ public/temp-pages/temp-login.php
@@ -10,33 +10,18 @@
 </head>
 <body>
   <?php require "temp-navbar.php"; ?>
-<center>Creating A New User</center>
+<center>Login Page</center>
 <div class="row">
     <div class="col s12 m6 l4 offset-m3 offset-l4">
         <form class="col s12">
           <div class="row">
             <div class="input-field">
-              <input placeholder="First Name" id="first_name" type="text" class="validate">
+              <input placeholder="Usernae" id="userName" type="text" class="validate">
             </div>
           </div>
           <div class="row">
             <div class="input-field">
-              <input placeholder="Last Name" id="last_name" type="text" class="validate">
-            </div>
-          </div>
-          <div class="row">
-            <div class="input-field">
-              <input placeholder="Password" id="password" type="password" class="validate">
-            </div>
-          </div>
-          <div class="row">
-            <div class="input-field">
-              <input placeholder="Confirm Password" id="confirmPassword" type="password" class="validate">
-            </div>
-          </div>
-          <div class="row">
-            <div class="input-field">
-              <input placeholder="Email" id="email" type="email" class="validate">
+              <input placeholder="Password" id="password" type="text" class="validate">
             </div>
           </div>
           <div class="row">
