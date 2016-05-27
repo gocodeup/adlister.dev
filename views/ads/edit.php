5736ac1f2af436129476f3da384f32a67e18eddf
BobbieO <bobbie2504@msn.com>
2016-05-27 14:33:04 -0500

    Added redirect to profile page after edit is completed.

--- views/ads/edit.php
+++ views/ads/edit.php
@@ -12,6 +12,7 @@ if(!empty($_POST)) {
     $ad->description = Input::get('description');
     $ad->image_url = Input::get('image_url');
     $ad->save();
+    header("Location: /account");
 }
 
 ?>
@@ -34,28 +35,29 @@ if(!empty($_POST)) {
     
     <div class="row">
         <div class="col-sm-6 col-sm-offset-3">
-            <form class="text-center">
+            <form class="text-center" method="POST">
                 <div class="form-group">
                     <label for="itemname">Title</label>
-                    <input type="text" class="form-control" value="<?= $ad->title; ?>">
+                    <input type="text" name="title" class="form-control" value="<?= $ad->title; ?>">
                 </div>
                 <div class="form-group">
                     <label for="price">Price</label>
-                    <input type="number" class="form-control" value="<?= $ad->price; ?>">
+                    <input type="number" name="price" class="form-control" value="<?= $ad->price; ?>">
                 </div>
                 <div class="form-group">
                     <label for="description">Description</label>
-                    <textarea class="form-control" rows="5"><?= $ad->description; ?></textarea>
+                    <textarea name="description" class="form-control" rows="5"><?= $ad->description; ?></textarea>
                 </div>
                 <div class="form-group">
                     <img src="<?= $ad->image_url; ?>">
                 </div>
                 <div class="form-group text-left">
                     <label for="itemPicture">Change Picture</label>
-                    <input type="file" id="itemPicture">
+                    <input name="image_url" type="file" id="itemPicture">
                 </div>
               <button type="submit" class="btn btn-default">Submit Changes</button>
             </form>
         </div>
     </div>
 </div>
+
