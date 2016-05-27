9b3acd1a917099080fdab24d83c67416edbebde6
Daniel <d.curran0518@gmail.com>
2016-05-27 16:29:33 -0500

    Was able to get signup.php to create users and throw them into the database, still need to workout password confirmation as well as creating hashed passwords

--- database/migrations/user_migration.php
+++ database/migrations/user_migration.php
@@ -10,6 +10,7 @@ $query = 'CREATE TABLE users (
     email VARCHAR(240) NOT NULL,
     username VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    confirm VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 )';
 $dbc->exec($query);
