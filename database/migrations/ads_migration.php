a5c142d1d4459930531d0215115a27f297f04793
James <canning.jimmy@gmail.com>
2016-05-27 08:46:05 -0500

    Change price to decimal type in ads table, please re-run ads migration and adds seeder

--- database/migrations/ads_migration.php
+++ database/migrations/ads_migration.php
@@ -11,7 +11,7 @@ $query = 'CREATE TABLE ads (
     date_listed DATE NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    price DECIMAL(4,2) UNSIGNED NOT NULL,
+    price DECIMAL(9,2) UNSIGNED NOT NULL,
     img_url TEXT NOT NULL,
     category VARCHAR(255) NOT NULL,
     tags VARCHAR(255),
