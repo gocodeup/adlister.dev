48048e44048175b385ac2937c4f2a7b887b2f91b
BobbieO <bobbie2504@msn.com>
2016-05-27 10:04:02 -0500

    Edited url path, added text.

--- database/seeds/ads_seeder.php
+++ database/seeds/ads_seeder.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . '/../../models/Ad.php';
 
+$dbc->exec('TRUNCATE ads');
+
 $ad = new Ad;
 $ad->title = 'Vintage Lawn Chair';
-$ad->image_url = 'artisanChair.jpg';
-$ad->description = 'Beautiful handmade lawn chair';
+$ad->image_url = '/../../img/artisanChair.jpg';
+$ad->description = 'Beautiful handmade lawn chair, authentic weathering. Serious offers only.';
 $ad->price = 75.00;
 $ad->user_id = 1;
 $ad->save();
@@ -13,15 +15,15 @@ $ad->save();
 
 $ad = new Ad;
 $ad->title = 'Mason Jar Cakes';
-$ad->image_url = 'cakeInAJar.jpg';
-$ad->description = 'Portable cakes.  Eat cake on a bench in a park.';
+$ad->image_url = '/../../img/cakeInAJar.jpg';
+$ad->description = 'Portable cakes.  Eat cake on a bench in a park. Three flavors available: chocolate, strawberry, and vanilla (shown).';
 $ad->price = 5.00;
 $ad->user_id = 2;
 $ad->save();
 
 $ad = new Ad;
 $ad->title = 'Homemade Nail Polish';
-$ad->image_url = 'homemadePolish.jpg';
+$ad->image_url = '/../../img/homemadePolish.jpg';
 $ad->description = 'I made this polish with non-toxic chemicals in my bathroom.';
 $ad->price = 15.00;
 $ad->user_id = 3;
@@ -29,15 +31,15 @@ $ad->save();
 
 $ad = new Ad;
 $ad->title = 'Organic Car Seat';
-$ad->image_url = 'organicCarSeat.jpg';
-$ad->description = 'Who needs an expensive car seat when you can have one made out of a pumpkin!';
+$ad->image_url = '/../../img/organicCarSeat.jpg';
+$ad->description = 'Who needs an expensive car seat when you can have one made out of a pumpkin! Baby not included.';
 $ad->price = 35.00;
 $ad->user_id = 4;
 $ad->save();
 
 $ad = new Ad;
 $ad->title = 'Deer Skull Wall Art';
-$ad->image_url = 'deerWallSculpture.jpg';
+$ad->image_url = '/../../img/deerWallSculpture.jpg';
 $ad->description = 'Skeleton of a deer found in my backyard.  Mounted on a repurposed panel from my grandad\'s house.  The perfect item for the hipster household.';
 $ad->price = 95.00;
 $ad->user_id = 1;
