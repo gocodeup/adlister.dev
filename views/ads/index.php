7c157a96e14596541848b1ff7a21c6513794f280
BobbieO <bobbie2504@msn.com>
2016-05-27 09:07:16 -0500

    View populated with title, price, description, and view button. Images not populating.

--- views/ads/index.php
+++ views/ads/index.php
@@ -5,16 +5,6 @@ require __DIR__ . '/../../models/Ad.php';
 
 $ads = Ad::all();
 
-  
-     
-          $ad = new Ad;
-          
-          $ad->img_url = Input::get('img_url');
-          $ad->title = Input::get('title');
-          $ad->price = Input::get('price');
-          $ad->description = Input::get('description');
-     
-
 ?>
 
 <div class="container text-center">
@@ -22,17 +12,25 @@ $ads = Ad::all();
     <h1>All Items</h1>
   </div> 
 
-  <?php foreach($ads as $ad) : ?>
-  <div class="thumbnail col-sm-3">
-    <img src="<?= $ad['image_url']; ?>">
-    <div class="caption">
-      <h3><?= $ad['title']; ?></h3>
-      <p><?= $ad['price']; ?></p>
-      <p><?= $ad['description']; ?></p>
-  <?php endforeach; ?>
+  <?php foreach($ads->attributes as $index => $ad) : ?>
+      <?php if($index % 4 == 0) : ?>
+        <div class="row">
+      <?php endif;  ?>
 
-  <p>
-      <a href="#" class="btn btn-primary" role="button">View</a>
-  </p> 
+        <div class="thumbnail col-sm-3">
+          <img src="<?= $ad['image_url']; ?>">
+          <div class="caption">
+            <h3><?= $ad['title']; ?></h3>
+            <p><?= $ad['price']; ?></p>
+            <p><?= $ad['description']; ?></p>
+          </div>
+          <p>
+              <a href="#" class="btn btn-primary" role="button">View</a>
+          </p> 
+      <?php if($index % 4 == 3) : ?>
+        </div> <!-- closes row -->
+      <?php endif; ?>    
+        </div> <!-- closes thumbnail -->
+  <?php endforeach; ?>
 </div>
   
