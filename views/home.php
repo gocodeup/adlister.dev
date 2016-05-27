c996f0a67cc0a234f4cc954afa0dcfe93d7c15bb
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-27 16:32:52 -0500

    added     -Home.php         carousel to homepage     -Main.css         #welcome         #features     -IMG         4 new pics

--- views/home.php
+++ views/home.php
@@ -1,3 +1,26 @@
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+  <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="../img/poo_sale1.png" alt="SellYourCrapOrnament">
+      </div>
+
+      <div class="item">
+        <img src="../img/poo_sale5.png" alt="SellYourCrapGift">
+      </div>
+
+      <div class="item">
+        <img src="../img/poo_sale2.png" alt="SellYourCrapCellPhone">
+      </div>
+    </div>
+  </div>
 <div class="container" id="container">
 
     <section id="welcome" class="home-row-picture">
