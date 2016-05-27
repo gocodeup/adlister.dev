1d58fcbe0aae06909feb982c7fa2b39fb5f7b8fa
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-27 15:04:43 -0500

    Removed links to edit and delete ad from individual show page.

--- views/ads/show.php
+++ views/ads/show.php
@@ -15,36 +15,24 @@ $user = User::find($user_id);
 ?>
 <!--Page for single advertisement -->
 <div class="container">
-<h1>Individual Items Page</h1>
+<h1><?= $ad->attributes['title']; ?></h1>
     <div class="row">
 
         <div class="col-sm-6">
-            <div class="thumbnail">
-                <img src="<?= $ad->attributes['image_url']; ?>" class="img-responsive" alt="">
-                <div class="caption">
-                    <h3><?= $ad->attributes['title']; ?></h3>
-                    <p><?= $ad->attributes['description']; ?></p>
-                </div>
-            </div>
+            <img src="<?= $ad->attributes['image_url']; ?>" class="img-responsive" alt="">
+            <p><?= $ad->attributes['description']; ?></p>
         </div>
 
         <div class="row">
             <div class="col-sm-6">
-                <h1>Price: $<?= $ad->attributes['price']; ?></h1>
+                <h2>Price: $<?= $ad->attributes['price']; ?></h2>
                 <h3>Contact Information:</h3> 
                     <p><?= $user->attributes['email']; ?></p>
                     <div>
  <!--                        <a href="">Posted BY USER</a> -->
                     </div>
             </div>
-            <div class="row">
-                <div class="col-sm-1">
-                    <a href="#" class="btn btn-primary" role="button">Edit Ad</a>
-                </div>
-                <div class="col-sm-1">
-                    <a href="#" class="btn btn-primary" role="button">Delete Ad</a>
-                </div>
-            </div>  
+
         </div>
     </div>
 </div>
