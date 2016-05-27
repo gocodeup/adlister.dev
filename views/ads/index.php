26b6614c07f6ba42f1c3c44feae508fe8069a29e
BobbieO <bobbie2504@msn.com>
2016-05-26 20:35:17 -0500

    Still broken.

--- views/ads/index.php
+++ views/ads/index.php
@@ -5,6 +5,16 @@ require __DIR__ . '/../../models/Ad.php';
 
 $ads = Ad::all();
 
+  
+     
+          $ad = new Ad;
+          
+          $ad->img_url = Input::get('img_url');
+          $ad->title = Input::get('title');
+          $ad->price = Input::get('price');
+          $ad->description = Input::get('description');
+     
+
 ?>
 
 <div class="container text-center">
@@ -12,13 +22,13 @@ $ads = Ad::all();
     <h1>All Items</h1>
   </div> 
 
-  <?php foreach($ads as $ad):?>
+  <?php foreach($ads as $ad) : ?>
   <div class="thumbnail col-sm-3">
     <img src="<?= $ad['image_url']; ?>">
     <div class="caption">
-      <h3><?= $ad['title'];?></h3>
-      <p><?= $ad['price'];?></p>
-      <p><?= $ad['description'];?></p>
+      <h3><?= $ad['title']; ?></h3>
+      <p><?= $ad['price']; ?></p>
+      <p><?= $ad['description']; ?></p>
   <?php endforeach; ?>
 
   <p>
