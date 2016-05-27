f94856a3535d45268a03107dcc3a386fbefd4ebf
Mauro Cabrales <maurocabrales@Macs-MacBook-Pro-2.local>
2016-05-27 15:14:16 -0500

    nothing changed

--- models/Ad.php
+++ models/Ad.php
@@ -4,5 +4,25 @@ require_once __DIR__ . '/Model.php';
 
 class Ad extends Model {
 	protected static $table = 'ads';
+
+public static function getUserAds($user_id)
+    {
+
+
+$query = 'SELECT * FROM ' . static::$table . " where user_id = $user_id";
+
+        $stmt = self::$dbc->prepare($query);
+        $stmt->execute();
+
+        //Store the resultset in a variable named $result
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        // The following code will set the attributes on the calling object based on the result variable's contents
+
+        $instance = null;
+
+return $results;
+
+    }
 }
 
