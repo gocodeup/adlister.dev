6f813c489337ec1ae2a7a378a41151637567b405
lo v <lov@los-MacBook-Pro.local>
2016-05-27 11:23:52 -0500

    fixed the signup so the data can be added into the users table

--- public/template/users-signup.php
+++ public/template/users-signup.php
@@ -1,5 +1,22 @@
 <?php
-require_once "navbar.php";
+require_once "../../models/User.php";
+session_start();
+
+var_dump($_POST);
+
+
+if(isset($_POST['name'])){ 
+    $user = new User();
+    $user->name = $_POST['name'];
+    $user->username = $_POST['username'];
+    $user->email = $_POST['email'];
+    if($_POST['passwordValidate'] == $_POST['password']){
+        $user->password = $_POST['password'];
+    }
+    $user->save();
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +25,6 @@ require_once "navbar.php";
     <title>Sign-Up</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-             
 </head>
 <body>
 <div class="container">
@@ -19,54 +34,54 @@ require_once "navbar.php";
             <div class="col-md-6 col-md-offset-3">
                 <p>Please fill out the information below so we can create your account.</p>
                 <?php if (isset($_SESSION['ERROR_MESSAGE'])) : ?>
-                    <div class="alert alert-danger">
+                    <!-- <div class="alert alert-danger"> -->
                         <p class="error"><?= $_SESSION['ERROR_MESSAGE']; ?></p>
                     </div>
                     <?php unset($_SESSION['ERROR_MESSAGE']); ?>
                 <?php endif; ?>
                 <?php if (isset($_SESSION['SUCCESS_MESSAGE'])) : ?>
-                    <div class="alert alert-success">
+                    <!-- <div class="alert alert-success"> -->
                         <p class="success"><?= $_SESSION['SUCCESS_MESSAGE']; ?></p>
                     </div>
                     <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
                 <?php endif; ?>
 
 
-                <form method="POST" action="" data-validation data-required-message="This field is required">
-
+                <form method="POST" action="/template/users-signup.php" >
                 <div class="container">
                     <div class="row">
-                        <form class="col s12">
+                        
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input placeholder="Name" id="name" type="text" class="validate">
+                                    <input placeholder="Name" id="name" type="text"  name="name"class="validate">
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input placeholder="Email" id="email" type="email" class="validate">
+                                    <input placeholder="Email" id="email" type="email" name="email" class="validate">
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input placeholder="Username" id="username" type="text" class="validate">
+                                    <input placeholder="Username" id="username" type="text" name="username" class="validate">
                                 </div>
                             </div>    
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input placeholder="Password" id="password" type="password" class="validate">
+                                    <input placeholder="Password" id="password" type="password" name="password" class="validate">
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="input-field col s12">
-                                    <input placeholder="Confirm Password" id="password" type="password" class="validate">
+                                    <input placeholder="Confirm Password" id="passwordValidate" type="password" name="passwordValidate" class="validate">
                                     <!-- <label for="password">Confirm Password</label> -->
                                 </div>
                             </div>
                             <div class="center-align">
-                                <a class="waves-effect waves-light btn-large">Create Account</a>
+                                <input type="submit" class="waves-effect waves-light btn-large" value="create account">
+                               <!--  <a class="waves-effect waves-light btn-large">Create Account</a> -->
                             </div>
-                    </form>
+                
                         <p>By creating account you agree to adlisters terms and conditions</p>
                         <p>Already have an account?<a href="">Sign in</a></p>
                 </form>
@@ -74,5 +89,9 @@ require_once "navbar.php";
         </div>
     </section>
 </div>
+
+  <!-- Compiled and minified JavaScript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 </body>
 </html>
