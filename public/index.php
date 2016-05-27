87af3e1a1e597c8c8ebba8c7ab94326f49d13cfd
nicolette <nicolette.williams8@icloud.com>
2016-05-27 10:19:46 -0500

    css for LIFE

--- public/index.php
+++ public/index.php
@@ -13,5 +13,6 @@
     <?php require $main_view; ?>
     <?php require '../views/partials/footer.php'; ?>
     <?php require '../views/partials/common_js.php'; ?>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 </body>
 </html>
--- views/ads/edit.php
+++ views/ads/edit.php
@@ -1,1 +1,58 @@
 <!--Page that includes the form to edit an existing ad-->
+<!--Page for creating new advertisement listings-->
+<div class="container" id="container">
+	
+	<div class="super-awesome-ad">
+		Edit Your Super Awesome Ad Here!
+	</div>	
+
+	<form class="form"type="POST" action="#">
+		
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 edit-price">
+				<input name="name" type="text" placeholder="ITEM FOR SALE" class="form-control" id="name">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 price">
+				<input name="price" type="text" placeholder="PRICE" class="form-control" id="price">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 price">
+				<div class="btn-group">
+				  <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				    CATEGORY <span class="caret"></span>
+				  </button>
+				  <ul class="dropdown-menu">
+				    <li><a href="#">APPLIANCES</a></li>
+				    <li><a href="#">AUTO</a></li>
+				    <li><a href="#">BOOKS</a></li>
+				    <li><a href="#">FREE</a></li>
+				    <li><a href="#">MISC.</a></li>
+				    <li><a href="#">OUTDOORS</a></li>
+				    <li><a href="#">TOOLS</a></li>
+				    <li><a href="#">TOYS & GAMES</a></li>
+				  </ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 description">
+				<textarea rows="4" name="description" type="text" placeholder="DESCRIPTION" class="form-control" id="description"></textarea>
+			</div>
+		</div>
+
+		<input type="submit" class="btn btn-danger btn-lg sign-up-btn">
+	</form>
+</div>
+
+<script>
+    $(document).ready(function() {
+    	$(".dropdown-toggle").dropdown();
+	});
+</script>
+
