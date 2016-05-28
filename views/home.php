8cd8571db8dcc5136b12800f8064d90bf719bcd6
BIGRED <RED@Jerods-MBP.attlocal.net>
2016-05-28 10:40:12 -0500

    Main.css      - changed the padding on .centerdiv from 70px to 50px         this allowed the footer to come into view and make a cleaner         page layout with out having to scroll down to view the footer. (line 144)      - changed the class name of .gearpic to .boxpic (line 101)      - added .thumbnail-projects omg to control image size of the hardcoded ads on Home.php (line 95)
    Signup.php
         - changed the class name of ‘gearpic' to 'boxpic' (line 33)
         - replaced the image  "/img/gears.png”  with this image "/img/boxofpoo.png” (line 34)
    
    Home.php
         - changed the h3 section-title from Featured Items to Our Featured Items (line 44)
         - added hardcoded ads to the page [class="thumbnail-projects”] (lines 46-77)
    
    img [folder]
         - added images
    
        -     elmobook.png
        -     decobcorn.png
        -     sofaset.jpg

--- views/home.php
+++ views/home.php
@@ -41,12 +41,45 @@
 
         <div class="row">
 
-            <h3 class="section-title">Featured Items</h3>
+            <h3 class="section-title">Our Featured Items</h3>
             <!-- Placeholder for featured items.-->
+              <div class="row">
+                <div class="col-xs-12 col-md-4">
+                    <div class="thumbnail-projects">
+                        <a href="#"><img class="img-responsive img-rounded" src="img/elmobook.png"></a>
+                        <div class="caption">
+                            <h3>Childrens Book</h3> 
+                            <p>Let us follow our beloved Elmo into this self discovery adventure. <br> $3.50</p>
+                            <p><a href="#" class="btn btn-primary" role="button">view</a></p>
+                        </div><!-- caption end -->
+                    </div><!-- thumbnail end -->
+                </div><!-- col end -->
+                <div class="col-xs-12 col-md-4">
+                    <div class="thumbnail-projects">
+                        <a href="#"><img class="img-responsive img-rounded" src="img/decobcorn.png"></a>
+                        <div class="caption">
+                            <h3>The D'cober</h3> 
+                            <p>D'cob just about any ear of corn that fits into this thing! <br> make offer</p>
+                            <p><a href="#" class="btn btn-primary" role="button">view</a></p>
+                        </div><!-- caption end -->
+                    </div><!-- thumbnail end -->
+                </div><!-- col end -->
+                <div class="col-xs-12 col-md-4">
+                    <div class="thumbnail-projects">
+                        <a href="#"><img class="img-responsive img-rounded" src="img/sofaset.jpg"></a>
+                        <div class="caption">
+                            <h3>Sofa Set</h3> 
+                            <p>Has the perfect amount of cushion in the right spots. <br> asking $250.00</p>
+                            <p><a href="#" class="btn btn-primary" role="button">view</a></p>
+                        </div><!-- caption end -->
+                    </div><!-- thumbnail end -->
+                </div><!-- col end -->
+              </div><!-- row end -->
         </div>
 
     </section>
 
+      
 </div>
 <script>
     $(function(){
