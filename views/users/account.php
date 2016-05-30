0dcebf9fb98099c0d2ecd1f1780eff3c6c350d7f
BobbieO <bobbie2504@msn.com>
2016-05-29 19:34:02 -0500

    Increased paragraph font size throughout. Moved create new ad button. Adjusted positioning on thumbnails and other buttons within thumbnails.

--- views/users/account.php
+++ views/users/account.php
@@ -28,53 +28,64 @@ require_once __DIR__ . '/../../utils/Auth.php';
 
     <hr>
     <div class="row">
-        <div class="col-sm-4 col-sm-offset-3 text-center">
+        <div class="col-sm-4 col-sm-offset-3 ">
             <h2>User Info</h2>
                 
-            <p>Name: <?php echo $user->name; ?></p>
+            <p>Name: <?= $user->name; ?></p>
 
-            <p>Username: <?php echo $user->username; ?></p>
+            <p>Username: <?= $user->username; ?></p>
         
-            <p>Email: <?php echo $user->email; ?></p>
+            <p>Email: <?= $user->email; ?></p>
 
-            <p>Location: <?php echo $user->location; ?></p>
+            <p>Location: <?= $user->location; ?></p>
 
-            <a href='account/edit' class="btn btn-default" type="submit">Edit Profile</a>
+            <div class="row">
+                <div class="col-sm-3 col-md-3 text-left">
+                    <a href='account/edit' class="btn btn-default" type="submit">Edit Profile</a>
+                </div>
+                <div class="col-sm-3 col-md-3 text-left">
+                    <a href='/ads/create' class="btn btn-default" type="submit">Create New Ad</a>
+                </div>
+            </div>
+
         </div>
     </div>
 
     <div>
-        <h2 class="text-left">Your Ads</h2>
+        <div class="row">
+            <h2>Your Ads</h2>
+        </div>  
 
         <div class="row">
-            <div class="col-sm-4 col-sm-offset-3">
-                <?php foreach ($userAds as  $ad):  ?>
-                    <div class="small-thumb thumbnail col-sm-3">
-                        <img class="img-rounded" src="<?= $ad['image_url']; ?>">
-                        <div class="caption">
-                            <h3><?= $ad['title'] ?></h3>
-                            <p>$<?= $ad['price']; ?></p>
-                            <p><?= $ad['description']; ?></p>
-                        </div>
-            
+                <?php foreach ($userAds as $index=>$ad):  ?>
+                    <?php if($index % 4 == 0) : ?>
                         <div class="row">
-                            <div class="col-sm-4">
-                                <h4><a class="btn btn-default" href="/ads/show?id=<?=$ad['id'] ?>">View Ad</a></h4>
+                    <?php endif;  ?>
+                        <div class="small-thumb thumbnail col-sm-3 text-center">
+                            <img class="img-rounded" src="<?= $ad['image_url']; ?>">
+                            <div class="caption">
+                                <h3><?= $ad['title'] ?></h3>
+                                <p>$<?= $ad['price']; ?></p>
+                                <p><?= $ad['description']; ?></p>
                             </div>
-                            <div class="col-sm-4">
-                                <h4><a class="btn btn-default" href="/ads/edit?id=<?=$ad['id'] ?>">Edit Ad</a></h4>
+                
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h4><a class="btn btn-default" href="/ads/show?id=<?=$ad['id'] ?>">View</a></h4>
+                                </div>
+                                <div class="col-sm-3">
+                                    <h4><a class="btn btn-default" href="/ads/edit?id=<?=$ad['id'] ?>">Edit</a></h4>
+                                </div>
+                                <div class="col-sm-3">
+                                    <h4><a class="btn btn-default" href="/ads/delete?id=<?=$ad['id'] ?>">Delete</a></h4>
+                                </div>   
                             </div>
-                            <div class="col-sm-4">
-                                <h4><a class="btn btn-default" href="/ads/delete?id=<?=$ad['id'] ?>">Delete Ad</a></h4>
-                            </div>   
-                        </div>
-                    </div>
+                    <?php if($index % 4 == 3) : ?>
+                        </div> <!-- closes row -->
+                    <?php endif; ?>  
+                        </div> <!-- closes thumbnail -->
                 <?php endforeach; ?>
-            </div>
         </div>
 
-        <div class="row text-center">
-            <a href='/ads/create' class="btn btn-default" type="submit">Create New Ad</a>
-        </div>  
     </div>
 </div>
