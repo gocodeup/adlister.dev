b3af446fd9b0431da63ff346b02b57858587fbea
Natalie Ortiz <ndnatalie@gmail.com>
2016-05-29 20:12:04 -0500

    Adding Goudy Bookletter to the entire website.  Had to increase size of font.

--- views/partials/head.php
+++ views/partials/head.php
@@ -5,4 +5,5 @@
 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link href='https://fonts.googleapis.com/css?family=Goudy+Bookletter+1911' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="/css/main.css">
