53709e3dd722509affd58d754fa8e315fd1dabb8
nicolette <nicolette.williams8@icloud.com>
2016-05-31 13:06:02 -0500

    toggles login, logout, signup buttons

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,16 +1,18 @@
 <!--partial view for navbar-->
 
     <nav class="navbar navbar-default navbar navbar-inverse">
+        <?php if(isset($_SESSION['IS_LOGGED_IN'])): ?>
         <div class="form-group signinbut">
             <a href="/users/login" type="submit" class="btn btn-default navbut">Log Out</a>
         </div>
-
+        <?php else: ?>
         <div class="form-group signinbut">
             <a href="/users/login" type="submit" class="btn btn-default navbut">Log In</a>
         </div>
         <div class="form-group signinbut">
             <a href="/signup" class="btn btn-default navbut">Sign Up!</a>
         </div>
+        <?php endif; ?>
 
         <div class="form-group signinbut">
             <a href="/items" class="btn btn-default navbut">All Items</a>
@@ -20,3 +22,4 @@
             <a href="/" type="submit" class="nav-home">Sell Your Crap!</a>
         </div>
     </nav>
+
