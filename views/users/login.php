18d02f6d25c5f7d0863bab747ad3e5111fc6b267
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-31 09:34:06 -0500

    Removed duplicate links and echo statements of username and password on failed logins

--- views/users/login.php
+++ views/users/login.php
@@ -71,7 +71,6 @@ if (!empty($_POST)){
                                 </div>
                     
                         </div>
-                        <p>New to adlister? <a href="/template/users-signup">Create account</a></p>
 
                         <p>New to adlister? <a href="/users/signup">Create account</a></p>
                     </div>
