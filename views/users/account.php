0d742e5ac91b6ed9440c0caf2dfb4277e724c908
Emartinez511 <mrblonde9993@gmail.com>
2016-05-30 22:02:43 -0500

    Still trying to get Upload user image.

--- views/users/account.php
+++ views/users/account.php
@@ -1,8 +1,16 @@
 <?php
+    var_dump($_SESSION['LOGGED_IN_ID']);
+if(isset($_POST['img_url'])){ 
+    $user = User::find($_SESSION['LOGGED_IN_ID']);
+    $user->name;
+    $user->username;
+    $user->email;
+    $user->user_img = saveUploadedImage('img_url');
+    $user->save();
+}
  foreach ($users as $user) {
     if ($_GET['id'] == $user['id']){ 
-
-
+    
 
 ?> 
  <div class="row">
@@ -12,7 +20,7 @@
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light">
                     <a href="">
-                        <img    height="180" src="img/user.jpg" class="wp-post-image" alt="ITEM_1" title="item_1" /> 
+                        <img height="180" src="/../public/img/uploads/UC.jpg" class="wp-post-image" alt="ITEM_1" title="item_1" /> 
                     </a>
                 </div>
                 <div class="card-content">
@@ -26,14 +34,14 @@
             <li class="collection-item avatar">
                 <img src="images/yuna.jpg" alt="" class="circle">
                 <span class="title">ACCOUNT INFORMATION</span>
-                    <p>First Line <br>
-                      Second Line
+                    <p>
+                    <a class="waves-effect waves-light btn">Edit Profile</a>
                     </p>
                     <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
             </li>
             <li class="collection-item avatar">
             <i class="material-icons circle">folder</i>
-            <span class="title">CHANGE PASSWORD</span>
+            <span class="title">VIEW ADS</span>
                 <p>First Line <br>
                   Second Line
                 </p>
@@ -49,11 +57,23 @@
             </li>
         </ul>
     </div>
-    <a class="waves-effect waves-light btn">Change Profile Picture</a>
     </div>
-  </div><!--Page for user account home
+    <form method="POST" action="/users/account?id=<?= $_GET['id'] ?>" class="col s12" enctype="multipart/form-data">
+        <div class="file-field input-field">
+            <div class="btn">
+              <span>File</span>
+              <input type="file" name="img_url">
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" name="">
+            </div>
+        </div>
+        <button class="btn waves-effect waves-light" type="submit">Submit
+        </button>
+    </form>
+  </div>
+  <?php }} ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
-  <?php }} ?>
 
 
