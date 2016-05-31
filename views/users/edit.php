d26e3895def821cdc766920fb75325a080262337
lo v <lov@los-MacBook-Pro.local>
2016-05-31 08:46:33 -0500

    made another edit to the href

--- views/users/edit.php
+++ views/users/edit.php
@@ -56,7 +56,7 @@ if(isset($_POST['name'])){
                 <?php endif; ?>
 
                 <!-- the action will redirect to the user account -->
-                <form method="POST" action="/users/account.php">
+                <form method="POST" action="/">
                     <div class="container">
                             <div class="row">
                                 <div class="input-field col s12">
