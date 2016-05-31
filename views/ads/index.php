b9cfcbb843d106aceccbebdb8a8e5c49f58b342f
Francisco Escobedo <franciscoeescobedo@gmail.com>
2016-05-31 10:10:16 -0500

    Fixed hrefs on ad index page and home page

--- views/ads/index.php
+++ views/ads/index.php
@@ -7,11 +7,13 @@
         <div class="col s12 m12 l4">
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light">
-                    <img width="305" height="229" src="<?= $ad['img_url']; ?>" class="responsive-img wp-post-image" alt="ITEM_1" title="item_1" sizes="(max-width: 305px) 100vw, 305px"/> 
+                    <a href="/ads/show?id=<?=$ad['id']?>">
+                        <img width="305" height="229" src="<?= $ad['img_url']; ?>" class="responsive-img wp-post-image" alt="ITEM_1" title="item_1" sizes="(max-width: 305px) 100vw, 305px"/> 
+                    </a>
                 </div>
                 <div class="card-content">
                     <p class="area"><a href="/ads/show?id=<?=$ad['id']?>"><?= $ad['ad_name']; ?></a></p>
-                    <a href="show.php"><span class="card-title activator brown-text text-darken-4"><?= $ad['ad_description'] ?></span></a>
+                    <a href="/ads/show?id=<?=$ad['id']?>"><span class="card-title activator brown-text text-darken-4"><?= $ad['ad_description'] ?></span></a>
                 </div>
             </div>
         </div>
