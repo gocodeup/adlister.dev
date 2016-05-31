3a0f989c74614b9f6071dbe4c48160b687dd8fbf
nicolette <nicolette.williams8@icloud.com>
2016-05-31 09:09:08 -0500

    added a logout button that actually works

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -2,8 +2,13 @@
 
     <nav class="navbar navbar-default navbar navbar-inverse">
         <div class="form-group signinbut">
+            <a href="/users/login" type="submit" class="btn btn-default navbut">Sign Out</a>
+        </div>
+
+        <div class="form-group signinbut">
             <a href="/users/login" type="submit" class="btn btn-default navbut">Sign in</a>
         </div>
+
         <div class="logo">
             <a href="/" type="submit" class="nav-home">Sell Your Crap!</a>
         </div>
