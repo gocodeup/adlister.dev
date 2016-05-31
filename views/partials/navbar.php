8a606001e6126d9dcb9686766ca07239cb340275
Emartinez511 <mrblonde9993@gmail.com>
2016-05-31 08:32:13 -0500

    I made an edit to login and logout.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -13,13 +13,6 @@
         <li><a href="/users/login">Login</a></li>
         <li><a href="/users/signup">Sign-Up</a></li>
         <?php } ?>
-        <?php foreach($ads as $ad){ ?>
-        <?php foreach($users as $user){
-          if($ad['user_id'] == $user['id'] && $user['permissions'] == 1) { ?> 
-        <li><a href="/users/account?id=<?=$user['id'] ?>"> Profile </a></li>
-        <?php } elseif($ad['user_id'] == $user['id']) { ?>
-        <li><a href="/users/signup">Sign-Up</a></li>
-        <?php }}} ?>
       </ul>
     </div>
   </nav>
