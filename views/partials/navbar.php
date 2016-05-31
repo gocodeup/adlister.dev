7380d8ac159381a330393c60d8548e5d92e6dbc9
BobbieO <bobbie2504@msn.com>
2016-05-30 20:25:18 -0500

    Edited styling of user name welcome to match rest of navbar.

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -4,67 +4,73 @@
 include_once __DIR__ . '/../../utils/Auth.php';
 require_once __DIR__ . '../../../models/User.php';
 
-  $user = User::find(Auth::id());
- ?>
+$user = User::find(Auth::id());
+
+?>
 
 <?php if (!Auth::check()) { ?>
         
 <!--partial view for navbar-->
 <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/home">Peddler's Corner</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="/home">Home</a></li>
-            <li><a href="/ads">Items</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/signup">Sign Up</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/home">Peddler's Corner</a>
       </div>
-    </nav>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/home">Home</a></li>
+          <li><a href="/ads">Items</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/signup">Sign Up</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
 
 <?php } else { ?>
   <!--partial view for navbar-->
 <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/home">Peddler's Corner</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="/home">Home</a></li>
-            <li><a href="/ads">Items</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right"> 
-          <li> <?= $user->name; ?></li>     
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="/account">Account Info</a></li>
-                <li><a href="/ads/create">Create Ad</a></li>
-                <li><a href="/logout">Logout</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/home">Peddler's Corner</a>
       </div>
-    </nav>
+        
+      <div id="navbar" class="navbar-collapse collapse">
+          
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="/home">Home</a></li>
+          <li><a href="/ads">Items</a></li>
+        </ul>
+          
+        <ul class="nav navbar-nav navbar-right"> 
+          <li class="active hello-user">Welcome <?= $user->name; ?>!</li>     
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="/account">Account Info</a></li>
+              <li><a href="/ads/create">Create Ad</a></li>
+              <li><a href="/logout">Logout</a></li>
+            </ul>
+
+          </li>
+        </ul>
+
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
 
 <?php } ?>
