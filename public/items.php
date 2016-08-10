d6e91c4dfd4bac56938b6f7f219adece66f407c5
Phillip Garcia <phillipgarcia@Phillips-MacBook-Pro-2.local>
2016-08-10 16:55:40 -0500

    Adding landing page and items list page along with product images.

---
+++ public/items.php
@@ -0,0 +1,41 @@
+<?php  
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Captain Rainbow Glitter Smash</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+	</header>
+	<div>
+		<div class="container">
+		<section id="features">			
+			<div>
+			<h3>Featured Items</h3>
+			</div>
+			<div id="item1">
+				<h4 class="itemName">name</h4>
+				<div class="itemImage">image</div>
+				<div class="item description">description</div>
+				<a href="">More Information</a>
+			</div>
+			<div id="item2">
+				<h4 class="itemName">name</h4>
+				<div class="itemImage">image</div>
+				<div class="item description">description</div>
+				<a href="">More Information</a>
+			</div>
+			<div id="item3">
+				<h4 class="itemName">name</h4>
+				<div class="itemImage">image</div>
+				<div class="item description">description</div>
+				<a href="">More Information</a>
+			</div>
+		</section>
+		</div>
+	</div>
+</body>
+</html>
---
+++ public/landing.php
@@ -0,0 +1,71 @@
+<?php  
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Captain Rainbow Glitter Smash</title>
+	<meta charset="utf-8">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<style type="text/css">
+	.itemImage{
+		height: 80px;
+		width: 80px;
+		align-content: center;
+	}
+	#shield {
+		height: 100%;
+		width: 100%;
+	}
+	.container-fluid {
+		background-color: grey;
+		text-align: center;
+	}
+
+</style>
+</head>
+<body>
+	<header>
+	</header>
+	<div>
+		<div class="container-fluid" id="title-block">
+			<h1>Welcome to Captain Rainbow Glitter Smash</h1>
+			<h3>Number 4 place to find the most authentic Avengers gear</h3>
+		</div>
+		<div class="container">
+		<section id="features">			
+			<div>
+			<h3>Featured Items</h3>
+			</div>
+			<div id="item1" class="container-fluid">
+				<h4 class="itemName">Thor's Hammer</h4>
+				<div class="itemImage">
+					<img src="/img/Captain_America_Shield.png" id="shield">
+				</div>
+				<div class="item description">description</div>
+				<a href="">More Information</a>
+			</div>
+			<div id="item2" class="container-fluid">
+				<h4 class="itemName">name</h4>
+				<div class="itemImage">image</div>
+				<div class="item description">description</div>
+				<a href="">More Information</a>
+			</div>
+			<div id="item3" class="container-fluid">
+				<h4 class="itemName">name</h4>
+				<div class="itemImage">image</div>
+				<div class="item description">description</div>
+				<a href="">More Information</a>
+			</div>
+		</section>
+		</div>
+	</div>
+</body>
+</html>
