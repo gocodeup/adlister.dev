35b7e354d4179ab157a32e559d74e557847547bf
Ernesto <neto_neto@hotmail.com>
2016-08-10 15:59:03 -0500

    Add jquery and minified bootstrap jquery scripts

--- views/partials/common_js.php
+++ views/partials/common_js.php
@@ -1,1 +1,3 @@
 <!--Partial view for common javascipt files-->
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
