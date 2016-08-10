bb5704ad42bc26b7a3b45a9121058a11e5cf0f3b
codeupknight <beijingtexan@gmail.com>
2016-08-10 16:48:49 -0500

    set up connection file

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,7 +1,12 @@
 <?php
 
+define('DB_HOST', "127.0.0.1");
+define('DB_NAME', "pokedex_db");
+define('DB_USER', "vagrant");
+define('DB_PASS', "vagrant");
+
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
