7d38cd90e471833c4c641df25f37694d5a358858
Michael Coyle <mcoyle74@gmail.com>
2016-08-11 17:30:41 -0500

    Committing user profile page (account.php). It still needs to have the user's ads iterated--will work on that at a later date.

--- views/users/account.php
+++ views/users/account.php
@@ -1,1 +1,21 @@
 <!--Page for user account home-->
+<div class="container">
+
+	<section id="account">
+		<h1></h1>
+
+		<div class="row">
+
+			<h2>User Info</h2>
+
+		</div>
+
+		<div class="row">
+
+			<h2>Your Ads</h2>
+
+		</div>
+
+	</section>
+
+</div>
