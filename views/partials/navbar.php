0e4d2a7273014c02f13cf2bb03f0f1e99e089655
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-08-11 09:06:46 -0500

    Updated Navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,1 +1,18 @@
 <!--partial view for navbar-->
+
+
+<div class="navbar navbar-fixed-top navbar-default">
+    <div class="navbar-inner">
+        <div class="container">      
+				<a class="brand" style="margin: 0; float: none; text-align:center" href="#">
+                <img src="../pokemon_logo.gif" /></a>
+        </div>
+    </div>
+</div> 
+
+<form class="navbar-form navbar-left" role="search">
+  <div class="form-group">
+    <input type="text" class="form-control" placeholder="Search">
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
