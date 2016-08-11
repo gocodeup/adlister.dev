af1c308ab09cf38debdf37c45a90d793402e7de3
codeupknight <beijingtexan@gmail.com>
2016-08-10 20:28:56 -0500

    updated db_connect for adlister database

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,14 +1,15 @@
 <?php
 
 define('DB_HOST', "127.0.0.1");
-define('DB_NAME', "pokedex_db");
-define('DB_USER', "vagrant");
+define('DB_NAME', "adlister_db");
+define('DB_USER', "admin");
 define('DB_PASS', "vagrant");
 
 // Get new instance of PDO object
 $dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 
 // Tell PDO to throw exceptions on error
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connection = $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-?>
+var_dump($connection);
+
