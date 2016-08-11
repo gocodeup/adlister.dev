8d19e1751def457c8a99a666d3797c681e726f10
Tyler Warren <warrejt@yahoo.com>
2016-08-11 14:55:07 -0500

    create ad template completed

--- views/ads/create.php
+++ views/ads/create.php
@@ -1,1 +1,38 @@
 <!--Page for creating new advertisement listings-->
+<div class="container">
+	<form class="form-horizontal">
+		<fieldset>
+			<!-- Form Name -->
+			<legend>Create Ad</legend>
+			<!-- Title input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="textinput">Title</label>  
+			  <div class="col-md-4">
+			  <input name="textinput" class="form-control input-md" id="textinput" type="text" required placeholder="Title here"> 
+			  </div>
+			</div>
+
+			<!-- Description input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="textinput">Description</label>  
+			  <div class="col-md-4"> 
+			  <textarea name="textarea" required placeholder="Ad Description here"></textarea> 
+			  </div>
+			</div>
+
+			<div class="form-group">
+				<p>
+					Please choose a picture:<br>
+					<input type="file" name="picture" size="40">
+				</p>
+			</div?
+
+			<!-- Submit Button -->
+			<div class="form-group">
+			  <div class="col-md-8">
+			    <button name="button1id" class="btn btn-success" id="button1id">Submit</button>
+			  </div>
+			</div>
+		</fieldset>
+	</form>
+</div>
