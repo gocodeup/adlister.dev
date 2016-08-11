cc5cc53891e68a90dc5019111c408f36e98fb175
Ernesto <neto_neto@hotmail.com>
2016-08-11 14:43:12 -0500

    Unfinished page controller

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -9,24 +9,26 @@ function pageController()
     $data = [];
 
     // finds position for ? in url so we can look at the url minus the get variables
-    $get_pos = strpos($_SERVER['REQUEST_URI'], '?');
-
-    // if a ? was found, cuts off get variables if not just gives full url
-    if ($get_pos !== false)
-    {
-    //saving to test git
-        $request = substr($_SERVER['REQUEST_URI'], 0, $get_pos);
-    }
-    else
-    {
-
-        $request = $_SERVER['REQUEST_URI'];
-    }
+    $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
     // switch that will run functions and setup variables dependent on what route was accessed
     switch ($request) {
-
-
+        case "/":
+            $main_view= '../views/home.php';
+            break;
+        case "/login":
+            $main_view= '../views/users/login.php';
+            break;
+        //not done
+        case "/signup":
+            $main_view= '../views/users/signup.php';
+            break;
+        case "/signup":
+            $main_view= '../views/users/signup.php';
+            break;
+        case "/signup":
+            $main_view= '../views/users/signup.php';
+            break;
         default:    // displays 404 if route not specified above
             $main_view = '../views/404.php';
             break;
