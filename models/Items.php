e0e1ce1b46ebdb613ae508dbc93dd5381177d6d3
Alex Scott <runhearn@gmail.com>
2016-08-12 12:23:20 -0500

    finished up items and tags creation form

--- models/Items.php
+++ models/Items.php
@@ -8,6 +8,15 @@ class Items extends Model {
 
 	// save the id of the user who is logged in posting the item
 
-	// 
+	public function addTags($array)
+	{	
+		$insert = 'INSERT INTO items_tags (item_id, tag_id) VALUES (:item_id, :tag_id)';
+		$stmt = self::$dbc->prepare($insert);
+		foreach($array as $tag) {
+			$stmt->bindValue(':item_id', $this->id, PDO::PARAM_INT);
+			$stmt->bindValue(':tag_id', $tag->id, PDO::PARAM_INT);
+			$stmt->execute();
+		}
+	}
 
 }
