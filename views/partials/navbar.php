0ae5a81d3c8ed8513e7430637e05c369dc866896
Scott <ascrawford@me.com>
2016-08-13 18:00:52 -0500

    Update to Navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,109 +1,44 @@
 <!--partial view for navbar-->
 
+<style>
+
+h1 {
+  color: #800020;
+}
+
+body {
+  border: 20px;
+}
+
+</style>
+
 <body>
 
 <nav class="navbar navbar-default navbar-inverse" role="navigation">
   <div class="container-fluid">
-    <div class="col-xs-12 col-md-4">
-      <div class="row">
-        <h1><strong>POKEDOPTION</strong></h1>
-
-    <!-- Brand and toggle get grouped for better mobile display -->
-<!--     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Login dropdown</a>
-
-    </div> -->
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div> -->
-
-        <div>
-          <form class="navbar-form navbar-right" role="signup">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Create Username">
-              <input type="text" class="form-control" placeholder="Create Password">
-            </div>
-            <button type="submit" class="btn btn-default">Join PokeDoption</button>
-          </form>
-        </div>
-    </div>
     <div class="row">
-        <div>
-          <form class="navbar-form top navbar-right" role="login">
+        <div class="col-xs-1">
+          <img src="volcanion.png" class="img-responsive">
+        </div>  
+        <div class="col-xs-3">
+          <h1><strong>POKEDOPTION</strong></h1>
+        </div>
+        <div class="col-xs-3">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Create A New Team</a></li>
+            <li><a href="#">Adopt A Pokemon</a></li>
+          </ul>
+        </div>
+        <div class="col-xs-5">  
+          <form class="navbar-form navbar" role="login">
             <div class="form-group">
               <input type="text" class="form-control" placeholder="Username">
               <input type="text" class="form-control" placeholder="Password">
-            </div>
             <button type="submit" class="btn btn-default">Login</button>
           </form>
-        <img class="img-responsive" src="volcanion.png">
-        </div>
+            <button type="submit" class="btn btn-default">Join</button>
+        </div>  
     </div>
-      <!-- <ul class="nav navbar-nav navbar-right">
-        <li><p class="navbar-text">Already have an account?</p></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-      <ul id="login-dp" class="dropdown-menu"> -->
-      <!--   <li>
-           <div class="row">
-              <div class="col-md-12">
-                Login via
-                <div class="social-buttons">
-                  <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                  <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                </div>
-                                or -->
-               <!--   <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                    <div class="form-group">
-                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                    </div>
-                    <div class="form-group">
-                       <label class="sr-only" for="exampleInputPassword2">Password</label>
-                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                             <div class="help-block text-right"><a href="">Forget the password ?</a></div>
-                    </div>
-                    <div class="form-group">
-                       <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                    </div>
-                    <div class="checkbox">
-                       <label>
-                       <input type="checkbox"> keep me logged-in
-                       </label>
-                    </div>
-                 </form> -->
-            <!--   <div class="bottom text-center">
-                New here ? <a href="#"><b>Join Us</b></a>
-              </div> -->
-    <!--        </div>
-        </li>
-      </ul>
-        </li>
-      </ul> -->
-      </div>
-  </div><!-- /.container-fluid -->
+  </div>
 </nav>
 </body>
