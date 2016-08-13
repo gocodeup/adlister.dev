83f278bce8402af2c488af000b577ca6be0b2060
codeupknight <beijingtexan@gmail.com>
2016-08-13 14:03:00 -0500

    revised pokemon class model for functionality

--- models/pokemon.php
+++ models/pokemon.php
@@ -6,14 +6,21 @@ class Pokemon extends Model {
 
 	protected static $table = 'pokedex';
 
-    public static function findByNameOrNumber($side) {
-	  if (Input::has($side)) {
-	    $pokemonId = Input::get($side);
+	public static function getPokemon($inputField) {
+	  $pokemonSearch = findByNameOrNumber($inputField);
+	  $query = createQuery($pokemonSearch['searchBy'], $pokemonSearch['pokemonId']);
+	  $result = Pokemon::executeQuery($query);
+	  return $pokemon;
+	}
+
+    protected static function findByNameOrNumber($inputField) {
+	  if (Input::has($inputField)) {
+	    $pokemonId = Input::get($inputField);
 	    if (is_numeric($pokemonId)) {
 	      $searchBy = "Pokedex";
 	    } else {
 	      $searchBy = "Pokemon";
-	      $pokemonId = "'%" . Input::get($side) . "%'";
+	      $pokemonId = "'%" . Input::get($inputField) . "%'";
 	      if ($pokemonId === "'%%'") {
 	        $pokemonId = '1';
 	        $searchBy = "Pokedex";
@@ -29,41 +36,27 @@ class Pokemon extends Model {
 	  ];
 	}
 
-	public static function searchForPokemon($searchBy, $pokemonId) {
-  $sql = <<<SELECTION
+	protected static function createQuery($searchBy, $pokemonId) {
+  		$sql = <<<SELECTION
 SELECT * FROM pokedex 
 WHERE pokedex.$searchBy LIKE $pokemonId 
 ORDER BY pokedex.$searchBy;
 SELECTION;
-	  return $sql;
-	}
-
-	public static function createPokemon($side) {
-	  $side = compare($side);
-	  $query = createQuery($side['searchBy'], $side['pokemonId']);
-	  $result = Pokemon::executeQuery($query);
-	  $pokemon = selectStats($result['result']);
-	  return $pokemon;
+		return $sql;
 	}
 
 	public static function selectStats($pokemon) {
-	  $stats = [
-	    'Name' => $pokemon['Pokemon'],
-	    'Total' => $pokemon['Total'],
-	    'Pokedex' => $pokemon['Pokedex'],
-	    '0' => $pokemon['Hp'],
-	    '1' => $pokemon['Attack'],
-	    '2' => $pokemon['Defense'],
-	    '3' => $pokemon['SpAtt'],
-	    '4' => $pokemon['SpDef'],
-	    '5' => $pokemon['Speed']
+	  return [
+	    'Hp' => $pokemon['Hp'],
+	    'Attack' => $pokemon['Attack'],
+	    'Defense' => $pokemon['Defense'],
+	    'SpAtt' => $pokemon['SpAtt'],
+	    'SpDef' => $pokemon['SpDef'],
+	    'Speed' => $pokemon['Speed']
 	  ];
-	  return $stats;
 	}
 
-	function compareTotals($leftPokemon, $rightPokemon) {
-	  $leftTotal = $leftPokemon['Total'];
-	  $rightTotal = $rightPokemon['Total'];
+	function compareTotals($leftTotal, $rightTotal) {
 	  if ($leftTotal > $rightTotal) {
 	    $leftStatus = "Higher Total Stats";
 	    $rightStatus = "Lower Total Stats";
