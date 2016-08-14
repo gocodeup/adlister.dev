e804d478597cfd64561be12e02f89d3fbf4c950d
codeupknight <beijingtexan@gmail.com>
2016-08-14 14:32:30 -0500

    moved from incorrect folder, and have nearly finished functionality for adding new teams

--- views/teams/add-team.php
+++ views/teams/add-team.php
@@ -1,17 +1,19 @@
 <?php
-
 ?>
-<form method="POST" action="" data-validation data-required-message="This field is required">
-	<div class="form-group">
-		<p>Team Name</p>
-	    <input type="text" minlength="3" maxlength="20" class="form-control" id="team_name" name="team_name" placeholder="Team Name" data-required>
-	</div>
-	<div class="form-group">
-		<p>Logo</p>
-	    <input type="text" class="form-control" id="logo" name="logo" placeholder="logo" data-required>
-	</div>
-	<div class="row">
-		<div class="col-sm-6">
-			<button type="submit" class="btn btn-primary">Sign In</button>
+<div class="col-sm-2"></div>
+<div class="col-sm-6 text-center">
+	<h3 class="text-center"><?= $_POST['MESSAGE'] ?></h3>
+	<form method="POST" action="" data-validation data-required-message="This field is required">
+		<div class="form-group">
+		    <input type="text" minlength="3" maxlength="20" class="form-control" id="team_name" name="TEAM_NAME" placeholder="Team Name" data-required>
 		</div>
-</form>
+		<div class="form-group">
+		    <input type="text" class="form-control" id="logo" name="IMAGE_URL" placeholder="Logo URL" data-required>
+		</div>
+		<div class="row">
+			<div class="col-sm-6">
+				<button type="submit" class="btn btn-primary">Sign In</button>
+			</div>
+		</div>
+	</form>
+</div>
