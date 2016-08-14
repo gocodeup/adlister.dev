f96f6127e1a2aea7303214362e9c45c60706fb49
Gavin <gavinvaught@gmail.com>
2016-08-14 13:59:13 -0500

    built query string into item href on index and home

--- views/ads/index.php
+++ views/ads/index.php
@@ -6,13 +6,12 @@
 		<?php foreach ($items->attributes as $item): ?>
 
 			<div class="col-xs-4">
-				<a href="#"><img class="img-thumbnail img-responsive featured-image" alt="<?= $item['name']; ?>" src="<?= $item['image']; ?>"></a>
+				<a href="/ads/show/?id=<?= $item['id'] ?>"><img class="img-thumbnail img-responsive featured-image" alt="<?= $item['name']; ?>" src="<?= $item['image']; ?>"></a>
 				<br>
 				<h4><?= $item['name']; ?></h4>
-				<div><?= $item['description']; ?></div>
+				<?= $item['description']; ?>
 				<br>
 				<strong>$<?= $item['price']; ?></strong>
-				<hr>
 			</div>
 		<?php endforeach ?>
 	</section>
