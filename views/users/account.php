11d7e3eb73cd7f9896b4c22349a9fa9a9466a9f9
Gavin <gavinvaught@gmail.com>
2016-08-14 16:42:37 -0500

    added a lot of functionality with logging in, editing and viewing account info

--- views/users/account.php
+++ views/users/account.php
@@ -1,1 +1,29 @@
 <!--Page for user account home-->
+<div class="row">
+	<div class="col-md-5 col-md-offset-1">
+		<h3>User Info</h3>
+		<p>
+		<strong>Name: </strong><?= $user->name ?>
+		</p>
+		<p>
+		<strong>Username: </strong><?= $user->username ?>
+		</p>
+		<p>
+		<strong>Email: </strong><?= $user->email ?>
+		</p>
+		<a href="/account/edit/?id=<?= $user->id ?>" class="btn btn-primary">Edit Account</a>
+	</div>
+	<div class="col-md-6">
+		<h3>Your Gavlistings</h3>
+		<?php foreach($items->attributes as $key => $item) : ?>
+			<h4><?= $item['name']; ?></h4>
+			<p>
+				<?= substr($item['description'], 0, 100) . "..."; ?>
+			</p>
+			<p>
+				<a href="/ads/show/?id=<?= $item['id']; ?>">See More</a>
+			</p>
+		<?php endforeach; ?>
+		<a href="/ads/create" class="btn btn-primary">New Listing</a>
+	</div>
+</div>
