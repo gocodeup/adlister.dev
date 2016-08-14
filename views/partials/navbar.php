34006a39e3558254e7c3ac1b906b4434c21a3746
Scott <ascrawford@me.com>
2016-08-14 10:29:29 -0500

    Update to navbar - sign in and change links

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -25,28 +25,17 @@ body {
         </div>
         <div class="col-xs-3">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Create A New Team</a></li>
-            <li><a href="#">Adopt A Pokemon</a></li>
+            <li><a href="#">Sign Up</a></li>
           </ul>
         </div>
         <div class="col-xs-5">  
           <form class="navbar-form navbar" role="signup">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Email">
+              <input type="text" class="form-control" placeholder="Username">
               <input type="text" class="form-control" placeholder="Password">
-            <button type="submit" class="btn btn-default">Sign Up</button>
+            <button type="submit" class="btn btn-default">Sign In</button>
             </div>
           </form>
-          <!-- <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
-                <li><a href="#">Something else here</a></li>
-              </ul>
-            </li>
-          </ul> -->
         </div>  
     </div>
   </div>
