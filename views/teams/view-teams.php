7e268ba6578df2feb76f2d8341c48d1fc3c8271f
codeupknight <beijingtexan@gmail.com>
2016-08-15 06:20:30 -0500

    changed from using session variable for ALL_TEAMS to post

--- views/teams/view-teams.php
+++ views/teams/view-teams.php
@@ -1,9 +1,9 @@
 <div class="col-sm-10 text-center">
 	<div>
-		<h1>All User's Teams</h1>
+		<h1>All Users Saved Teams</h1>
 	</div>
 	<hr>
-	<?php foreach ($_SESSION['ALL_TEAMS'] as $teamId) : ?>
+	<?php foreach ($_POST['ALL_TEAMS'] as $teamId) : ?>
 	<?php extract(displayTeamById($teamId)); ?>
 	<div class="col-sm-4">
 		<a class="all-teams-table" href="visit-team?team=<?= $teamId ?>">
