20041dd87d4d44ee0383d231c40b177b2cf55c1c
codeupknight <beijingtexan@gmail.com>
2016-08-14 21:09:37 -0500

    Revised to redirect users to home page if they are logged in and try to access signup or log in page. Added redirects for new pages.

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -4,68 +4,84 @@ require_once __DIR__ . '/../utils/helper_functions.php';
 
 function pageController()
 {
-    if isset($_SESSION['IS_LOGGED_IN'])
-        {
-        $_POST['MESSAGE'] = "";
-        // defines array to be returned and extracted for view
-        $data = [];
+    $_POST['MESSAGE'] = "";
+    // defines array to be returned and extracted for view
+    $data = [];
 
-        // finds position for ? in url so we can look at the url minus the get variables
-        $get_pos = strpos($_SERVER['REQUEST_URI'], '?');
+    // finds position for ? in url so we can look at the url minus the get variables
+    $get_pos = strpos($_SERVER['REQUEST_URI'], '?');
 
-        // if a ? was found, cuts off get variables if not just gives full url
-        if ($get_pos !== false)
-        {
+    // if a ? was found, cuts off get variables if not just gives full url
+    if ($get_pos !== false)
+    {
 
-            $request = substr($_SERVER['REQUEST_URI'], 0, $get_pos);
-        }
-        else
-        {
+        $request = substr($_SERVER['REQUEST_URI'], 0, $get_pos);
+    }
+    else
+    {
 
-            $request = $_SERVER['REQUEST_URI'];
-        }
+        $request = $_SERVER['REQUEST_URI'];
+    }
 
-        // switch that will run functions and setup variables dependent on what route was accessed
-        switch ($request) {
-            case '/signup':
-                $data += signupUserController();
+    // switch that will run functions and setup variables dependent on what route was accessed
+    switch ($request) {
+        case '/signup':
+            if (Auth::loggedIn()) {
+                $main_view = '../views/home.php';
+                break;
+            } else {
+                signupController();
                 $main_view = '../views/users/signup.php';
                 break;
+            }
 
-            case '/login':
-                $main_view = '../views/users/login.php';
-                loginController();
-                break;
-
-            case '/add-team':
-                $main_view = '../views/teams/add-team.php';
-                addTeamController();
-                break;
-
-            case '/logout':
+        case '/login':
+            if (Auth::loggedIn()) {
                 $main_view = '../views/home.php';
-                Auth::logout();
                 break;
-
-            case '/view-teams':
-                $main_view = '../views/teams/view-teams.php';
+            } else {
+                loginController();
+                $main_view = '../views/users/login.php';
                 break;
+            }
 
-            case '/add-members':
-                $main_view = '../views/teams/add-member.php';
-                addMemberController();
-                break;
+        case '/logout':
+            $main_view = '../views/home.php';
+            Auth::logout();
+            break;
 
-            case '/compare':
-                $main_view = '../views/partials/comparison.php';
+        case '/add-members':
+            $main_view = '../views/teams/add-member.php';
+            addMemberController();
+            break;
 
-            default:    // displays 404 if route not specified above
-                $main_view = '../views/404.php';
-                break;
+        case '/add-team':
+            $main_view = '../views/teams/add-team.php';
+            addTeamController();
+            break;
+
+        case '/my-teams':
+            $main_view = '../views/teams/my-teams.php';
+            myTeamsController();
+            break;
+
+        case '/visit-team':
+            $main_view = '../views/teams/visit-team.php';
+            extract(visitTeamController());
+            break;
+
+        case '/view-teams':
+            $main_view = '../views/teams/view-teams.php';
+            break;
+
+        case '/compare':
+            $main_view = '../views/partials/comparison.php';
+            break;
+
+        default:    // displays 404 if route not specified above
+            $main_view = '../views/404.php';
+            break;
         }
-    } else {
-        $main_view = '/views/home.php';
-    }
     $data['main_view'] = $main_view;
     return $data;
 }
