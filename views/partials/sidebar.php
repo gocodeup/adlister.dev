8f61625b97255c7e9e888a840a0b3a324fd4f87c
codeupknight <beijingtexan@gmail.com>
2016-08-14 21:02:34 -0500

    minor edits for appearance

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -3,16 +3,16 @@
 <link rel="stylesheet" href="/css/main.css">
 
 <body>
-    <div class="sidebar">
-          <h2>Welcome to PokeDoption!</h2>
-          <h5>Create a new team to adopt pokemon and view their total team statistics!</h5>
-              <h4><a href="#">View All Teams</a></h4>
-              <img src="squirtlesun.gif" class="img-responsive" class="sidebarImage">
-              <h4><a href="#">Visit My Teams</a></h4>
-              <img src="pikachu.gif" class="img-responsive">
-              <h4><a href="#">Add New Team</a></h4></li>
-              <img src="purple.gif" class="img-responsive">
-              <h4><a href="#">View All Pokemon</a><h4></li>
-              <img src="bluething.png" class="img-responsive">
+    <div class="sidebar text-center">
+          <h3>Welcome to PokéDoption!</h3>
+          <h5>Create a new team to adopt Pokémon and view their combined statistics!</h5>
+              <h4><a href="/view-teams">View All Teams</a></h4>
+              <img src="squirtlesun.gif" class="img-responsive sidebarImage">
+              <h4><a href="/my-teams">Visit My Teams</a></h4>
+              <img src="pikachu.gif" class="img-responsive sidebarImage">
+              <h4><a href="/add-team">Add New Team</a></h4></li>
+              <img src="purple.gif" class="img-responsive sidebarImage">
+              <h4><a href="/pokedex">View All Pokemon</a><h4></li>
+              <img src="bluething.png" class="img-responsive sidebarImage">
     </div>
 </body>
