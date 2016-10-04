15700ddafe0b123d5a975a88fc0d10beeb581d49
Dan Ross <danross826@gmail.com>
2016-10-04 15:48:58 -0500

    I made the migrator for the ads table in db and made a blank seeder for when the data is decided

---
+++ database/migrations/ad_migration.php
@@ -0,0 +1,17 @@
+<?php
+
+$_ENV = include __DIR__ . '/../../.env.php';
+require_once '../db_connect.php';
+
+$dbc->exec('DROP TABLE IF EXISTS ads');
+
+$query = 'CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(240) NOT NULL,
+    description TEXT,
+    price decimal(10,2) NOT NULL,
+    url VARCHAR(255),
+    PRIMARY KEY (id)
+)';
+
+$dbc->exec($query);
