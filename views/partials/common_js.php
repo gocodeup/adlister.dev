94a4a4a425535b484859c36ec49bc3006bd5a987
Liz Peacock <liz.peacock11@gmail.com>
2016-10-04 15:45:05 -0500

    added jquery cdn

--- views/partials/common_js.php
+++ views/partials/common_js.php
@@ -1,2 +1,5 @@
 <!--Partial view for common javascipt files-->
+<!-- javascript cdn -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- jquery cdn -->
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
