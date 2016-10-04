60aee4c12bb6876ee5355b186a1b23e681f5f0fc
primalrambler <primal.rambler@gmail.com>
2016-10-04 17:40:59 -0500

    testing image links

--- views/ads/index.php
+++ views/ads/index.php
@@ -45,8 +45,8 @@ _________________________________________________________ -->
                         <div class="navbar-header">
 
                             <a class="navbar-brand home" href="index.html">
-                                <img src="img/logo.png" alt="Universal logo" class="hidden-xs hidden-sm">
-                                <img src="img/logo-small.png" alt="Universal logo" class="visible-xs visible-sm"><span class="sr-only">Universal - go to homepage</span>
+                                <img src="../img/logo.png" alt="Universal logo" class="hidden-xs hidden-sm">
+                                <img src="../img/logo-small.png" alt="Universal logo" class="visible-xs visible-sm"><span class="sr-only">Universal - go to homepage</span>
                             </a>
                             <div class="navbar-buttons">
                                 <button type="button" class="navbar-toggle btn-template-main" data-toggle="collapse" data-target="#navigation">
@@ -426,7 +426,7 @@ _________________________________________________________ -->
                         <div class="product">
                             <div class="image">
                                 <a href="shop-detail.html">
-                                    <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                                    <img src="../img/product1.jpg" alt="" class="img-responsive image1">
                                 </a>
                             </div>
                             <!-- /.image -->
