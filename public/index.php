16fa973fb888021890238fa2aeb9d29fbea3e691
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-04 12:30:29 -0500

    Changed the seeder and feeder to have foreign key and role id

--- public/index.php
+++ public/index.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
-require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ .'/../bootstrap.php';
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>OooLister</title>
-    <?php require '../views/partials/head.php'; ?>
+    <?php require __DIR__ .'../views/partials/head.php'; ?>
 </head>
 <body>
-    <?php require '../views/partials/navbar.php'; ?>
+    <?php require __DIR__ .'../views/partials/navbar.php'; ?>
 
     <?php require $main_view; ?>
 
-    <?php require '../views/partials/common_js.php'; ?>
+    <?php require __DIR__ .'../views/partials/common_js.php'; ?>
 </body>
 </html>
