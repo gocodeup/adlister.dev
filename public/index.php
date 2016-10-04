77ef8d34efae1452641de8245ffca711dd16d756
Liz Peacock <liz.peacock11@gmail.com>
2016-10-04 14:00:46 -0500

    added navbar html to partials/navbar.php added css for navbar to main.css

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@ require_once __DIR__ . '/../bootstrap.php';
 <html>
 <head>
     <title>OooLister</title>
-    <?php require '../views/partials/head.php'; ?>
+    <?php require __DIR__ . '../views/partials/head.php'; ?>
 </head>
 <body>
-    <?php require '../views/partials/navbar.php'; ?>
+    <?php require __DIR__ . '../views/partials/navbar.php'; ?>
 
     <?php require $main_view; ?>
 
-    <?php require '../views/partials/common_js.php'; ?>
+    <?php require __DIR__ . '../views/partials/common_js.php'; ?>
 </body>
 </html>
