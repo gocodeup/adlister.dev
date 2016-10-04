95706af1b3daa4c29096329e34484364280d85ec
primalrambler <primal.rambler@gmail.com>
2016-10-04 17:23:41 -0500

    copied in bootstrap template html

--- views/ads/index.php
+++ views/ads/index.php
@@ -1,18 +1,4 @@
 <!--Page for an index of advertisements-->
-<?php
-session_start();
-// require_once __DIR__ . '/../bootstrap.php';
-
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>gaglister</title>
-    <?php require __DIR__.'/../views/partials/head.php'; ?>
-</head>
-<body>
-    <h1>INDEX PAGE</h1>
-	<!-- <?php require __DIR__.'/../views/partials/navbar.php'; ?> -->
 
 	    <div id="all">
 
@@ -870,10 +856,3 @@ _________________________________________________________ -->
         <!-- /#copyright -->
 
         <!-- *** COPYRIGHT END *** -->
-
-	<!-- <?php require $main_view; ?> -->
-
-
-    <?php require __DIR__.'/../views/partials/common_js.php'; ?>
-</body>
-</html>
