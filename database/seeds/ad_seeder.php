371f1160a40f0826e19c79e48830d55b5142e936
Dan Ross <danross826@gmail.com>
2016-10-05 14:52:34 -0500

    changed migrators and seeders to have foreign keys and an image url

--- database/seeds/ad_seeder.php
+++ database/seeds/ad_seeder.php
@@ -7,8 +7,9 @@ $ad->name = 'placeHolder';
 $ad->description = 'placeHolder';
 $ad->price = 100.00;
 $ad->url = "";
-$ad->image_url = "";
+$ad->image_url = "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx";
 $ad->featured = 1;
+$ad->user_id = 1;
 $ad->save();
 
 $ad = new Ad;
@@ -16,18 +17,9 @@ $ad->name = 'placeHolder';
 $ad->description = 'placeHolder';
 $ad->price = 100.00;
 $ad->url = "";
-$ad->image_url = "";
+$ad->image_url = "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx";
 $ad->featured = 1;
-$ad->save();
-
-
-$ad = new Ad;
-$ad->name = 'placeHolder';
-$ad->description = 'placeHolder';
-$ad->price = 100.00;
-$ad->url = "";
-$ad->image_url = "";
-$ad->featured = 1;
+$ad->user_id = 2;
 $ad->save();
 
 
@@ -36,6 +28,18 @@ $ad->name = 'placeHolder';
 $ad->description = 'placeHolder';
 $ad->price = 100.00;
 $ad->url = "";
-$ad->image_url = "";
+$ad->image_url = "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx";
+$ad->featured = 1;
+$ad->user_id = 3;
+$ad->save();
+
+
+$ad = new Ad;
+$ad->name = 'placeHolder';
+$ad->description = 'placeHolder';
+$ad->price = 100.00;
+$ad->url = "";
+$ad->image_url = "https://www.royalcanin.com/~/media/Royal-Canin/Product-Categories/cat-adult-landing-hero.ashx";
 $ad->featured = 0;
+$ad->user_id = 4;
 $ad->save();
