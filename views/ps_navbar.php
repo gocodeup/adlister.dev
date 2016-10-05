96df4612fe69cf8b96bf2e09d3c5acc006d9fde2
Michael Truong <michael.d.truong@icloud.com>
2016-10-05 10:39:57 -0500

    refactored code, now use require

---
+++ views/ps_navbar.php
@@ -0,0 +1,10 @@
+<div class="nbar">
+    <div class="title">photoSHOP</div>
+    <button class="showLogIn">Log In</button>
+    <button class="showPost">Post</button>
+    <form class="search">
+        <input type="text">
+        <button type="submit">Search</button>
+    </form>
+    <div class="showUsername">Insert Username</div>
+</div>
