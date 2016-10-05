569933d785e9010d5867cdafdbbf42f7bc2f38f4
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-05 16:16:51 -0500

    updated env

--- env.template.php
+++ env.template.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST' => '127.0.0.1',
     'DB_NAME' => 'adlister_db',
     'DB_USER' => 'adlister_user',
-    'DB_PASS' => 'dr00993874',
+    'DB_PASS' => 'quackerjack',
 
     'USER_PASS' => '',
 );
