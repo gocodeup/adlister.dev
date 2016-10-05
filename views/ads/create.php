1546dedc4b221c29d1b875d75869c3f1d4c6316e
Steven Letsinger <stevenletsinger@Stevens-MacBook-Pro.local>
2016-10-05 10:52:28 -0500

    getting files set up

--- views/ads/create.php
+++ views/ads/create.php
@@ -1,1 +1,41 @@
-<!--Page for creating new advertisement listings-->
+<div class="container view">
+
+	<section id="create">
+
+		<div class="row">
+
+			<h1 class="section-title">Create Alisting!</h1>
+
+			<div class="col-md-6 col-md-offset-3">
+
+				<form method="POST" action="" data-validation data-required-message="This field is required" enctype="multipart/form-data">
+
+					<div class="form-group">
+					    <input type="text" class="form-control" id="name" name="name" placeholder="Name of listing" data-required>
+					</div>
+					<div class="form-group">
+					    <input type="text" class="form-control" id="description" name="description" placeholder="Describe your listing" data-required>
+					</div>
+					<div class="form-group">
+					    <input type="text" class="form-control" id="price" name="price" placeholder="Price of listing" data-required>
+					</div>
+					<div class="form-group">
+						<label for="image">Image Upload</label>
+						<input type="file" id="image" name="image" placeholder="Image of your Listing">
+					    <input type="text" class="form-control" id="tag" name="tag" placeholder="Tag your Gavin" data-required>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<button type="submit" class="btn btn-primary">Create</button>
+						</div>
+					</div>
+
+				</form>
+
+			</div>
+
+		</div>
+
+	</section>
+
+</div>
--- views/ads/edit.php
+++ views/ads/edit.php
@@ -1,1 +1,39 @@
 <!--Page that includes the form to edit an existing ad-->
+<div class="container view">
+
+	<section id="edit">
+
+		<div class="row">
+
+			<h1 class="section-title">Edit Your listing!</h1>
+
+			<div class="col-md-6 col-md-offset-3">
+
+				<p>Please fill out the information below so we can update your listing.</p>
+
+				<form method="POST" action="" data-validation data-required-message="This field is required">
+
+					<div class="form-group">
+					    <input type="text" class="form-control" id="name" name="name" placeholder="Change the Name of your Listing" data-required>
+					</div>
+					<div class="form-group">
+					    <input type="text" class="form-control" id="description" name="description" placeholder="Change the Description of your Listing" data-required>
+					</div>
+					<div class="form-group">
+					    <input type="text" class="form-control" id="price" name="price" placeholder="Change the Price of your Listing" data-required>
+					</div>
+					<div class="form-group">
+						<label for="image">Image Upload</label>
+						<input type="file" id="image" name="image" placeholder="Image of your Listing">
+					
+					<button type="submit" class="btn btn-primary">Update Listing</button>
+
+				</form>
+
+			</div>
+
+		</div>
+
+	</section>
+
+</div>
