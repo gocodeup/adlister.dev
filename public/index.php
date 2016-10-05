d47e23ef6d9f4d871c2d16cd365e5e2212b475d4
Liz Peacock <liz.peacock11@gmail.com>
2016-10-05 11:03:05 -0500

    removed html from index, populated home.php view, added some css to main.css

--- public/index.php
+++ public/index.php
@@ -11,27 +11,10 @@ session_start();
 </head>
 <body>
     <?php require __DIR__ .'/../views/partials/navbar.php'; ?>
-    <div class="container">
 
-        <div class="row">
+    <?php require $main_view; ?> 
 
-            <div class="col-lg-12">
-                <div class="jumbotron">
-                    <h1>Welcome to Kitchen Kingdom!
-                    </h1>
-                    <p>Your source for all the unique kitchen gadgets you never knew you needed</p>
-                </div>
-            </div>
-
-        </div>
-
-        <hr>
-
-<!--     <?php require $main_view; ?> -->
 	<?php require __DIR__ . '/../views/partials/footer.php'; ?>
-
-    </div>
-<!-- /.container -->
 
 
     <?php require __DIR__ . '/../views/partials/common_js.php'; ?>
