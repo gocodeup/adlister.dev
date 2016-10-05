ec3deacd4f2764aa85a3a58386e94900b72553c5
Dan Ross <danross826@gmail.com>
2016-10-05 15:51:42 -0500

    got first loop working on items page

--- views/itemsPage.php
+++ views/itemsPage.php
@@ -1,40 +1,49 @@
+ <?php
+
+          
+
+
+require_once __DIR__ . '/../models/Ad.php';
+
+require_once __DIR__ . '/../models/Model.php';
+
+require_once __DIR__ . '/../models/User.php';
+
+$ads = Ad::all();
+
+var_dump($ads);
+
+?>
+
+
  <div class="container">
      <div class="row">
-        <div class="col-sm-4">
-            <img src="../img/predator.jpg" alt="Predator">
-           <p><a href="">LED Predator Costume</a></p>
-            <p class="featuredItem">Bring one of cinema’s most feared villains to life in a stunning way by dressing up in this LED predator costume. Composed of over 2,500 LEDs, this eye-catching ensemble is wireless and comes with a small controller that lets you control the lighting sequence.</p>   
-        </div>
-        <div class="col-sm-4">
-            <img src="../img/predator.jpg" alt="Predator">
-            <p><a href="">LED Predator Costume</a></p>
-            <p class="featuredItem">Bring one of cinema’s most feared villains to life in a stunning way by dressing up in this LED predator costume. Composed of over 2,500 LEDs, this eye-catching ensemble is wireless and comes with a small controller that lets you control the lighting sequence.</p>
-            
-        </div>
-        <div class="col-sm-4">
-            <img src="../img/predator.jpg" alt="Predator">
-            <p><a href="">LED Predator Costume</a></p>
-            <p class="featuredItem">Bring one of cinema’s most feared villains to life in a stunning way by dressing up in this LED predator costume. Composed of over 2,500 LEDs, this eye-catching ensemble is wireless and comes with a small controller that lets you control the lighting sequence.</p>
-            
-        </div>                
-    </div>
-     <div class="row">
-        <div class="col-sm-4">
-            <img src="../img/predator.jpg" alt="Predator">
-            <p><a href="">LED Predator Costume</a></p>
-            <p class="featuredItem">Bring one of cinema’s most feared villains to life in a stunning way by dressing up in this LED predator costume. Composed of over 2,500 LEDs, this eye-catching ensemble is wireless and comes with a small controller that lets you control the lighting sequence.</p>   
-        </div>
-        <div class="col-sm-4">
-            <img src="../img/predator.jpg" alt="Predator">
-            <p><a href="">LED Predator Costume</a></p>
-            <p class="featuredItem">Bring one of cinema’s most feared villains to life in a stunning way by dressing up in this LED predator costume. Composed of over 2,500 LEDs, this eye-catching ensemble is wireless and comes with a small controller that lets you control the lighting sequence.</p>
-            
-        </div>
-        <div class="col-sm-4">
-            <img src="../img/predator.jpg" alt="Predator">
-            <p><a href="">LED Predator Costume</a></p>
-            <p class="featuredItem">Bring one of cinema’s most feared villains to life in a stunning way by dressing up in this LED predator costume. Composed of over 2,500 LEDs, this eye-catching ensemble is wireless and comes with a small controller that lets you control the lighting sequence.</p>
-            
-        </div>                
+        <div class="col-lg-12">
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Url</th>
+                </tr>
+
+        <?php foreach ($ads->attributes as $attribute=>$value): ?>
+          <tr>
+            <td><img src="<?= $value['image_url']   ?>" height='100' width='125'></td>
+            <td><?= $value['name']; ?></td>
+            <td><?= $value['description']; ?></td>
+            <td><?= $value['price']; ?></td>
+            <td> <a href="<?= $value['url'] ?>"><?= $value['url'] ?></a></td>
+          </tr>
+        <?php endforeach;?>
+
+
+
+
+            </table>
+
+
+        </div>            
     </div>
 </div>
