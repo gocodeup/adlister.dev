77b63fe3c767df4c303a43f4a2c3c2b95673f27b
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2016-10-05 09:05:28 -0500

    simple commit modified bootstrap.php but not a big change added and deleted

--- bootstrap.php
+++ bootstrap.php
@@ -10,5 +10,7 @@ require_once __DIR__ . '/utils/Auth.php';
 require_once __DIR__ . '/utils/Input.php';
 
 
+
+
 // require front controller
 require_once __DIR__ . '/controllers/PageController.php';
