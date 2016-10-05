a95f9194b42b73f74fe309126cd7b5e315f12238
primalrambler <primal.rambler@gmail.com>
2016-10-05 11:48:41 -0500

    removed extraneous html from template. left main image, description, page and a placeholder column

--- views/ads/show.php
+++ views/ads/show.php
@@ -1,1 +1,97 @@
 <!--Page for single advertisement -->
+<div id="all">
+
+<!-- ==== Bread crumb for navigations ===== -->
+	<div id="heading-breadcrumbs">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <h1>Item Name Goes Here</h1>
+                </div>
+                <div class="col-md-5">
+                    <ul class="breadcrumb">
+                        <li><a href="/">Home</a>
+                        </li>
+                        <li><a href="/ads">All Items</a>
+                        </li>
+                        <li>Item Name Goes Here</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+	</div>
+<!-- ==== Page contents ===== -->
+    <div id="content">
+        <div class="container">
+
+            <div class="row">
+
+	        <!-- *** LEFT COLUMN *** -->
+
+            <div class="col-md-9">
+	            <div class="row" id="productMain">
+	                <div class="col-sm-6">
+	                    <div id="mainImage">
+	                        <img src="/img/detailbig1.jpg" alt="" class="img-responsive">
+	                    </div>
+	                </div>
+	                    <div class="col-sm-6">
+	                        <div class="box">
+	                                <div class="sizes">
+
+	                                    <h3>Product Details</h3>
+
+	                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	                                </div>
+
+	                                <p class="price">$124.00</p>
+
+	                        </div>
+	                    </div>
+	            </div>
+                <div class="box" id="details">
+                    <blockquote>
+                        <p><em>Say something witty • revolving knock knock joke • or user's tag line for item</em>
+                        </p>
+                    </blockquote>
+                </div>
+            </div>
+            <!-- /.col-md-9 -->
+            <!-- *** LEFT COLUMN END *** -->
+
+            <!-- *** RIGHT COLUMN *** -->
+
+                <div class="col-sm-3">
+                    <div class="panel panel-default sidebar-menu">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Optional Column</h3>
+                        </div>
+                    </div>
+
+                    <div class="banner">
+                        <a href="shop-category.html">
+                            <img src="/img/banner.jpg" alt="sales 2014" class="img-responsive">
+                        </a>
+                    </div>
+                    <!-- /.banner -->
+                </div>
+                <!-- /.col-md-3 -->
+
+                    <!-- *** RIGHT COLUMN END *** -->
+
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /#content -->
+    </div>
+
