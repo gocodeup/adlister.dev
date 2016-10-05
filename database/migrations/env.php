1a3b4b8e7a06ba05d2ca341ded445e0fcbea8b9c
Joseph Garcia <josephgarcia@Josephs-MacBook-Air.local>
2016-10-05 16:19:37 -0500

    updated env

---
+++ database/migrations/env.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'DB_HOST' => '127.0.0.1',
+    'DB_NAME' => 'adlister_db',
+    'DB_USER' => 'adlister_user',
+    'DB_PASS' => 'quackerjack',
+
+    'USER_PASS' => '',
+);
