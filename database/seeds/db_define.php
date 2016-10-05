6313057f957c196db489491d72f4cb26e7107b95
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-05 10:42:47 -0500

    Initial Commit:  db_define allows access to our database without compromising secure data. Added the necessary require_once to give us access for testing and manipulation of data.

---
+++ database/seeds/db_define.php
@@ -0,0 +1,7 @@
+<?php
+require "adlister_db.php";
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'adlister_db');
+define('DB_USER', 'vagrant');
+define('DB_PASS', 'vagrant');
+define("LIMIT", 10);
