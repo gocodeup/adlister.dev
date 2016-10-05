41afaff468025ffccc785bda46e3262a67ff751b
Michael Truong <michael.d.truong@icloud.com>
2016-10-05 17:16:02 -0500

    scrapped one pager, added welcome page

---
+++ public/welcome.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <?php require "../views/ps_head.php";  ?>
+
+    </head>
+    <body>
+
+        <?php require "../views/ps_navbar.php"; ?>
+
+        <div class="container front">
+            <div class="banner">
+                <img src="/img/placeholder.png" class="bannerImg">
+            </div>
+        </div>
+
+        <?php require "../views/ps_login.php"; ?>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script src="/js/ps_animations.js"></script>
+
+    </body>
+</html>
