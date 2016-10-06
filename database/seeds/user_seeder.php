65089425b24baaf0f1f18de7e87590a8a62ed726
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-10-06 10:37:06 -0500

    Update:
    Seeder wasn't allowing for data to be injected into the database with the hardcoded "Listings" we created.  We received an error and found out we had left a couple of semi-colons off of our descriptions.
    
    We fixed the bug by adding the semi_colons and re-runing our seeder.  Everything checked out okay and our data is being added accordingly.

--- database/seeds/user_seeder.php
+++ database/seeds/user_seeder.php
@@ -41,3 +41,64 @@ $listing->item_location_city ='San Antonio';
 $listing->item_location_state ='TX';
 $listing->user_id = 1;
 $listing->save();
+
+$listing = new Listing;
+$listing->item_name = '1967 Ford Thunderbird';
+$listing->item_description = 'The car received a dual-stage repaint in the correct Light Green Metallic. The interior looks beautiful in the photos.The factory air conditioning works as it should and blows cold.';
+$listing->item_price = '24000.00';
+$listing->item_location_city ='San Antonio';
+$listing->item_location_state ='TX';
+$listing->user_id = 2;
+$listing->save();
+
+$listing = new Listing;
+$listing->item_name = 'Little Red Tractor';
+$listing->item_description = 'Tractor would make a super cute gift for the little one in your life. This ad will be deleted when sold. Don\'t wait or it may be too late! Please email me with any questions that you may have. Also available for pick up in Round Rock and Austin.';
+$listing->item_price = '100.00';
+$listing->item_location_city ='San Antonio';
+$listing->item_location_state ='TX';
+$listing->user_id = 4;
+$listing->save();
+
+$listing = new Listing;
+$listing->item_name = 'Tag Heuer Formula 1 Grand Date';
+$listing->item_description = 'Great everyday Tag Heuer watch.  Swiss made with a sporty rubber strap. Watch retails at $1,295.00 asking $650 or best offer.';
+$listing->item_price = '650.00';
+$listing->item_location_city ='Austin';
+$listing->item_location_state ='TX';
+$listing->user_id = 3;
+$listing->save();
+
+$listing = new Listing;
+$listing->item_name = 'DC Comics';
+$listing->item_description = 'Hot DC Comics for Sale!  Green Arrow: The Longbow Hunters (1987) #1-3 First Print $15 for the set
+	DC Green Arrow (1987, 1st Series) #1, 1st print. $10
+	Wonder Woman #1 - (1987) $25
+	Suicide Squad #1 (1987) -$25
+	Batman The Killing Joke First Print $45
+	Batman The Dark Knight Returns #1-4 1 - 2nd print 2 to 4 1st print $125
+	The Flash #1 1987 $20
+';
+$listing->item_price = '250.00';
+$listing->item_location_city ='San Antonio';
+$listing->item_location_state ='TX';
+$listing->user_id = 1;
+$listing->save();
+
+$listing = new Listing;
+$listing->item_name = 'Tonka Mighty Dump Truck';
+$listing->item_description = 'Tonka Steel Toughest Mighty Dump Truck..built Tonka tough and to built to last. New condition.';
+$listing->item_price = '30';
+$listing->item_location_city ='San Antonio';
+$listing->item_location_state ='TX';
+$listing->user_id = 4;
+$listing->save();
+
+$listing = new Listing;
+$listing->item_name = '2008 Honda CBR 600';
+$listing->item_description = '2008 Honda CBR 600, 15k miles Very well maintained, Garage kept, 2nd owner, Just registered. New front tire just put on and a Yoshimura Exhaust';
+$listing->item_price = '5300';
+$listing->item_location_city ='San Antonio';
+$listing->item_location_state ='TX';
+$listing->user_id = 2;
+$listing->save();
