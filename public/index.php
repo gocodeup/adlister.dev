10ea6b9347c7aa1136c0df0902efe6f6c79e5081
xconstaud <xconstaud@gmail.com>
2017-04-18 10:39:08 -0500

    Fix Session Start Perams

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
-session_start();
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+
 require_once __DIR__ . '/../bootstrap.php';
 
 ?>
@@ -7,13 +12,13 @@ require_once __DIR__ . '/../bootstrap.php';
 <html>
 <head>
     <title>OooLister</title>
-    <?php require '../views/partials/head.php'; ?>
+    <?php require __DIR__ . '/../views/partials/head.php'; ?>
 </head>
 <body>
-    <?php require '../views/partials/navbar.php'; ?>
+    <?php require __DIR__ . '/../views/partials/navbar.php'; ?>
 
     <?php require $mainView; ?>
 
-    <?php require '../views/partials/common_js.php'; ?>
+    <?php require __DIR__  .'/../views/partials/common_js.php'; ?>
 </body>
 </html>
