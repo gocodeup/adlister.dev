eecc9e30484713197d4d7ca3b972591686feaa72
xconstaud <xconstaud@gmail.com>
2017-04-18 11:23:24 -0500

    Add Ad Migration

---
+++ database/migrations/ad_migration.php
@@ -0,0 +1,19 @@
+<?php
+
+$_ENV = include __DIR__ . '/../../.env.php';
+require_once '../db_connect.php';
+
+$dbc->exec('DROP TABLE IF EXISTS ads');
+
+$query = 'CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(240) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    brand VARCHAR(255) NOT NULL,
+    price DECIMAL(6,2) NOT NULL,
+    description TEXT NOT NULL,
+    photodir VARCHAR(240) NOT NULL,
+    PRIMARY KEY (id)
+)';
+
+$dbc->exec($query);
