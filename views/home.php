0dde6bc44d12c77fbe517a094152a2c7b94ac539
xconstaud <xconstaud@gmail.com>
2017-04-18 17:02:39 -0500

    Implement SASS

--- views/home.php
+++ views/home.php
@@ -1,29 +1,40 @@
 <div class="container-fluid">
-
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+    
     <section id="welcome">
-
         <div class="row">
-
             <div class="col">
-
                 <h1 class="text-center">Welcome To Adlister</h1>
-                <button type="submit" for="submit" class="btn btn-outline-primary">Submit</button>
-
-
             </div>
-
         </div>
-
     </section>
-
     <section id="features">
-
         <div class="row">
-
             <h3 class="section-title">Featured Items</h3>
             <!-- Placeholder for featured items.-->
         </div>
-
     </section>
-
 </div>
