c70b9da6186a7730f1c52163fd2c7bbfd11614eb
Todd Hendricks <hendricks.ta@gmail.com>
2017-04-18 15:47:32 -0500

    add ad model and populate database

--- database/migrations/user_migration.php
+++ database/migrations/user_migration.php
@@ -6,14 +6,6 @@ require_once '../db_connect.php';
 $dbc->exec('DROP TABLE IF EXISTS users');
 $dbc->exec('DROP TABLE IF EXISTS ads');
 
-$ads = 'CREATE TABLE IF NOT EXISTS ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(240) NOT NULL,
-    description VARCHAR (1000) NOT NULL,
-    username VARCHAR(240) NOT NULL,
-    PRIMARY KEY (id)
-)';
-
 $query = 'CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name VARCHAR(240) NOT NULL,
@@ -22,6 +14,16 @@ $query = 'CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 )';
+
+$ads = 'CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(240) NOT NULL,
+    description VARCHAR (1000) NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)';
+
 
 $dbc->exec($query);
 $dbc->exec($ads);
---
+++ database/seeds/ad_seeder.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once __DIR__ . '/../../models/Ad.php';
+
+$ads = new Ad;
+$ads->name = 'Finn Mertens';
+$ads->description = 'product description';
+$ads->user_id = 1;
+$ads->save();
+
+$ads = new Ad;
+$ads->name = 'Jake';
+$ads->description = 'product description';
+$ads->user_id = 2;
+$ads->save();
+
+$ads = new Ad;
+$ads->name = 'Simon Petrikov';
+$ads->description = 'product description';
+$ads->user_id = 3;
+$ads->save();
+
+$ads = new Ad;
+$ads->name = 'Marceline Abadeer';
+$ads->description = 'product description';
+$ads->user_id = 4;
+$ads->save();
+ 
+ ?>
