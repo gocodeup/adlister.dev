76e33f5e3d42d5929b872cffd12d3b9fd66ad952
Phillip <ppalatas@gmail.com>
2017-04-18 12:18:06 -0500

    Committing Changes to the Home Page layout. 	-Created a basic format for the "Top Items" container 	-Header created in basic form and color -- Need to add in a photoshopped logo and anchor the li's to page location anchors

--- views/home.php
+++ views/home.php
@@ -1,27 +1,51 @@
-<div class="container">
+<?php
 
-    <section id="welcome">
 
-        <div class="row">
+?>
 
-            <div class="col-xs-12">
+<body>
+    <div id="header">
+        <ul>
+    
+            <li class="welcomeLi"><h3>Welcome To Adlister</h3></li>
+            <li>Log In</li>
+            <li>Search</li>
+            <li>Categories</li>
+        </ul>
+    </div>
 
-                <h1 class="text-center">Welcome To Adlister</h1>
+            <center><h2 class="section-title">Check Out Our Featured Items</h2></center>
+    
+        <div id="featuredItemsContainer">
+            <div class="featuredItemsInnerContainer">
+              
+               <!--  <span class=" arrowLeft glyphicon glyphicon-chevron-left"></span> -->
+                
+                <div class="featuredItems"></div>
+                <div class="featuredItems"></div>
+                <div class="featuredItems"></div>
+                <div class="featuredItems"></div>
+                
+                <!-- <span class="arrowRight glyphicon glyphicon-chevron-right"></span> -->
+                
+            </div>
+           <!--  <div class="col-xs-12">
 
+
+            </div> -->
+
+        </div>
+
+     
+
+        <section id="features">
+
+            <div class="row">
+
+                <!-- Placeholder for featured items.-->
             </div>
 
-        </div>
+        </section>
 
-    </section>
-
-    <section id="features">
-
-        <div class="row">
-
-            <h3 class="section-title">Featured Items</h3>
-            <!-- Placeholder for featured items.-->
-        </div>
-
-    </section>
-
-</div>
+    </div>
+   </body>
--- views/partials/head.php
+++ views/partials/head.php
@@ -1,1 +1,10 @@
 <!--Partial for head.  Good place to include things like css files.-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Adlister</title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	 <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+	 <link href="https://fonts.googleapis.com/css?family=Muli|Open+Sans+Condensed:300" rel="stylesheet">
+	<link rel="stylesheet" href="./css/main.css">
+</head>
