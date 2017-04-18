610808754bb2c9b0e598283aacabdcbe0c8f5ba7
xconstaud <xconstaud@gmail.com>
2017-04-18 14:27:51 -0500

    Update PageController case

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -10,11 +10,33 @@ function pageController()
 
     // get the part of the request after the domain name
     $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    
 
     // switch that will run functions and setup variables dependent on what route was accessed
     switch ($request) {
         case '/' :
             $mainView = '../views/home.php';
+            break;
+        case '/create' :
+            $mainView = '../views/ads/create.php';
+            break;
+        case '/edit' :
+            $mainView = '../views/ads/edit.php';
+            break;
+        case '/show' :
+            $mainView = '../views/ads/show.php';
+            break;
+        case '/account' :
+            $mainView = '../views/users/account.php';
+            break;
+        case '/edit' :
+            $mainView = '../views/users/edit.php';
+            break;
+        case '/login' :
+            $mainView = '../views/users/login.php';
+            break;
+        case '/signup' :
+            $mainView = '../views/users/signup.php';
             break;
         default:    // displays 404 if route not specified above
             $mainView = '../views/404.php';
