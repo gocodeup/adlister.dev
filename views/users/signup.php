c43725d57f833b32b9e7db30dee384fe77671a53
Cameron Palatas <cameronlewispalatas@gmail.com>
2017-04-18 13:26:09 -0500

    Signup w/ bootstrap error messages

--- views/users/signup.php
+++ views/users/signup.php
@@ -92,37 +92,9 @@ if(!empty($_POST)) {
                     <div class="form-group">
                         <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <button type="submit" name="submit" class="btn btn-primary">Signup</button>
-
-                        </div>
-                        <?php unset($_SESSION['ERROR_MESSAGE']); ?>
-                    <?php endif; ?>
-                    <?php if (isset($_SESSION['SUCCESS_MESSAGE'])) : ?>
-                        <div class="alert alert-success">
-                            <p class="success"><?= $_SESSION['SUCCESS_MESSAGE']; ?></p>
-                        </div>
-                        <?php unset($_SESSION['SUCCESS_MESSAGE']); ?>
-                    <?php endif; ?>
-
-                    <form method="POST" action="" data-validation data-required-message="This field is required">
-
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Full Name" data-required>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Email" data-required>
-                        </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" id="username" name="username" placeholder="Username" data-required>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
-                        </div>
                         <div class="row">
                             <div class="col-sm-6">
-                                <button type="submit" class="btn btn-primary">Signup</button>
+                                <button type="submit" name="submit" class="btn btn-primary">Signup</button>
                             </div>
                             <div class="col-sm-6 text-right">
                                 <a href="/login" class="btn btn-success">Go To Login</a>
