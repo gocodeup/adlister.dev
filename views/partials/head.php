1fa444e381e7aa54af1f987cc35c9e959a9d59e1
Matt Valdez <mattvaldez01@gmail.com>
2017-04-18 11:12:44 -0500

    added bootstrap and font awesome

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,1 +1,3 @@
 <!--Partial for head.  Good place to include things like css files.-->
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
