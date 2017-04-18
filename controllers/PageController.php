0b72441ce863593e0cdeaba298d87c84c46bc1c1
John Watts <johnwatts@gmx.com>
2017-04-18 14:29:09 -0500

    Page controller updated so that navbar links work to access every page

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -13,7 +13,15 @@ function pageController()
 
     // switch that will run functions and setup variables dependent on what route was accessed
     switch ($request) {
-        case $request: $mainView = '../views/home.php';
+		case "/": $mainView = '../views/home.php';
+		break;
+		case "/index": $mainView = '../views/home.php';
+		break;
+        case "/items": $mainView = '../views/partials/items.php';
+        break;
+		case "/login": $mainView = '../views/users/login.php';
+        break;
+		case "/signup": $mainView = '../views/users/signup.php';
         break;
         default:    // displays 404 if route not specified above
             $mainView = '../views/404.php';
