4cf0a844aa60ebf9092daffc2e2e080cae2f6cd6
John Nichols <jtn0113@yahoo.com>
2017-04-18 14:31:07 -0500

    Updated Ads class

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../utils/helper_functions.php';
+require_once '../models/Ads.php';
 
 function pageController()
 {
@@ -13,8 +14,10 @@ function pageController()
 
     // switch that will run functions and setup variables dependent on what route was accessed
     switch ($request) {
-        case $request: $mainView = '../views/home.php';
-        break;
+        case '/ads': 
+            $data['ads'] = Ads::all();
+            $mainView = '../views/partials/items.php';
+            break;
         default:    // displays 404 if route not specified above
             $mainView = '../views/404.php';
             break;
@@ -26,3 +29,4 @@ function pageController()
 }
 
 extract(pageController());
+require $mainView;
