e7510dd18c369fc230556e731b61685667e04548
Valerie Burk <valerieburk@aim.com>
2017-04-18 14:51:21 -0500

    Adding updates

--- database/seeds/user_seeder.php
+++ database/seeds/user_seeder.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . '/../../models/User.php';
 
 $user = new User;
-$user->name = 'Finn Mertens';
-$user->email = 'finn@fansofbilly.com';
-$user->username = "finn_the_human";
+$user->name = 'Michael Scott';
+$user->email = 'MScott@dundermifflin.com';
+$user->username = "littleKidLover";
 $user->password = $_ENV['USER_PASS'];
 $user->save();
 
