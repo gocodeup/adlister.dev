0e50d701cb5184b762f16807f9a4fa352aa13352
Matt Valdez <mattvaldez01@gmail.com>
2017-04-18 12:04:20 -0500

    wrote out navbar (links not working yet)

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,1 +1,32 @@
-<!--partial view for navbar-->
+<!--***** links not working yet *******-->
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="#">Adlister</a>
+	</div>
+
+	<!-- Collect the nav links, forms, and other content for toggling -->
+	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	  <ul class="nav navbar-nav">
+		<li><a href="/public/index.php">Home</a></li>
+		<li><a href="../views/partials/items.php">Items</a></li>
+		<li><a href="../views/users/login.php">Login</a></li>
+		<li><a href="../views/users/signup.php">Signup</a></li>
+	  </ul>
+	  <form class="navbar-form navbar-right">
+		<div class="form-group">
+		  <input type="text" class="form-control" placeholder="Search">
+		</div>
+		<button type="submit" class="btn btn-default">Submit</button>
+	  </form>
+	</div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
