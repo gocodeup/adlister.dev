bbc479fb4f5742e0ea47478f947e6056a3ab0f86
xconstaud <xconstaud@gmail.com>
2017-04-19 13:12:28 -0500

    Add navbar, remove sidebar, update style, add local jquery and fonts

--- views/home.php
+++ views/home.php
@@ -7,28 +7,7 @@
 <div class="container-fluid">
     <section id="welcome">
         <div class="row">
-            <div class="col">
-                <div class="sideBar">
-                    <p>Categories</p>
-                    <div class="line"></div>
-                    <a href="#"><p>Stuff</p></a>
-                    <a href="#"><p>Stuff</p></a>
-                    <a href="#"><p>Stuff</p></a>
-                    <a href="#"><p>Stuff</p></a>
-                    <a href="#"><p>Stuff</p></a>
-                    <a href="#"><p>Stuff</p></a>
-                    <a href="#"><p>Stuff</p></a>
-                </div>
-            </div>
-            <div class="col">
-                <h1 class="text-center">Welcome To Adlister</h1>
-            </div>
-        </div>
-    </section>
-    <section id="features">
-        <div class="row">
-            <h3 class="section-title">Featured Items</h3>
-            <!-- Placeholder for featured items.-->
+            
         </div>
     </section>
 </div>
