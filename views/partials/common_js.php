e772b1e8144e63dff24e8a7903cf2b45db025649
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-19 11:54:11 -0500

    changes the way that invalid input is handled

--- views/partials/common_js.php
+++ views/partials/common_js.php
@@ -1,1 +1,19 @@
-<!--Partial view for common javascipt files-->
+<!DOCTYPE html>
+<html>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function() {
+        console.log($('.input-required'));
+        $('.input-required').on('invalid', function() {
+            this.setCustomValidity(' ');
+            $(this).css('background-color', '#FFA09C');
+        }).on('input', function() {
+            this.setCustomValidity('');
+            $(this).css('background-color', '');
+        });
+    });
+</script>
+</body>
+</html>
--- views/users/login.php
+++ views/users/login.php
@@ -1,38 +1,5 @@
-<?php
-
-// remove Auth::logout() to never show this page if logged in
-Auth::logout();
-
-if(isset($_SESSION['LOGGED_IN_ID'])) {
-    header("Location: /");
-}
-
-if(isset($_POST['submit'])) {
-    $authorize = new Auth();
-    $errors = [];
-
-    try {
-        $validated = $authorize::attempt($_POST['username'], $_POST['password']);
-    } catch (Exception $e) {
-        $errors['error'] = $e->getMessage();
-    }
-}
-
-
-?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Adlister Home</title>
-    <meta charset="utf-8">  
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway" rel="stylesheet">
-    <link rel="stylesheet" href="css/WebDEVPhillip.css">    
-</head>
 <body>
     <div class="container">
 
@@ -40,35 +7,33 @@ if(isset($_POST['submit'])) {
 
             <div class="row">
 
-                <h1 class="section-title"><center>Login To OooLister</center></h1>
+                <h1 class="section-title text-center">Login To OooLister</h1>
 
                 <div class="col-md-6 col-md-offset-3">
 
-                    <p style="text-align: center;">Login with your email/username and password</p>
+                    <p class="text-center">Login with your email/username and password</p>
 
-                    <form method="POST" action="" data-validation data-required-message="This field is required">
+                    <form method="POST" action="">
 
                         <div class="form-group">
-                        <?php
-                        if(isset($_POST['submit'])) {
-                            if(empty($errors)) {
-                                header("Location: /");
-                            } else {
-                                echo '<div class="alert alert-danger"><p class="error">' . $errors['error'] . '</p></div>';
-                            }
-                        }
-                        ?>
-                            <input type="text" class="form-control" id="email_user" name="username" placeholder="Username" data-required>
+                            <?php if (isset($_SESSION['ERROR_MESSAGE'])): ?>
+                                <div class="alert alert-danger">
+                                    <p class="error">
+                                        <?php echo $_SESSION['ERROR_MESSAGE']; ?>
+                                    </p>
+                                </div>
+                            <?php endif; ?>
+                            <input type="text" class="form-control input-required" id="email_user" name="username" placeholder="Username" required>
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
+                            <input type="password" class="form-control input-required" id="password" name="password" placeholder="Password" required>
                         </div>
                         <div class="row">
                             <div class="col-sm-6">
                                 <button type="submit" name="submit" class="btn btn-primary">Login</button>
                             </div>
                             <div class="col-sm-6 text-right">
-                                <a href="/signup" class="btn btn-success">Go To Signup</a>
+                                <a href="/signup/" class="btn btn-success">Go To Signup</a>
                             </div>
                         </div>
                     </form>
