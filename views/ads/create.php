aa594a3d98c21ae144795fe8d95496a961f1ee7c
xconstaud <xconstaud@gmail.com>
2017-04-19 14:38:14 -0500

    Style All pages

--- views/ads/create.php
+++ views/ads/create.php
@@ -1,15 +1,30 @@
 <!--Page for creating new advertisement listings-->
-<link rel="stylesheet" href="../css/items_styles.css">
-<div class="center" "offset-md-3">
-<div class="form-group" "create-form">
-    <input type="text" class="form-control" id="name" name="name" placeholder="Name" style="width: 20em" data-required>
-</div>
-<div class="form-group" "create-form">
-    <input type="text" class="form-control" id="email" name="email" placeholder="Price" style="width: 20em" data-required>
-</div>
-<div class="form-group">
-    <p>Description:</p>
-    <textarea cols="38" rows="10" name="comment"></textarea>
-</div>
-<button type="submit" class="btn btn-primary">Create</button>
+<div class="container">
+    <div class="row">
+        <div class="col-6 offset-3">
+            <form>
+                <div class="form-group row">
+                    <label for="example-text-input" class="col-2 col-form-label">Name:</label>
+                    <div class="col-10">
+                        <input class="form-control" type="text" id="productNameText">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="example-text-input" class="col-2 col-form-label">Price:</label>
+                    <div class="col-10">
+                        <input class="form-control" type="number" id="priceText">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="example-text-input" class="col-2 col-form-label">Description:</label>
+                    <div class="col-10">
+                        <textarea class="form-control" placeholder="Product Description Here" id="descriptionTextarea" rows="5"></textarea>
+                    </div>
+                </div>
+                <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+                <small id="fileHelp" class="form-text text-muted">Photos must be smaller than 3mb and in one of the following formats: PNG, JPG, JPEG</small>
+                <button type="submit" class="btn btn-success float-right">Submit</button>
+            </form>
+        </div>
+    </div>
 </div>
--- views/home.php
+++ views/home.php
@@ -7,7 +7,33 @@
 <div class="container-fluid">
     <section id="welcome">
         <div class="row">
-            
+            <div class="card" style="width: 20rem;">
+                <a href="#"><img class="card-img-top" src="https://images-cdn.9gag.com/photo/aGwd2jn_700b.jpg" alt="Card image cap" style="width: 100%"></a>
+                <div class="card-block">
+                    <a href="#"><h4 class="card-title">Card title</h4></a>
+                    <p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews.
+             Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.
+             Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+                </div>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <a href="#"><img class="card-img-top" src="https://images-cdn.9gag.com/photo/aGwd2jn_700b.jpg" alt="Card image cap" style="width: 100%"></a>
+                <div class="card-block">
+                    <a href="#"><h4 class="card-title">Card title</h4></a>
+                    <p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews.
+             Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.
+             Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+                </div>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <a href="#"><img class="card-img-top" src="https://images-cdn.9gag.com/photo/aGwd2jn_700b.jpg" alt="Card image cap" style="width: 100%"></a>
+                <div class="card-block">
+                    <a href="#"><h4 class="card-title">Card title</h4></a>
+                    <p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews.
+             Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.
+             Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+                </div>
+            </div>
         </div>
     </section>
 </div>
