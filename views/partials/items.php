63d410fb394e4d7e6c832c9fd2334714a6f3239a
Matt Valdez <mattvaldez01@gmail.com>
2017-04-19 10:38:04 -0500

    made href go to the show page

--- views/partials/items.php
+++ views/partials/items.php
@@ -24,7 +24,7 @@ require_once __DIR__ . '/../../controllers/PageController.php';
 			<?php foreach($adListings as $ad) { ?>
 				<div class="col-sm-4">
 					<div class="panel panel-primary">
-						<div class="panel-heading"><a href="#" style="color: white"><?= $ad->title ?><div class="pull-right"><?= "$" . $ad->price ?></div></a></div>
+						<div class="panel-heading"><a href="/show" style="color: white"><?= $ad->title ?><div class="pull-right"><?= "$" . $ad->price ?></div></a></div>
 						<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>
 						<div class="panel-footer"><?= $ad->description ?></div>
 					</div>
