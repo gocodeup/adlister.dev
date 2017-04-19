5ad2dd40e3f2e491860f3e225206979649bcb4d4
xconstaud <xconstaud@gmail.com>
2017-04-19 10:22:49 -0500

    Update Homepage styling, add sidebar and cover

--- views/home.php
+++ views/home.php
@@ -1,7 +1,25 @@
+<div class="cover">
+    <div id="jobsText">
+        <h1 id="">Shop like Jobs... Steve Jobs</h1>
+    </div>
+</div>
+<div class="grayBox">Find your stuff in the United States</div>
 <div class="container-fluid">
-    
     <section id="welcome">
         <div class="row">
+            <div class="col">
+                <div class="sideBar">
+                    <p>Categories</p>
+                    <div class="line"></div>
+                    <a href="#"><p>Stuff</p></a>
+                    <a href="#"><p>Stuff</p></a>
+                    <a href="#"><p>Stuff</p></a>
+                    <a href="#"><p>Stuff</p></a>
+                    <a href="#"><p>Stuff</p></a>
+                    <a href="#"><p>Stuff</p></a>
+                    <a href="#"><p>Stuff</p></a>
+                </div>
+            </div>
             <div class="col">
                 <h1 class="text-center">Welcome To Adlister</h1>
             </div>
