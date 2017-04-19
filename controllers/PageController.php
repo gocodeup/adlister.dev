1f5a8ed7ad0dc49e67a9ab31411067e8727b4c5b
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-19 11:08:07 -0500

    added login functionality

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -15,65 +15,71 @@ function pageController()
 
     // switch that will run functions and setup variables dependent on what route was accessed
     switch ($request) {
-        // TODO: put routes here
+        
         case '/':
             $mainView = '../views/home.php';
             break;
+
         case '/login': 
             $mainView = '../views/users/login.php';
-
-            /////////////////////////////////
-            //       Login Section         //
-            /////////////////////////////////
             $data['message'] = "";
             
-
-            // check if the form was submitted
-            if (Input::has('email_user') && Input::has('password')) {
-                Auth::attempt(Input::get('email_user'), Input::get('password'));
-                $data['message'] = "Either username or password were incorrect";
-            } else if (Auth::check()) {
-                header('Location: /');
-                exit;
-            } 
+            if(!empty($_POST)) {
+                if (Input::has('email_user') && Input::has('password')) {
+                    Auth::attempt(Input::get('email_user'), Input::get('password'));
+                    $data['message'] = "Either username/email or password were incorrect";
+                } else if (Auth::check()) {
+                    header("Location: /");
+                    exit;
+                }   
+            }
             break;
+
         case '/logout':
             Auth::logout();
             $mainView = '/';
             break;
+
         case '/signup':
             $mainView = '../views/users/signup.php';
             break;
+
         case '/account':
             $mainView = '../views/users/account.php';
             break;
+
         case '/account/edit':
             $mainView = '../views/users/edit.php';
             if(!Auth::check()) {
-                header('Location: /login.php');
+                header('Location: /login');
                 exit;
             }
             break;
+
         case '/ads':
             $mainView = '../views/ads/index.php';
             break;
+
         case '/ads?id={n}':
             $mainView = '../views/ads/show.php';
             break;
+
         case '/ads/create':
             $mainView = '../views/ads/create.php';
             if(!Auth::check()) {
-                header('Location: /login.php');
+                header('Location: /login');
                 exit;
             }
             break;
+
         case '/ads/edit?id={n}':
             $mainView = '../views/ads/edit.php';
             if(!Auth::check()) {
-                header('Location: /login.php');
+                header('Location: /login');
                 exit;
             }   
             break;
+
         default:    // displays 404 if route not specified above
             $mainView = '../views/404.php';
             break;
