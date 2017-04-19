0d82b3dfcad421a207b1f9957bc495891e0720ef
Valerie Burk <valerieburk@aim.com>
2017-04-19 09:04:21 -0500

    adding yesterday's stuff

--- database/migrations/shoes_migration.php
+++ database/migrations/shoes_migration.php
@@ -5,23 +5,24 @@ require_once '../db_connect.php';
 
 $dbc->exec('DROP TABLE IF EXISTS shoes');
 
-$query = 'CREATE TABLE shoes (
-    shoe_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    title VARCHAR(240) NOT NULL,
-    price FLOAT NOT NULL,
-    msrp FLOAT NOT NULL,
-    color VARCHAR(255) NOT NULL,
-    style VARCHAR(240) NOT NULL,
+$query = "CREATE TABLE shoes (
+    shoe_id INT UNSIGNED AUTO_INCREMENT,
+    title VARCHAR(240) DEFAULT 'UNKNOWN',
+    price DECIMAL(4,2) NOT NULL,
+    msrp DECIMAL(4,2) NOT NULL,
+    color VARCHAR(255) DEFAULT 'UNKNOWN',
+    style VARCHAR(240) DEFAULT 'UNKNOWN',
     size FLOAT NOT NULL,
-    location VARCHAR(240) NOT NULL,
-    brand VARCHAR(240) NOT NULL,
-    shoe_condition VARCHAR(240) NOT NULL,
-    contact_name VARCHAR(240) NOT NULL,
+    location VARCHAR(240) DEFAULT 'UNKNOWN',
+    category VARCHAR(240) DEFAULT 'UNKNOWN',
+    brand VARCHAR(240) DEFAULT 'UNKNOWN',
+    shoe_condition VARCHAR(240) DEFAULT 'UNKNOWN',
+    contact_name VARCHAR(240) DEFAULT 'UNKNOWN',
     contact_number VARCHAR(240) NOT NULL,
-    image_location VARCHAR(240) NOT NULL,
+    image_location VARCHAR(240) DEFAULT 'UNKNOWN',
     user_id INT UNSIGNED NOT NULL,
     PRIMARY KEY (shoe_id),
     FOREIGN KEY (user_id) REFERENCES users (id)
-)';
+)";
 
 $dbc->exec($query);
