15dee7287eeea3e733303f76636ba3f0fc127960
Connor <Csumners17@gmail.com>
2017-04-19 11:40:29 -0500

    Add single ad page, more styling

--- views/ads/show.php
+++ views/ads/show.php
@@ -1,1 +1,24 @@
 <!--Page for single advertisement -->
+<link rel="stylesheet" href="/../css/items_styles.css">
+<div class="row">
+    <div class="col-sm-8 offset-sm-0 col-md-8">
+      <a href="#"><img class="card-img-single" src="https://images-cdn.9gag.com/photo/aGwd2jn_700b.jpg" alt="Card image cap" style="width: 50%"></a>
+      <div class="card-block">
+        <a href="#"><h4 class="card-title">Title</h4></a>
+        <p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews.
+             Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.
+             Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+        <button class="btn btn-primary" type="submit">Edit Add</button>
+      </div>
+</div>
+<div class="class-sm-4 offset-sm-0 col-md-4" id="single-item">
+<div>
+     <p>Leverage agile frameworks to provide a robust synopsis for high level overviews.
+        Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.
+        Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+        <a href="#">Posted by Reagan</a>
+        <div class="w-100"></div>
+        <button class="btn btn-primary" id="single-button" type="submit">Delete Add</button>
+    </div>
+</div>
+</div>
