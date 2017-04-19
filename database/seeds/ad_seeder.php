181b32144f329178558ebe9a135c007d2b26f607
Todd Hendricks <hendricks.ta@gmail.com>
2017-04-19 15:19:23 -0500

    add image to migration and seeder files

--- database/seeds/ad_seeder.php
+++ database/seeds/ad_seeder.php
@@ -6,24 +6,28 @@ $ads = new Ad;
 $ads->name = 'Finn Mertens';
 $ads->description = 'product description';
 $ads->user_id = 1;
+$ads->image = '/../../public/images/pic08.jpg';
 $ads->save();
 
 $ads = new Ad;
 $ads->name = 'Jake';
 $ads->description = 'product description';
 $ads->user_id = 2;
+$ads->image = '/../../public/images/pic08.jpg'; 
 $ads->save();
 
 $ads = new Ad;
 $ads->name = 'Simon Petrikov';
 $ads->description = 'product description';
 $ads->user_id = 3;
+$ads->image = '/../../public/images/pic08.jpg';
 $ads->save();
 
 $ads = new Ad;
 $ads->name = 'Marceline Abadeer';
 $ads->description = 'product description';
 $ads->user_id = 4;
+$ads->image = '/../../public/images/pic08.jpg';
 $ads->save();
 	 
  ?>
