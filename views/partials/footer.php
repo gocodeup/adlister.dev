982fdd85927bdd354b8855382720f0ff07e34888
Matt Valdez <mattvaldez01@gmail.com>
2017-04-20 18:43:29 -0500

    added footer; need to style

---
+++ views/partials/footer.php
@@ -0,0 +1,11 @@
+<footer class="text-center">
+	<div class="footer-below">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					Copyright &copy; Adlister 2017
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
