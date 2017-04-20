af53d63787d1a667a52c994302f2dd0748f5e907
Matt Valdez <mattvaldez01@gmail.com>
2017-04-20 17:41:27 -0500

    added link to main.css

--- views/partials/head.php
+++ views/partials/head.php
@@ -6,4 +6,4 @@
 
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 <link href="css/font-awesome.min.css" rel="stylesheet">
-<link href="" rel="stylesheet" type="text/css">
+<link href="css/main.css" rel="stylesheet" type="text/css">
