e98856965cd2364e9e84019369c171619d1fe70a
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-20 12:12:20 -0500

    cleans up and sets up to work with new features in common_js.php

--- views/users/login.php
+++ views/users/login.php
@@ -1,45 +1,40 @@
-<!DOCTYPE html>
-<html>
-<body>
-    <div class="container">
+<div class="container">
 
-        <section id="login">
+    <section id="login">
 
-            <div class="row">
+        <div class="row">
 
-                <h1 class="section-title text-center">Login To OooLister</h1>
+            <h1 class="section-title text-center">Log In To OooLister</h1>
 
-                <div class="col-md-6 col-md-offset-3">
+            <div class="col-md-6 col-md-offset-3">
 
-                    <p class="text-center">Login with your email/username and password</p>
+                <p class="text-center">Log in with your email/username and password</p>
 
-                    <form method="POST" action="">
+                <form method="POST" action="">
 
-                        <div class="form-group">
-                            <?php if (isset($_SESSION['ERROR_MESSAGE'])): ?>
-                                <div class="alert alert-danger">
-                                    <p class="error">
-                                        <?php echo $_SESSION['ERROR_MESSAGE']; ?>
-                                    </p>
-                                </div>
-                            <?php endif; ?>
-                            <input type="text" class="form-control input-required" id="email_user" name="username" placeholder="Username" required>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control input-required" id="password" name="password" placeholder="Password" required>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <button type="submit" name="submit" class="btn btn-primary">Login</button>
+                    <div class="form-group">
+                        <?php if (isset($_SESSION['ERROR_MESSAGE'])): ?>
+                            <div class="alert alert-danger">
+                                <p class="error">
+                                    <?php echo $_SESSION['ERROR_MESSAGE']; ?>
+                                </p>
                             </div>
-                            <div class="col-sm-6 text-right">
-                                <a href="/signup/" class="btn btn-success">Go To Signup</a>
-                            </div>
+                        <?php endif; ?>
+                        <input type="text" class="form-control input-required" id="email_user" name="username" placeholder="Username or Email" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control input-required" id="password" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <button type="submit" class="btn btn-primary">Log In</button>
                         </div>
-                    </form>
-                </div>
+                        <div class="col-sm-6 text-right">
+                            <a href="/signup/" class="btn btn-success">Sign Up</a>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </section>
-    </div>
-</body>
-</html>
+        </div>
+    </section>
+</div>
