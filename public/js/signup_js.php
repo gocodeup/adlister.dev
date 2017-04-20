828b032550dd4027001cef22d7cfa4360fa4943e
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-20 13:48:31 -0500

    updates custom validation

--- public/js/signup_js.php
+++ public/js/signup_js.php
@@ -1,44 +1,45 @@
 <script>
     $(document).ready(function() {
-        // getting the minimum length for username and password from the php
         var $username = $('#username'),
             $password = $('#password'),
             $passwordConfirm = $('#password-confirm');
 
+        function isLongEnough(input) {
+            return input.val() && input.val().length >= input.attr('data-min-length');
+        }
 
 
         $username.change(function() {
-            // check if username is too short
-            if ($username.val() && $username.val().length < $username.attr('data-min-length')) {
+            if (!isLongEnough($username)) {
                 $username
-                    .css('background-color', '#F2BB6E')
-                    .get(0)
+                .css('background-color', '#F2BB6E')
+                .get(0)
                     .setCustomValidity(' ');
+                .end();
             }
         });
 
         $password.change(function() {
-            // check if password is too short
-            if ($password.val() && $password.val().length < $password.attr('data-min-length')) {
+            if (!isLongEnough($password)) {
                 $password
                     .css('background-color', '#F2BB6E')
                     .get(0)
-                    .setCustomValidity(' ');
+                        .setCustomValidity(' ')
+                    .end();
             }
         }).off('input')
+            // overriding the default input event set in common_js.php
         .on('input', function() {
             $passwordConfirm
                 .val('')
                 .css('background-color', '')
                 .get(0)
-                .setCustomValidity('');
+                    .setCustomValidity('')
+                .end();
 
             // if it's already invalid because the password's too short, don't mess with it
-            if ($password.is(':invalid')
-                && $password.val()
-                && $password.val().length < $password.attr('data-min-length')
-                ) {
-                // the only time this should change anything is when the background color is red from trying to submit too short a password
+            if ($password.is(':invalid') && $password.val() && $password.val().length < $password.attr('data-min-length')) {
+                // except make the background color orange (which should only make a difference when it's already red)
                 $password.css('background-color', '#F2BB6E');
                 return;
             }
@@ -47,39 +48,64 @@
             $password
                 .css('background-color', '')
                 .get(0)
-                .setCustomValidity('');
+                    .setCustomValidity('')
+                .end();
         });
 
         $passwordConfirm.change(function() {
+            // the most important part of this is the else if
             if (!$password.val() || $password.is(':invalid')) {
+                // although in most cases redundant, this is here for reassurement's sake
                 $passwordConfirm.val('');
-            } else if ($password.val() >= $password.attr('data-min-length')
-                && $passwordConfirm.val()
-                && $passwordConfirm.val() !== $password.val()
-                ) {
+                return;
+            }
+
+            // if the values don't match up
+            if ($passwordConfirm.val() && $passwordConfirm.val() !== $password.val()) {
                 $passwordConfirm
                     .css('background-color', '#F2BB6E')
-                    .get(0).setCustomValidity(' ');
+                    .get(0)
+                        .setCustomValidity(' ')
+                    .end();
             }
         }).keypress(function(e) {
+            // no need for the user to input anything if password is empty/invalid, so why let them?
             if (!$password.val() || $password.is(':invalid')) {
                 e.preventDefault();
+                // in fact, tell that user to fix that one first
+                $password
+                    .get(0)
+                        .focus();
+                // and if it's completely empty, do a little of the work for them
+                if (!$password.val()) {
+                    $password.trigger($.Event('input'));
+                }
             }
         }).off('input')
+            // overriding the default input event set in common_js.php
         .on('input', function() {
             var bgColor = '';
 
-            // if password is empty
+            // reset validity
+            $passwordConfirm
+                .get(0)
+                    .setCustomValidity('')
+                .end();
+
+            // this is here in case someone copies and pastes, as the previous event won't fire in that situation
             if (!$password.val() || $password.is(':invalid')) {
                 $passwordConfirm.val('');
                 return;
             }
 
-            if ($(this).val() === $('#password').val()) {
-                bgColor = 'green';
+            // we get here and they match up...
+            if ($passwordConfirm.val() === $password.val()) {
+                bgColor = '#79C479';
             }
-            $('#password').css('background-color', bgColor);
-            $(this).css('background-color', bgColor);
+
+            $password.css('background-color', bgColor);
+
+            $passwordConfirm.css('background-color', bgColor);
         });
     });
 </script>
