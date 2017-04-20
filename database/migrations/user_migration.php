534429ca5357bfd2bf0ad0f78a77d2344f99f4d6
Cameron Palatas <cameronlewispalatas@gmail.com>
2017-04-20 17:09:17 -0500

    Updated Migration file

--- database/migrations/user_migration.php
+++ database/migrations/user_migration.php
@@ -6,7 +6,7 @@ require_once '../db_connect.php';
 $dbc->exec('DROP TABLE IF EXISTS users');
 
 $query = <<<SQL
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
@@ -16,4 +16,21 @@ CREATE TABLE users (
 );
 SQL;
 
+$query2 = <<<SQL
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `product_name` varchar(70) NOT NULL,
+  `category` varchar(70) NOT NULL,
+  `description` varchar(600) NOT NULL,
+  `price` int(10) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+SQL;
+
 $dbc->exec($query);
+$dbc->exec($query2);
