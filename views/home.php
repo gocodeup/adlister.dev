bf87f5e7f3b22d11f540f16ce40d12b0034caa9b
Matt Valdez <mattvaldez01@gmail.com>
2017-04-20 16:09:14 -0500

    made images link to the show page

--- views/home.php
+++ views/home.php
@@ -30,8 +30,8 @@
             <?php foreach($selectThree as $ad) { ?>
                 <div class="col-sm-4">
                     <div class="panel panel-primary">
-                        <div class="panel-heading"><a href="/show?id=<?= $ad->id ?>" style="color: white"><?= $ad->title ?><div class="pull-right"><?= "$" . $ad->price ?></div></a></div>
-                        <div class="panel-body"><img src=<?= "$ad->image" ?> class="img-responsive" style="width:100%; height:250px" alt="Image"></div>
+                        <div class="panel-heading"><a href="/show?id=<?= $ad->id ?>" style="color: white"><?= $ad->title ?><div class="pull-right"><?= "$" . $ad->price ?></div></div>
+                        <div class="panel-body"><img src=<?= "$ad->image" ?> class="img-responsive" style="width:100%; height:250px" alt="Image"></div></a>
                         <div class="panel-footer"><?= $ad->description ?></div>
                     </div>
                 </div>
