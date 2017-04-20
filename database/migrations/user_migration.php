658e0f6b610db5b47cc3662403d3840f0ac6925c
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-20 12:13:10 -0500

    minor readability modifications

--- database/migrations/user_migration.php
+++ database/migrations/user_migration.php
@@ -5,13 +5,15 @@ require_once '../db_connect.php';
 
 $dbc->exec('DROP TABLE IF EXISTS users');
 
-$query = 'CREATE TABLE users (
+$query = <<<SQL
+CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(240) NOT NULL,
     email VARCHAR(240) NOT NULL,
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-)';
+);
+SQL;
 
 $dbc->exec($query);
