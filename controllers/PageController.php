973a370f68aa3427089e430c00c57aa184a0d11c
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-20 12:05:26 -0500

    adds/ports over several controller functions for various pages

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -1,42 +1,41 @@
 <?php
 
-require_once __DIR__ . '/../utils/helper_functions.php';
-// // $id = "SELECT `id` FROM `users` WHERE `username` = '" $loggedInUsername . "'";
-
-// // $postID = "SELECT p.post_id from posts p
-// --             INNER JOIN users u ON u.id = p.post_userid
-// --             WHERE `p.post_userid` = '" . $id;
+require_once '../utils/helper_functions.php';
 
 function parseRequest($request)
 {
-    $matches = [];
+    $parts = [];
 
-    switch (preg_match('#(?i:(?<uri>^.*(?:\/(?!\d*(?:\W*|\W{2,}.*)$)\w+)+\/?))(?:\b(?<id>\d+)\b)?#', $request, $matches)) {
-        case '0':
-            $matches['uri'] = $request;
-        case '1':
-            $matches['id'] = isset($matches['id']) ? '?id=' . $matches['id'] : '';
-            break;
-        case false:
+    if (preg_match('#(?i:(?<uri>^.*(?:\/(?!\d*(?:\W*|\W{2,}.*)$)\w+)+\/?))\b(?<pageId>\d+\b)?#', $request, $parts) === false) {
             throw new Exception('Error processing request.');
-            break;
     }
 
-    $matches['uri'] = preg_replace('#([^\/])$#', '$1/', $matches['uri']);
+    if (!isset($parts['uri'])) {
+        $parts['uri'] = $request;
+    }
 
-    $requestTest = preg_replace('#([^\/])$#', '$1/', $matches['uri'] . $matches['id']);
+    if (!isset($parts['pageId'])) {
+        $parts['pageId'] = NULL;
+    }
+
+    $parts['uri'] = preg_replace('#([^\/])$#', '$1/', $parts['uri']);
+
+    $requestTest = preg_replace('#([^\/])$#', '$1/', $parts['uri'] . $parts['pageId']);
 
     if ($requestTest !== $request) {
         header("Location: $requestTest");
+        die;
     }
 
-    return $matches;
+    return $parts;
 }
 
 function pageController()
 {
     // defines array to be returned and extracted for view
     $data = [];
+
+    $data['requiredJS'] = [];
 
     // get the part of the request after the domain name
     $request = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -49,7 +48,11 @@ function pageController()
             $mainView = '../views/home.php';
             break;
         case '/ads/':
-            $mainView = '../views/ads/index.php';
+            if (isset($pageId)) {
+                $mainView = '../views/ads/show.php';
+            } else {
+                $mainView = '../views/ads/index.php';
+            }
             break;
         case '/ads/create/':
             $mainView = '../views/ads/create.php';
@@ -57,35 +60,84 @@ function pageController()
         case '/ads/edit/':
             $mainView = '../views/ads/edit.php';
             break;
-        case '/user':
-            header('Location: /user/');
-            break;
         case '/user/':
-            if (empty($id)) {
-                $id = isset($_SESSION['logged_in_user_id']) ? $_SESSION['logged_in_user_id'] : '?id=0';
+            if (!isset($pageId)) {
+                if (isset($_SESSION['LOGGED_IN_ID'])) {
+                    header("Location: /user/{$_SESSION['LOGGED_IN_ID']}/");
+                    die;
+                } else {
+                    // TODO: display "user not found" page
+                    break;
+                }
             }
+
+            $data['user'] = User::find($pageId);
+
+            if (is_null($data['user'])) {
+                // TODO: display "user not found" page
+                break;
+            }
+
             $mainView = '../views/users/account.php';
+
             break;
-        case '/user/edit':
-            if (isset($_SESSION['logged_in_user_id'])) {
-                $mainView = '../views/users/edit.php' .  $_SESSION['logged_in_user_id'];
-            } else {
-                $mainView = '';    // need to display "not logged in" page
+        case '/user/edit/':
+            if (!isset($_SESSION['LOGGED_IN_ID'])) {
+                // TODO: display "not logged in" page
+                break;
             }
+
+            if (!isset($pageId)) {
+                header("Location: /user/{$_SESSION['LOGGED_IN_ID']}/");
+                die;
+            }
+
+            if ($_SESSION['LOGGED_IN_ID'] !== $pageId and $_SESSION['LOGGED_IN_ID'] !== 'ADMIN ID HERE') {
+                // TODO: display "access denied" page
+                break;
+            }
+
+            $data['user'] = User::find($pageId);
+
+            $mainView = '../views/users/edit.php';
+        case '/login/':
+            if (isset($_SESSION['LOGGED_IN_ID'])) {
+                header('Location: /');
+                die;
+            }
+
+            $mainView = '../views/users/login.php';
+            $username = Input::get('username');
+            $password = Input::get('password');
+            
+            if (!empty($_POST) and Auth::attempt($username, $password)) {
+                header('Location: /');
+                die;
+            }
+
             break;
-        case '/login':
-            if (!isset($_SESSION['logged_in_user_id'])) {
-                $mainView = '../views/users/login.php';
-            } else {
-                header('adlister.dev');
+        case '/signup/':
+            if (isset($_SESSION['LOGGED_IN_ID'])) {
+                header('Location: /');
+                die;
             }
-            break;
-        case '/signup':
-            if (!isset($_SESSION['logged_in_user_id'])) {
-                $mainView = '../views/users/signup.php';
-            } else {
-                header('adlister.dev');
+
+            $newUser = new User();
+            $newUser->name = Input::get('name');
+            $newUser->email = Input::get('email');
+            $newUser->username = Input::get('username');
+            $newUser->password = Input::get('password');
+
+            if ($newUser->save()) {
+                unset($_SESSION['ERROR_MESSAGES']);
+                header('Location: /login/');    // TODO: change this to a page displaying a simple message that the account was created successfully
+                die;
             }
+
+            $mainView = '../views/users/signup.php';
+
+            $data['requiredJS'][] = './js/signup_js.php';
+
             break;
         default:    // displays 404 if route not specified above
             $mainView = '../views/404.php';
@@ -93,8 +145,6 @@ function pageController()
     }
 
     $data['mainView'] = $mainView;
-
-    $data['query'] = $id;
 
     return $data;
 }
