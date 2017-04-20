70b684ba8f6bc08e47c5c8417b86a2664d59d914
Matt Valdez <mattvaldez01@gmail.com>
2017-04-20 16:11:43 -0500

    edit ad now pops up dialog box instead of separate page

--- views/ads/show.php
+++ views/ads/show.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-sm-4">
 			<div class="panel panel-primary">
-					<div class="panel-heading"><a href="/show?id=<?= $showItem->id ?>" style="color: white"><?= $showItem->title ?><div class="pull-right"><?= "$" . $showItem->price ?></div></a></div>
+					<div class="panel-heading"><?= $showItem->title ?><div class="pull-right"><?= "$" . $showItem->price ?></div></div>
 					<div class="panel-body"><img src=<?= "$showItem->image" ?> class="img-responsive" style="width:100%; height:250px" alt="Image"></div>
 					<div class="panel-footer"><?= $showItem->description ?></div>
 				<div>Posted by <?= $user->username ?></div> 
@@ -13,7 +13,7 @@
 	</div><!-- ends row -->
 </div><!-- ends container -->
 <?php if (Auth::id() == $showItem->user_id): ?>
-	<a style="margin:5%" href="/edit" class="btn btn-primary">Edit Ad</a>
+	<a style="margin:5%" href="" class="btn btn-primary" data-toggle="modal" data-target="#editAd">Edit Ad</a>
 	<a style="margin:5%" class="btn btn-primary pull-right" href="" data-toggle="modal" data-target="#deleteAd">Delete Ad</a>
 <?php endif; ?>
 
@@ -55,6 +55,57 @@
 						</div>
 						<div class="modal-footer">
 							<a href="/delete" type="submit" class="btn btn-default btn-success">Confirm Delete</a>
+							<button type="submit" data-dismiss="modal" class="btn btn-default btn-danger">Cancel</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+		<!-- Editing Ad Modal -->
+	<div id="editAd" class="modal fade" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Edit Ad</h4>
+				</div>
+				<div class="modal-body">
+					<form  method="POST">
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Title: </label>
+								<input value="<?=$showItem->title?>" type="text" name="">
+								
+							</div>
+						</div>
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Price: </label>
+								<input value="<?="$".$showItem->price?>" type="text" name="">
+								
+							</div>
+						</div>
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Location: </label>
+								<input value="<?=$showItem->location?>" type="text" name="">
+								
+							</div>
+						</div>
+						<div class="row control-group">
+							<div class="form-group col-xs-12 floating-label-form-group controls">
+								<label>Description: </label>
+								<textarea rows="4" class="form-control"><?=$showItem->description?></textarea>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<a href="/delete" type="submit" class="btn btn-default btn-success">Update Ad</a>
 							<button type="submit" data-dismiss="modal" class="btn btn-default btn-danger">Cancel</button>
 						</div>
 					</form>
