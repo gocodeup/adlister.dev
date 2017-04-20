e72ddb6bdff4817a543c4f69915bb569c7bc3e4a
Matt Valdez <mattvaldez01@gmail.com>
2017-04-20 13:17:13 -0500

    started on account page

--- views/users/account.php
+++ views/users/account.php
@@ -1,1 +1,16 @@
 <!--Page for user account home-->
+<div>
+	<h2>User Info</h2>
+	<div>
+	
+	</div>
+	<a href="/update" class="btn btn-primary">Update Account</a>
+</div>
+
+<div>
+	<h2>Your Ads</h2>
+	<div>
+	
+	</div>
+	<a href="/create" class="btn btn-primary">Create New Ad</a>
+</div>
