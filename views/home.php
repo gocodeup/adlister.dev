27c44be70ea525526562fe604db4b087e9e6c0db
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-04-20 12:09:29 -0500

    minor cleanup

--- views/home.php
+++ views/home.php
@@ -1,53 +1,45 @@
-<?php
-
-
-?>
-
 <body>
-        <?php if(isset($_SESSION['LOGGED_IN_ID'])) {
-            echo "Hello " . ucwords(Auth::user()->name);
-        } else {
-            echo "Hello guest!";
-        }
-        ?>
-        <div id="featuredItemsContainer">
-            <center><h2 class="section-title">Check Out Our Featured Items</h2></center>
-            <div class="featuredItemsInnerContainer">
-                <div class="featuredItems"></div>
-                <div class="featuredItems"></div>
-                <div class="featuredItems"></div>
-                <div class="featuredItems"></div>
-            </div>
+    <?php echo 'Hello '; ?>
+    <?php if (isset($_SESSION['LOGGED_IN_ID'])): ?>
+        <?php echo Auth::user()->name; ?>
+    <?php else: ?>
+        <?php echo 'guest'; ?>
+    <?php endif; ?>
+    <?php echo '!'; ?>
+    <div id="featuredItemsContainer">
+        <h2 class="section-title text-center">Check Out Our Featured Items</h2>
+        <div class="featuredItemsInnerContainer">
+            <div class="featuredItems"></div>
+            <div class="featuredItems"></div>
+            <div class="featuredItems"></div>
+            <div class="featuredItems"></div>
         </div>
-
-        <div class="recentlyAdded">
-            <h1 class="section-title latestPosts">Latest Posts</h1>
-            <hr>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-            <div class="recentlyAddedCategories"></div>
-        </div>
-        <div class="adsContainer">
-            <div id="ads">Insert Add Here</div>
-            <div id="ads">Insert Add Here</div>
-        </div>
-
-        <section id="features">
-
-            <div class="row">
-
-                <!-- Placeholder for featured items.-->
-            </div>
-
-        </section>
-
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 
+    <div class="recentlyAdded">
+        <h1 class="section-title latestPosts">Latest Posts</h1>
+        <hr>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+        <div class="recentlyAddedCategories"></div>
+    </div>
+    <div class="adsContainer">
+        <div id="ads">Insert Add Here</div>
+        <div id="ads">Insert Add Here</div>
+    </div>
 
-   </body>
+    <section id="features">
+
+        <div class="row">
+
+            <!-- Placeholder for featured items.-->
+        </div>
+
+    </section>
+
+</body>
