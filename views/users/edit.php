eeb171bff31330a16398c24f4909b73babf2034a
Matt Valdez <mattvaldez01@gmail.com>
2017-04-20 13:18:07 -0500

    started on update account page and edit ad page

--- views/users/edit.php
+++ views/users/edit.php
@@ -4,7 +4,7 @@
 
         <div class="row">
 
-            <h1 class="section-title">Edit Account</h1>
+            <h1 class="section-title">Updating Account</h1>
 
             <div class="col-md-6 col-md-offset-3">
 
@@ -25,13 +25,16 @@
                 <form method="POST" action="" data-validation data-required-message="This field is required">
 
                     <div class="form-group">
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Full Name" value="<?= $user->name; ?>" data-required>
+                        <label>Full Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Full Name" value="<?= Auth::user()->name; ?>" data-required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?= $user->email; ?>" data-required>
+                        <label>Email</label>
+                        <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?= Auth::user()->email; ?>" data-required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" value="<?= $user->username; ?>" data-required>
+                        <label>Username</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username" value="<?= Auth::user()->username; ?>" data-required>
                     </div>
                     <button type="submit" class="btn btn-primary">Update Account</button>
 
