5110dfdb60dc7e31e4986985b49d12b6f2a585a4
xconstaud <xconstaud@gmail.com>
2017-04-20 11:17:21 -0500

    Add Ads model and modify footer

---
+++ models/Ads.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__ . '/Model.php';
+
+class Ads extends Model {
+    protected static $table = 'ads';
+
+    public static function count() {
+        self::dbConnect();
+        $query = self::$dbc->query("SELECT COUNT(*) FROM $table")->fetchColumn();
+
+        return $query;
+    }
+
+    public static function paginate($pageNo, $resultsPerPage = 10) {
+        self::dbConnect();
+        $limit = $resultsPerPage;
+        $offset = ($pageNo * $limit) - $limit;
+
+        $stmt = self::$dbc->prepare("SELECT * FROM $table LIMIT :limit OFFSET :offset");
+        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return($results);
+    }
+
+}
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,5 @@
-<footer class="footer">
-    <div class="container">
+<footer class="footer navbar-fixed-bottom">
+    <div class="container footerContainer">
         <span class="text-muted">©2017 Will, Connor and the guy who is never here</span>
     </div>
 </footer>
