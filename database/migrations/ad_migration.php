2b31167c11254aa012e30e65d3bbb5aab583c135
Todd Hendricks <hendricks.ta@gmail.com>
2017-04-20 11:41:47 -0500

    breaks up migration tables

---
+++ database/migrations/ad_migration.php
@@ -0,0 +1,20 @@
+<?php 
+
+$_ENV = include __DIR__ . '/../../.env.php';
+require_once '../db_connect.php';
+
+$dbc->exec('TRUNCTATE TABLE IF EXISTS ads');
+
+$ads = 'CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(240) NOT NULL,
+    description VARCHAR (1000) NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    image VARCHAR (255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)';
+
+$dbc->exec($ads);
+
+ ?>
--- database/migrations/user_migration.php
+++ database/migrations/user_migration.php
@@ -3,9 +3,7 @@
 $_ENV = include __DIR__ . '/../../.env.php';
 require_once '../db_connect.php';
 
-$dbc->exec('DROP TABLE IF EXISTS ads');
-$dbc->exec('DROP TABLE IF EXISTS users');
-$dbc->exec('DROP TABLE IF EXISTS favorites');
+$dbc->exec('TRUNCATE TABLE IF EXISTS users');
 
 $query = 'CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -16,26 +14,5 @@ $query = 'CREATE TABLE IF NOT EXISTS users (
     PRIMARY KEY (id)
 )';
 
-$ads = 'CREATE TABLE IF NOT EXISTS ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(240) NOT NULL,
-    description VARCHAR (1000) NOT NULL,
-    user_id INT UNSIGNED NOT NULL,
-    image VARCHAR (255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-)';
-
-$favorites = 'CREATE TABLE IF NOT EXISTS favorites (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    account_id INT UNSIGNED NOT NULL,
-    ad_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (account_id) REFERENCES users(id),
-    FOREIGN KEY (ad_id) REFERENCES ads(id)
-)';
-
-
 $dbc->exec($query);
-$dbc->exec($ads);
-$dbc->exec($favorites);
+
