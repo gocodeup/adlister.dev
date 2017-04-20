a2c53e5fc6347e8ddbb08b6bd9977e8d468894e3
xconstaud <xconstaud@gmail.com>
2017-04-20 13:03:29 -0500

    Update 404 with Jared

--- views/404.php
+++ views/404.php
@@ -1,9 +1,11 @@
 <div class="container">
-
     <section>
-
-        <h1 class="section-title">404 Error</h1>
-
+        <div class="row">
+            <div class="col-sm offset-sm-0 col-md-6 offset-md-3 centerText">
+                <h1 class="section-title">Sorry...</h1>
+                <img src="/img/jared.gif">
+                <h1 class="section-title">404: Jared Not Found</h1>
+            </div>
+        </div>
     </section>
-
 </div>
