a7e5eb96b67b324795bc8fc7eca4a6a5a58de878
Matt Valdez <mattvaldez01@gmail.com>
2017-04-21 01:25:10 -0500

    took out inline styling and put it in main.css

--- views/home.php
+++ views/home.php
@@ -1,43 +1,34 @@
-
 <div class="container">
 
-    <section id="welcome">
+	<section id="welcome">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 id="homeTitle" class="text-center">Welcome To Adlister</h1>
+			</div>
+		</div>
+	</section>
 
-        <div class="row">
+	<section id="features">
+		<div class="row">
+			<h2 class="section-title text-center">Featured Items</h2>
+			<div class="container">
+				<div class="row">
+					<?php foreach($selectThree as $ad) : ?>
+						<div class="col-sm-4">
+							<a href="/show?id=<?= $ad->id ?>" id="homeItemPanel" >
+								<div class="panel panel-primary">
+									<div class="panel-heading"><?= $ad->title ?><div class="pull-right"><?= "$" . $ad->price ?></div></div>
+									<div class="panel-body"><img src=<?= "$ad->image" ?> class="img-responsive" id="homeItemImage" alt="Image"></div>
+									<div class="panel-footer"><?= substr($ad->description, 0, 25) . "..." ?></div>
+								</div>
+							</a>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</div><!-- ends container -->
+		</div>
+	</section>
 
-            <div class="col-xs-12">
-
-                <h1 id="homeTitle" class="text-center">Welcome To Adlister</h1>
-
-            </div>
-
-        </div>
-
-    </section>
-
-    <section id="features">
-
-        <div class="row">
-
-            <h2 class="section-title text-center">Featured Items</h2>
-            <!-- Placeholder for featured items.-->
-        </div>
-
-    </section>
-    
-    <div class="container">
-        <div class="row">
-            <?php foreach($selectThree as $ad) { ?>
-                <div class="col-sm-4">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading"><a href="/show?id=<?= $ad->id ?>" style="color: white"><?= $ad->title ?><div class="pull-right"><?= "$" . $ad->price ?></div></div>
-                        <div class="panel-body"><img src=<?= "$ad->image" ?> class="img-responsive" style="width:100%; height:250px" alt="Image"></div></a>
-                        <div class="panel-footer"><?= $ad->description ?></div>
-                    </div>
-                </div>
-            <?php } ?>
-        </div>
-    </div><!-- ends container -->
-    <a href="/items" id="homeViewAll" style="margin-left: 45%" class="btn btn-primary">View All Items</a>
-
+	<a href="/items" id="homeViewAll" class="btn btn-primary">View All Items</a>
+	
 </div>
