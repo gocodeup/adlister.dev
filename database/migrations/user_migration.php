35cb5d39c904d9e4de98648080d6d72ef114262c
Cameron Palatas <cameronlewispalatas@gmail.com>
2017-04-21 10:20:55 -0500

    Added to Pagecontroller.php

--- database/migrations/user_migration.php
+++ database/migrations/user_migration.php
@@ -17,19 +17,4 @@ CREATE TABLE IF NOT EXISTS users (
 
 // $dbc->exec('DROP TABLE IF EXISTS posts');
 
-$query2 = "CREATE TABLE IF NOT EXISTS `posts` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `product_name` varchar(70) NOT NULL,
-  `category` varchar(70) NOT NULL,
-  `description` varchar(600) NOT NULL,
-  `price` int(10) NOT NULL,
-  `phone` varchar(12) NOT NULL,
-  `email` varchar(120) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8";
-
 $dbc->exec($query);
-$dbc->exec($query2);
