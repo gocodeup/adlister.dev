7fc8ae8204fb9135f66da94d01096d88a86f87fa
Phillip <ppalatas@gmail.com>
2017-04-21 09:08:00 -0500

    made changes trying to fix strict mime type error -- Still messed up

--- public/js/js_files.php
+++ public/js/js_files.php
@@ -2,9 +2,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="js/responsiveCarousel.min.js"></script>
-    <script type="text/javascript">
+    <script type="application/javascript" src="js/jquery-1.11.0.min.js"></script>
+    <script type="application/javascript" src="js/responsiveCarousel.min.js"></script>
+    <script type="application/javascript">
         $(function(){
         $('.crsl-items').carousel({
             visible: 4,
