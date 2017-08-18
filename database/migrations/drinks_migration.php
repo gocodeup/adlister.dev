1d17a8c8e40ebe5fe0dfd2ef99cddeedb2d28818
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-18 12:01:36 -0500

    Added migration queries for each migration file

--- database/migrations/drinks_migration.php
+++ database/migrations/drinks_migration.php
@@ -5,12 +5,12 @@ require_once '../db_connect.php';
 
 $dbc->exec('DROP TABLE IF EXISTS users');
 
-$query = 'CREATE TABLE livestock (
+$query = 'CREATE TABLE drinks (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     name VARCHAR(240) NOT NULL,
     date_posted DATE NOT NULL,
-    type_of_livestock VARCHAR(240) NOT NULL,
+    type_of_drink VARCHAR(240) NOT NULL,
     price FLOAT NOT NULL,
     description TEXT NOT NULL,
     purchase_status VARCHAR(240) NOT NULL,
---
+++ database/migrations/weapons_migration.php
@@ -0,0 +1,20 @@
+<?php
+
+$_ENV = include __DIR__ . '/../../.env.php';
+require_once '../db_connect.php';
+
+$dbc->exec('DROP TABLE IF EXISTS users');
+
+$query = 'CREATE TABLE weapons (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    name VARCHAR(240) NOT NULL,
+    date_posted DATE NOT NULL,
+    type_of_weapon VARCHAR(240) NOT NULL,
+    price FLOAT NOT NULL,
+    description TEXT NOT NULL,
+    purchase_status VARCHAR(240) NOT NULL,
+    PRIMARY KEY (id)
+)';
+
+$dbc->exec($query);
