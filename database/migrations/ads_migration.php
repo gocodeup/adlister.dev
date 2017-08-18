559695f4a7f350fcd321f5d89755a73c8ac98a3d
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-18 14:30:38 -0500

    did yet another MINOR edit on misspelling

--- database/migrations/ads_migration.php
+++ database/migrations/ads_migration.php
@@ -3,7 +3,7 @@
 $_ENV = include __DIR__ . '/../env.php';
 require_once '../db_connect.php';
 
-$connection>exec('DROP TABLE IF EXISTS users');
+$connection->exec('DROP TABLE IF EXISTS ads');
 
 $query = 'CREATE TABLE ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
