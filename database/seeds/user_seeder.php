8340e8b96d5da467e1e73d206d263b0ac20e9d0a
Jen Ayala <jenayala1@gmail.com>
2017-08-18 16:33:21 -0500

    User seeder, user account page, & user edit page in process - working on db connect issues with the seeder

--- database/seeds/user_seeder.php
+++ database/seeds/user_seeder.php
@@ -1,31 +1,60 @@
 <?php
 
 require_once __DIR__ . '/../../models/User.php';
+require_once '../db_connect.php';
 
-$user = new User;
+
+// $connection->exec("TRUNCATE users");
+// $statement = "INSERT INTO  users(name, email, username, password) VALUES (:name, :email, :username, :password)
+// $preparedStatement = $connection->prepare($statement);
+
+$user = new User();
 $user->name = 'Finn Mertens';
 $user->email = 'finn@fansofbilly.com';
 $user->username = "finn_the_human";
 $user->password = $_ENV['USER_PASS'];
 $user->save();
 
-$user = new User;
+$user = new User();
 $user->name = 'Jake';
 $user->email = 'jake@fansofbilly.com';
 $user->username = "jake_the_dog";
 $user->password = $_ENV['USER_PASS'];
 $user->save();
 
-$user = new User;
+$user = new User();
 $user->name = 'Simon Petrikov';
 $user->email = 'iceking@princessaholicanonymous.com';
 $user->username = "ice_king";
 $user->password = $_ENV['USER_PASS'];
 $user->save();
 
-$user = new User;
+$user = new User();
 $user->name = 'Marceline Abadeer';
 $user->email = 'marceline@vampirequeen.com';
 $user->username = "marceline_abadeer";
 $user->password = $_ENV['USER_PASS'];
 $user->save();
+
+$user = new User();
+$user->name = 'Jen Ayala';
+$user->email = 'jenayala1@email.com';
+$user->username = "pandoran_elf";
+$user->password = $_ENV['USER_PASS'];
+$user->save();
+
+$user = new User();
+$user->name = 'Gonzolo Rodriguez';
+$user->email = 'gonzolo@email.com';
+$user->username = "vlad_the_impossible";
+$user->password = $_ENV['USER_PASS'];
+$user->save();
+
+$user = new User();
+$user->name = 'Emmanuelle Maher';
+$user->email = 'emmanuelle@email.com';
+$user->username = "the_iron_maiden";
+$user->password = $_ENV['USER_PASS'];
+$user->save();
+
+echo "users added".PHP_EOL;
---
+++ models/Ads.php
@@ -0,0 +1,11 @@
+<?php
+
+$_ENV = require __DIR__ . '/../env.php';
+$_ENV = require __DIR__. '/../Model.php';
+
+class Ads extends Model
+{
+
+
+
+}
