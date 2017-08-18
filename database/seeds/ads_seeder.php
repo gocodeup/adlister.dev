f1a72ad41d722bea2ddd9ace5fef76a816977f29
Eric Pharr <ericjpharr@gmail.com>
2017-08-18 16:06:27 -0500

    Updated seeder.

--- database/seeds/ads_seeder.php
+++ database/seeds/ads_seeder.php
@@ -8,5 +8,28 @@ $ad->description = "Just some old shoes";
 $ad->seller_id = 1;
 $ad->save();
 
+$ad = new Ad;
+$ad->name = "Jakes's old shirt";
+$ad->description = "Just an old shirt";
+$ad->seller_id = 2;
+$ad->save();
+
+$ad = new Ad;
+$ad->name = "Simon's old simon game";
+$ad->description = "Just a simple simon game";
+$ad->seller_id = 3;
+$ad->save();
+
+$ad = new Ad;
+$ad->name = "Finn's old wooden sword";
+$ad->description = "Just an old wooden sword";
+$ad->seller_id = 1;
+$ad->save();
+
+$ad = new Ad;
+$ad->name = "Marceline's old guitar";
+$ad->description = "Just an old guitar";
+$ad->seller_id = 4;
+$ad->save();
 
  ?>
