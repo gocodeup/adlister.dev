266d08052674fae3478743230e0ea508126c51c6
Hunter Warren <hunter.warren@gmail.com>
2017-08-18 16:48:30 -0500

    added a bootstrap CDN link and a main.css link to the header.

--- public/index.php
+++ public/index.php
@@ -4,11 +4,14 @@ require_once __DIR__ . '/../bootstrap.php';
 require_once "../controllers/PageController.php";
 
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>OooLister</title>
     <?php require '../views/partials/head.php'; ?>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
     <?php require '../views/partials/navbar.php'; ?>
@@ -16,5 +19,7 @@ require_once "../controllers/PageController.php";
     <?php require $mainView; ?>
 
     <?php require '../views/partials/common_js.php'; ?>
+    
+    <?php require '../views/partials/footer.php'; ?>
 </body>
 </html>
