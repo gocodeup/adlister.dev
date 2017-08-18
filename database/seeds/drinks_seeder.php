7c3acfa9aeac8b69f7e7b6a6d5aad5344317a55f
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-18 15:54:14 -0500

    Updated seeders

--- database/seeds/drinks_seeder.php
+++ database/seeds/drinks_seeder.php
@@ -5,8 +5,16 @@ require_once '../db_connect.php';
 
 $connection->exec("TRUNCATE drinks");
 
-$statement = "INSERT INTO drinks (user_id, name, sub_category) VALUES (:user_id, :name, :sub_category)";
+$statement = "INSERT INTO drinks (user_id, name, sub_category) VALUES 
+('2', 'Oat Mead', 'Mead'),
+('5', 'Absinthe', 'Wine'),
+('5', 'Hemlock For The Weary', 'Ale'),
+('3', 'Ye Olde Malt', 'Beer'),
+('4', 'Athlete\s Boot', 'Wine'),
+('1', 'Venom', 'Mead'),
+('1', 'Hopped', 'Beer'),
+('4', 'Serf Brew', 'Ale')" ;
 
-$preparedStatement = $connection->prepare($statement);
+$connection->exec($statement);
 
 echo "values added".PHP_EOL;
