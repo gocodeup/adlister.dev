0ba4a92d94f3a998af0b71f07ad8aa3672e95b0e
David <david.newton39@yahoo.com>
2017-08-18 16:52:39 -0500

    Modified user migration and added items migration file

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=' . $_ENV['127.0.0.1'] . ';dbname=' . $_ENV['adlister_db'], $_ENV['chloedavid_user'], $_ENV['password123']);
+$dbc = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
