903a046c6ee0ea8682ffae77fcd260657ab6ae1d
Gonzalo Rodriguez <g@stm.pm>
2017-08-18 13:42:59 -0500

    html creation

--- public/index.php
+++ public/index.php
@@ -1,19 +1,145 @@
-<?php
-session_start();
-require_once __DIR__ . '/../bootstrap.php';
-
-?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>OooLister</title>
-    <?php require '../views/partials/head.php'; ?>
-</head>
-<body>
-    <?php require '../views/partials/navbar.php'; ?>
-
-    <?php require $mainView; ?>
-
-    <?php require '../views/partials/common_js.php'; ?>
-</body>
+    <head>
+        <title>OooLister</title>
+        <!--Partial for head.  Good place to include things like css files.-->
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    </head>
+    <body>
+        <!--partial view for navbar-->
+        <div class="container">
+            <section>
+                <h1 class="section-title">404 Error</h1>
+            </section>
+        </div>
+        <!--Partial view for common javascipt files-->
+        <div class="centerflipcards">
+            <div class="square-flip">
+                <div class='square' data-image="/img/armor-back.jpg">
+                    <div class="square-container">
+                        <div class="align-center">
+                            <img src="img/armor.png" class="boxshadow" alt="">
+                        </div>
+                        <h2 class="textshadow">Armor</h2>
+                        <h3 class="textshadow">"practiaclly bullet proof"</h3>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+                <div class='square2' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container2">
+                        <div class="align-center"></div>
+                        <a href="http://kallyas.net" target="_blank" class="boxshadow kallyas-button">View Demos</a>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+            </div>
+            <div class="square-flip">
+                <div class='square' data-image="img/weapons-back.png">
+                    <div class="square-container">
+                        <div class="align-center">
+                            <img src="img/weapons.png" class="boxshadow" alt="">
+                        </div>
+                        <h2 class="textshadow">Weapons</h2>
+                        <h3 class="textshadow">"Nice and sharp"</h3>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+                <div class='square2' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container2">
+                        <div class="align-center"></div>
+                        <a href="http://kallyas.net" target="_blank" class="boxshadow kallyas-button">View Demos</a>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+            </div>
+            <div class="square-flip">
+                <div class='square' data-image="img/tools-back.png">
+                    <div class="square-container">
+                        <div class="align-center">
+                            <img src="img/tools.png" class="boxshadow" alt="">
+                        </div>
+                        <h2 class="textshadow">Tools</h2>
+                        <h3 class="textshadow">"fix everything"</h3>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+                <div class='square2' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container2">
+                        <div class="align-center"></div>
+                        <a href="http://kallyas.net" target="_blank" class="boxshadow kallyas-button">View Demos</a>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+            </div>
+            <div class="square-flip">
+                <div class='square' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container">
+                        <div class="align-center">
+                            <img src="http://titanicthemes.com/files/flipbox/kallyas.png" class="boxshadow" alt="">
+                        </div>
+                        <h2 class="textshadow">Torture Devices</h2>
+                        <h3 class="textshadow">"teaching lessons"</h3>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+                <div class='square2' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container2">
+                        <div class="align-center"></div>
+                        <a href="http://kallyas.net" target="_blank" class="boxshadow kallyas-button">View Demos</a>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+            </div>
+            <div class="square-flip">
+                <div class='square' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container">
+                        <div class="align-center">
+                            <img src="http://titanicthemes.com/files/flipbox/kallyas.png" class="boxshadow" alt="">
+                        </div>
+                        <h2 class="textshadow">Livestock</h2>
+                        <h3 class="textshadow">"all the sheeples"</h3>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+                <div class='square2' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container2">
+                        <div class="align-center"></div>
+                        <a href="http://kallyas.net" target="_blank" class="boxshadow kallyas-button">View Demos</a>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+            </div>
+            <div class="square-flip">
+                <div class='square' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container">
+                        <div class="align-center">
+                            <img src="http://titanicthemes.com/files/flipbox/kallyas.png" class="boxshadow" alt="">
+                        </div>
+                        <h2 class="textshadow">Ales</h2>
+                        <h3 class="textshadow">"only the best meads"</h3>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+                <div class='square2' data-image="http://titanicthemes.com/files/flipbox/kallyas-bundle.png">
+                    <div class="square-container2">
+                        <div class="align-center"></div>
+                        <a href="http://kallyas.net" target="_blank" class="boxshadow kallyas-button">View Demos</a>
+                    </div>
+                    <div class="flip-overlay"></div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <p>Made with ♡ by Nicola Mihaita</p>
+            <a href="http://hogash.com" target="_blank">@Hogash.com</a>
+            <br />
+            <br />
+            <br />
+        </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+        <script src="js/main.js">
+</script>
+    </body>
 </html>
