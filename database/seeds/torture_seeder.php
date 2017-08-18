5e9152f022ebb17a1f8cab6c0c3fa1dcfa7fda2c
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-18 15:36:36 -0500

    Updated seeders to include default values for each table

--- database/seeds/torture_seeder.php
+++ database/seeds/torture_seeder.php
@@ -5,8 +5,17 @@ require_once '../db_connect.php';
 
 $connection->exec("TRUNCATE torture");
 
-$statement = "INSERT INTO torture (user_id, name, sub_category) VALUES (:user_id, :name, :sub_category)";
 
-$preparedStatement = $connection->prepare($statement);
+$statement = "INSERT INTO torture (user_id, name, sub_category) VALUES 
+('4', 'Dream Gal', 'Iron Maiden'),
+('2', 'Eat Cake', 'Guillotine'),
+('5', 'S&M', 'Shame Mask'),
+('5', 'Flogger\'s Friend', 'Pillory'),
+('2', 'Simple Shackles', 'Chains'),
+('3', 'Mommy Issues', 'Iron Maiden'),
+('1', 'Out To Dry', 'Rack'),
+('2', 'Old But Still Good', 'Chains')" ;
+
+$connection->exec($statement);
 
 echo "values added".PHP_EOL;
--- database/seeds/weapons_seeder.php
+++ database/seeds/weapons_seeder.php
@@ -5,8 +5,6 @@ require_once '../db_connect.php';
 
 $connection->exec("TRUNCATE weapons");
 
-// $statement = "INSERT INTO weapons (user_id, name, sub_category) VALUES (:user_id, :name, :sub_category)";
-
 $statement = "INSERT INTO weapons (user_id, name, sub_category) VALUES 
 ('1', 'Kingslayer', 'Sword'),
 ('2', 'Heartpiercer', 'Spear'),
