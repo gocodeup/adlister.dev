34a5f57473b53809caced46f167da7a4bc8a84ea
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-18 12:12:15 -0500

    Renamed variables and table columns for more uniformity

--- database/migrations/torture_migration.php
+++ database/migrations/torture_migration.php
@@ -3,18 +3,18 @@
 $_ENV = include __DIR__ . '/../../.env.php';
 require_once '../db_connect.php';
 
-$dbc->exec('DROP TABLE IF EXISTS users');
+$connection->exec('DROP TABLE IF EXISTS users');
 
 $query = 'CREATE TABLE torture (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     name VARCHAR(240) NOT NULL,
     date_posted DATE NOT NULL,
-    type_of_torture VARCHAR(240) NOT NULL,
+    sub_category VARCHAR(240) NOT NULL,
     price FLOAT NOT NULL,
     description TEXT NOT NULL,
-    purchase_status VARCHAR(240) NOT NULL,,
+    availability VARCHAR(240) NOT NULL,,
     PRIMARY KEY (id)
 )';
 
-$dbc->exec($query);
+$connection->exec($query);
