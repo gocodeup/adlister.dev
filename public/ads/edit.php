d93ba9fcee9cdbd03c383ae3e437c0555a711f44
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 12:09:08 -0500

    Added forms to edit and create add

--- public/ads/edit.php
+++ public/ads/edit.php
@@ -11,6 +11,39 @@ require_once __DIR__ . "../../../bootstrap.php";
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
-	<p>HELLO</p>
+<h1>Edit ad</h1>
+	<div class= "col-md-6 col-md-offset-3">
+		<form method="POST" action="">
+			<div class="form-group">
+				<label for="category">Category:</label>
+				<input type="text" id="category"></input>
+			</div>
+			<div class="form-group">
+				<label for="price">Price:</label>
+				<input type="text" id="price"></input>
+			</div>
+			<div class="form-group">
+				<label for="description">Description:</label>
+				<input type="text" id="description"></input>
+			</div>
+			<div class="form-group">
+				<label for="pick_up_location">Pick Up Location:</label>
+				<input type="text" id="pick_up_location"></input>
+			</div>
+			<div class="form-group">
+				<label for="location_city">Location City:</label>
+				<input type="text" id="location_city"></input>
+			</div>
+			<div class="form-group">
+				<label for="image">Image:</label>
+				<input type="text" id="image"></input>	
+			</div>
+			<div class="row">
+                <div class="col-sm-6">
+                    <button type="submit" class="btn btn-primary">Submit changes</button>
+                </div>
+           	</div>
+        </form>
+	</form>
 </body>
 </html>
