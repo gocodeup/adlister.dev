d511dcfe4565634bb01310b3004fe47599866a6d
Chloe Adams <chloeadams@Chloes-MacBook-Pro.local>
2017-08-21 11:32:19 -0500

    Committing JS

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,34 +1,80 @@
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-               <span class="sr-only">Toggle navigation</span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">AdLister</a>
-        </div>
+<?php
+require('public/css/main.css');
+require('public/css/navbar.js');
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="/items">Items For Sale</a></li>
-                <?php if (Auth::check()) : ?>
-                    <li><a href="/users/account?id=<?= Auth::id(); ?>">Account</a></li>
-                    <li><a href="/logout">Logout</a></li>
-                    <li><a href="/items/create">Create Post</a></li>
-                <?php else : ?>
-                    <li><a href="/login">Login</a></li>
-                    <li><a href="/signup">Signup</a></li>
-                <?php endif; ?>
+<nav>
+    <ul class="desktop-menu" id="desktopMenu">
+
+        <li>
+            <a id="home" href="/">
+                <img src="broccolilogo.png" class="yonsei-logo">VEGGIE LIST
+            </a>
+        </li>
+
+        <li class="desktop-link">
+            <a href="#">HOME</a>
+
+
+        <li class="desktop-link">
+            <a href="/items">VEGGIES FOR SALE
+            </a>
+        </li>
+
+         <?php if (Auth::check()) : ?>
+
+        <li>
+            <a href="/users/account?id=<?= Auth::id(); ?>">Account
+            </a>
+        </li>
+        
+        <li>
+            <a href="/logout">Logout
+            </a>
+        </li>
+        
+        <li>
+            <a href="/items/create">Create Post
+            </a>
+        </li>
+        
+        <?php else : ?>
+                    
+        <li>
+            <a href="/login">Login
+            </a>
+        </li>
+                    
+        <li>
+            <a href="/signup">Signup
+            </a>
+        </li>
+        
+        <?php endif; ?>
+        
+        
+        <?php if (Auth::check()) : ?>
+                
+            <ul class="desktopMenu hidden-xs">
+                 <span class="navbar-text">Welcome <?= Auth::user()->name; ?></span>
             </ul>
-            <?php if (Auth::check()) : ?>
-                <ul class="nav navbar-nav navbar-right hidden-xs">
-                    <span class="navbar-text">Welcome <?= Auth::user()->name; ?></span>
-                </ul>
-            <?php endif; ?>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+        
+        <?php endif; ?>
+
+        
+
+            <li id="mobile-menu">
+                <a id="home2" href="index.html">
+                    <img src="broccolilogo.png" class="yonsei-logo">
+                </a>
+
+          
+            </a>
+
+            <a id="mobile-icon-container" href="#" onclick="responsiveMenu(); return false;">
+                <img id="mobile-icon" src="https://eliya33.github.io/church/images/mobile-menu-icon-125x125.png" alt="Mobile Menu Icon">
+            </a>
+    
+        </li>
+
+    </ul>
 </nav>
