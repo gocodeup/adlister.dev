fbddf23454efa686b1751d9f01b690b6ec812756
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 09:45:03 -0500

    attempted to update ad_seeder to make it functional. will return to later

--- database/seeds/ads_seeder.php
+++ database/seeds/ads_seeder.php
@@ -5,7 +5,8 @@ require_once '../db_connect.php';
 
 $connection->exec("TRUNCATE ads");
 
-$statement = "INSERT INTO ads (user_id, username, date_posted, product_name, category, sub_category, price, description, availability_status, pick_up_location, location_city, location_state, date_sold, contact_info, image) VALUES (:user_id, :username, :date_posted, :product_name, :category, :sub_category, :price, description, :availability_status, :pick_up_location, :location_city, :location_state, :date_sold, :contact_info, :image)";
+$statement = "INSERT INTO ads (user_id, username, date_posted, product_name, category, sub_category, price, description, availability_status, pick_up_location, location_city, location_state, date_sold, contact_info, image) VALUES 
+('1', 'testuser', '2009-04-12', 'testname', 'testcategory', 'testsub', '23', 'tests test', 'available', 'here', 'there', 'texas', 'test', 'test', 'test')";
 
 $preparedStatement = $connection->prepare($statement);
 
