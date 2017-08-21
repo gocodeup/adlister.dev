fc9681290ca7fe8226503216f17f2ace70a243cb
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 10:01:30 -0500

    Deleted unnecessary columns from ads_migration.php

--- database/seeds/ads_seeder.php
+++ database/seeds/ads_seeder.php
@@ -6,8 +6,8 @@ require_once '../db_connect.php';
 $connection->exec("TRUNCATE ads");
 
 $statement = "INSERT INTO ads (user_id, username, date_posted, product_name, category, sub_category, price, description, availability_status, pick_up_location, location_city, location_state, date_sold, contact_info, image) VALUES 
-('1', 'testuser', '2009-04-12', 'testname', 'testcategory', 'testsub', '23', 'tests test', 'available', 'here', 'there', 'texas', 'test', 'test', 'test')";
+('user_id', 'username', '1994-12-10', 'product_name', 'category', 'sub_category', 32 , 'description', 'availability_status', 'pick_up_location', 'location_city', 'location_state', '2000-02-03', 'contact_info', 'image')" ;
 
-$preparedStatement = $connection->prepare($statement);
+$connection->prepare($statement);
 
 echo "values added".PHP_EOL;
