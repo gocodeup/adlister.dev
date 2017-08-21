27b184b1ea00dcfa216f266d62076ea32cd6fd13
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 13:24:48 -0500

    edited input types on forms

--- public/ads/create.php
+++ public/ads/create.php
@@ -13,46 +13,46 @@ require_once __DIR__ . "../../../bootstrap.php";
 <body>
 <h1>Create an ad</h1>
 	<div class= "col-md-6 col-md-offset-3">
-		<form method="POST" action="">
+		<form method="POST" action="" enctype="multipart/form-data">
 			<div class="name">
 				<label for="name">Name:</label>
-				<input type="text" id="name"></input>
+				<input type="text" name="name"></input>
 			</div>
 			<div class="form-group">
 				<label for="category">Category:</label>
-				<input type="text" id="category"></input>
+				<input type="text" name="category"></input>
 			</div>
 			<div class="form-group">
 				<label for="sub_category">Sub-Category:</label>
-				<input type="text" id="sub_category"></input>
+				<input type="text" name="sub_category"></input>
 			</div>
 			<div class="form-group">
 				<label for="price">Price:</label>
-				<input type="text" id="price"></input>
+				<input type="text" name="price"></input>
 			</div>
 			<div class="form-group">
 				<label for="description">Description:</label>
-				<input type="text" id="description"></input>
+				<textarea name="description" cols="40" rows="4"></textarea>
 			</div>
 			<div class="form-group">
 				<label for="pick_up_location">Pick Up Location:</label>
-				<input type="text" id="pick_up_location"></input>
+				<input type="text" name="pick_up_location"></input>
 			</div>
 			<div class="form-group">
 				<label for="location_city">Location City:</label>
-				<input type="text" id="location_city"></input>
+				<input type="text" name="location_city"></input>
 			</div>
 			<div class="form-group">
 				<label for="image">Image:</label>
-				<input type="text" id="image"></input>	
+				<input type="file" name="image"></input>	
 			</div>
 			<div class="row">
-                <div class="col-sm-6">
+               <div class="col-sm-6">
                     <button type="submit" class="btn btn-primary">Submit changes</button>
                 </div>
            	</div>
         </form>
-    </div>
+	</div>
 
 </body>
 </html>
