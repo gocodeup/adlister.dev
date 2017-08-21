b5146a0ad18cab1cd8262cb673a870c74dd2a97a
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 10:46:26 -0500

    Rearranged folders for more effective path

--- public/ads/create.php
+++ public/ads/create.php
@@ -14,8 +14,6 @@ require_once __DIR__ . ' /../utils/helper_functions.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
-	<?php include_once 'assets/header.php' ?>
-        <div class="container">
-            <?php include_once 'assets/header.php' ?>
+	<p>HELLO</p>
 </body>
 </html>
