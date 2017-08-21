1a0277223c3ccf370b74645289b583bcd6925f13
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 11:26:52 -0500

    made minor changes

--- public/ads/show.php
+++ public/ads/show.php
@@ -1,8 +1,5 @@
 <?php
-require_once __DIR__ . '/../../database/db_connect.php';
-require_once "../../utils/Input.php";
-require_once "../../utils/Auth.php";
-require_once __DIR__ . '../../utils/helper_functions.php';
+require_once __DIR__ . "../../../bootstrap.php";
 
 ?>
 
