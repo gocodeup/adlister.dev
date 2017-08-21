9012b3f3d8af6d18206b5ba699934b6f48e1ccc1
Gonzalo Rodriguez <g@stm.pm>
2017-08-21 15:27:53 -0500

    added the working footer

--- public/login.php
+++ public/login.php
@@ -98,9 +98,9 @@
                </div>
             </section>
          </div>
-
-      </div>
 <?php include 'assets/footer.php' ?>
+      </div>
+
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
       <script type="text/javascript" src="js/main.js"></script>
