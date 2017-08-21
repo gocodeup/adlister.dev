4a6654e2efa588f4d002f2f9270293bb9b95bfe1
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-08-21 13:54:00 -0500

    Rearranged divs, added css and scripts to page

--- public/ads/create.php
+++ public/ads/create.php
@@ -7,52 +7,63 @@ require_once __DIR__ . "../../../bootstrap.php";
 <html>
 <head>
 	<title>Create Listing</title>
-	<link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../css/menu.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
 </head>
 <body>
-<h1>Create an ad</h1>
-	<div class= "col-md-6 col-md-offset-3">
-		<form method="POST" action="" enctype="multipart/form-data">
-			<div class="name">
-				<label for="name">Name:</label>
-				<input type="text" name="name"></input>
-			</div>
-			<div class="form-group">
-				<label for="category">Category:</label>
-				<input type="text" name="category"></input>
-			</div>
-			<div class="form-group">
-				<label for="sub_category">Sub-Category:</label>
-				<input type="text" name="sub_category"></input>
-			</div>
-			<div class="form-group">
-				<label for="price">Price:</label>
-				<input type="text" name="price"></input>
-			</div>
-			<div class="form-group">
-				<label for="description">Description:</label>
-				<textarea name="description" cols="40" rows="4"></textarea>
-			</div>
-			<div class="form-group">
-				<label for="pick_up_location">Pick Up Location:</label>
-				<input type="text" name="pick_up_location"></input>
-			</div>
-			<div class="form-group">
-				<label for="location_city">Location City:</label>
-				<input type="text" name="location_city"></input>
-			</div>
-			<div class="form-group">
-				<label for="image">Image:</label>
-				<input type="file" name="image"></input>	
-			</div>
-			<div class="row">
-               <div class="col-sm-6">
-                    <button type="submit" class="btn btn-primary">Submit changes</button>
-                </div>
-           	</div>
-        </form>
+<?php include_once '../assets/menu.php'; ?>
+<div id="content">
+	<div class="menu-trigger"></div>
+	<?php include '../assets/header.php' ?>
+	<div class="container">
+		<h1>Create an ad</h1>
+		<div class= "col-md-6 col-md-offset-3">
+			<form method="POST" action="" enctype="multipart/form-data">
+				<div class="name">
+					<label for="name">Name:</label>
+					<input type="text" name="name"></input>
+				</div>
+				<div class="form-group">
+					<label for="category">Category:</label>
+					<input type="text" name="category"></input>
+				</div>
+				<div class="form-group">
+					<label for="sub_category">Sub-Category:</label>
+					<input type="text" name="sub_category"></input>
+				</div>
+				<div class="form-group">
+					<label for="price">Price:</label>
+					<input type="text" name="price"></input>
+				</div>
+				<div class="form-group">
+					<label for="description">Description:</label>
+					<textarea name="description" cols="40" rows="4"></textarea>
+				</div>
+				<div class="form-group">
+					<label for="pick_up_location">Pick Up Location:</label>
+					<input type="text" name="pick_up_location"></input>
+				</div>
+				<div class="form-group">
+					<label for="location_city">Location City:</label>
+					<input type="text" name="location_city"></input>
+				</div>
+				<div class="form-group">
+					<label for="image">Image:</label>
+					<input type="file" name="image"></input>	
+				</div>
+				<div class="row">
+			        <div class="col-sm-6">
+		                <button type="submit" class="btn btn-primary">Submit changes</button>
+		            </div>
+		 	  	</div>
+		    </form>
+		</div>
+		
 	</div>
-
+</div>
+	<script type="text/javascript" src="../js/main.js"></script>
+	<script type="text/javascript" src="../js/menu.js"></script>
 </body>
 </html>
