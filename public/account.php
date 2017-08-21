6e0b994d345093dcd19181563d53f5537be788b5
Jen Ayala <jenayala1@gmail.com>
2017-08-21 12:32:52 -0500

    unresolved issue with the required files, reverted to previous file links, styling on pages

--- public/account.php
+++ public/account.php
@@ -1,11 +1,14 @@
 <?php
-require_once __DIR__ . '/../database/db_connect.php';
-require_once __DIR__ . ' /bootstrap.php';
+require_once __DIR__  .  '/../database/db_connect.php';
+require_once "../utils/Input.php";
+require_once "../utils/Auth.php";
+require_once __DIR__  .  ' /../utils/helper_functions.php';
+
 
  function pageController()
 {
     $data = [];
-    if(isset($_SESSION['l']))
+
 
 }
 extract(pageController());
@@ -15,9 +18,10 @@ extract(pageController());
 <html>
     <head>
         <title>Account Page</title>
-        <!--Partial for head.  Good place to include things like css files.-->
-        <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/menu.css">
+       <link rel="stylesheet" href="css/main.css">
+       <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
     </head>
     <body>
             <?php include_once 'assets/menu.php' ?>
@@ -35,10 +39,10 @@ extract(pageController());
             </form>
         </div>
 
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-            <script src="js/main.js">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="js/main.js"></script>
+      <script type="text/javascript" src="js/menu.js"></script>
             </script>
         </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -1,6 +1,10 @@
 <?php
-require_once __DIR__ . '/../database/db_connect.php';
-require_once __DIR__ .  '/bootstrap.php';
+require_once __DIR__ .  '/../database/db_connect.php';
+require_once __DIR__ .  '/../database/db_connect.php';
+require_once "../utils/Input.php";
+require_once "../utils/Auth.php";
+require_once __DIR__ . ' /../utils/helper_functions.php';
+
 
 function pageController()
 {
@@ -45,11 +49,13 @@ function pageController()
  <html>
      <head>
          <title>Login Page</title>
-         <link rel="stylesheet" href="css/main.css">
          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+         <link rel="stylesheet" type="text/css" href="css/menu.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
      </head>
      <body>
-         <?php include_once 'assets/header.php' ?>
+         <?php include_once 'assets/menu.php' ?>
         <div class="container">
             <?php include_once 'assets/header.php' ?>
                 <h1 class="section-title">Login To Antiquitas Lost </h1>
@@ -89,5 +95,9 @@ function pageController()
                 </div>
             </section>
         </div>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="js/main.js"></script>
+      <script type="text/javascript" src="js/menu.js"></script>
     </body>
 </html>
