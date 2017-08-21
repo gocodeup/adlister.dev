f725eca61ed75fd6d9aeee600a5c72e117b1bf72
Jen Ayala <jenayala1@gmail.com>
2017-08-21 10:28:33 -0500

    merge conflict resolution

--- utils/Log.php
+++ utils/Log.php
@@ -4,7 +4,36 @@ require_once '../models/User.php';
 
 class Log
 {
+    	private $filename;
+    	private $handle;
+    	public function __construct($prefix= "log")
+    	{
+    		if (!is_string($prefix)){
+    			$prefix = "log";
+    		}
 
+    		$this->filename = "{$prefix}-".date("Y-m-d").".log";
+    		$this->handle = fopen($this->filename, "a");
+    	}
+    	protected function logMessage($logLevel, $message)
+    	{
+    		$this->filename = date("Y-m-d");
+    	    $stringToWrite = date("Y-m-d H:i:s"). "[" .$logLevel. "]" . $message . PHP_EOL;
+    	    fwrite($this->handle, $stringToWrite);
+    	}
+    	public function info($message)
+    	{
+    		$this->logMessage("INFO", $message);
+    	}
+    	public function error($message)
+    	{
+    		$this->logMessage("ERROR", $message);
+    	}
+
+    	public function __destruct(){
+    		fclose($this->handle);
+    	}
+    }
 
 
 }
