8975e41c16b45f8197472d1560ecd8dcce6267b6
Chloe Adams <chloeadams@Chloes-MacBook-Pro.local>
2017-08-22 09:34:55 -0500

    Committing Yesterday's Changes to Login, SignUp, and Homepage

--- utils/Auth.php
+++ utils/Auth.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../views/users/login.php';
+
 require_once '../models/User.php';
 
 class Auth
--- views/users/login.php
+++ views/users/login.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
     <title>Adlister</title>
-<body>
+
 <div class="container">
 
     <section id="login">
 
         <div class="row">
 
-            <h1 class="section-title">Login To AdLister</h1>
+            <div id='broccoliIcon'>
+
+                <h1 class="section-title">Login to Veggie Connect</h1>
+                    <img src="broccolilogo.png" class="yonsei-logo rotate brocc">
+
+            </div>
 
             <?php if (isset($_SESSION['ERROR_MESSAGE'])) : ?>
                 <div class="alert alert-danger">
@@ -25,22 +30,22 @@
 
             <div class="col-md-6 col-md-offset-3">
 
-                <p>Login with your email/username and password</p>
+                <p>Login with your email/username and password.</p>
 
                 <form method="POST" action="" data-validation data-required-message="This field is required">
 
                     <div class="form-group">
-                        <input type="text" class="form-control" id="email_user" name="email_user" placeholder="Email or Username" data-required>
+                        <input type="text" class="form-control" id="email_user" name="email_user" placeholder="EMAIL OR USERNAME" data-required>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Password" data-required>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="PASSWORD" data-required>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <button type="submit" class="btn btn-primary">Login</button>
+                    <div class="loginButtonRow">
+                        <div class="column one-half loginButtonRow">
+                            <button class="button btn btn-primary">LOGIN</button>
                         </div>
-                        <div class="col-sm-6 text-right">
-                            <a href="/signup" class="btn btn-success">Go To Signup</a>
+                        <div class="column one-half loginButtonRow">
+                            <a href="/signup" id="signupButton" class="button btn btn-success">NOT A MEMBER? SIGN-UP</a>
                         </div>
                     </div>
                 </form>
@@ -52,5 +57,4 @@
     </section>
 
 </div>
-</body>
 </html>
